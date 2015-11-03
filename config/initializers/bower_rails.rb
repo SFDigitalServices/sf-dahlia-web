d0e361bf8e9101ad814e883e979c3cdfdf124bf1
Christine Dolendo <chris.dolendo@gmail.com>
2015-11-03 11:25:17 -0800

    [#107322916] - Setup installation of bower through npm

--- config/initializers/bower_rails.rb
+++ config/initializers/bower_rails.rb
@@ -13,7 +13,7 @@ BowerRails.configure do |bower_rails|
 
   # Invokes rake bower:install:deployment instead rake bower:install.
   # Defaults to false
-  # bower_rails.use_bower_install_deployment = true
+  bower_rails.use_bower_install_deployment = true
   #
   # Invokes rake bower:install and rake bower:install:deployment
   # with -F (force) flag. Defaults to false
