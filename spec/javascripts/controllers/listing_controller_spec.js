8d9dc2ff3213f378661a63ed6cf64c441170ae30
Dave Kaplan <dave@exygy.com>
2015-11-05 14:54:01 -0800

    [#101070554] - Working tests for ListingController; had to include jquery gem in development/test in order to use jasmine-jquery-rails

--- spec/javascripts/controllers/listing_controller_spec.js
+++ spec/javascripts/controllers/listing_controller_spec.js
@@ -1,34 +1,42 @@
-// 'use strict';
+(function() {
+  'use strict';
 
-// describe('ListingController', function() {
-//   // jasmine.getJSONFixtures().fixturesPath = '/public/json';
-//   var scope;
-//   var state;
-//   var fakeListingService;
-//   var fakeListings = getJSONFixture('/listings.json').listings;
-//   var fakeListing = getJSONFixture('/listings/0.json').listing;
+  describe('ListingController', function() {
+    jasmine.getJSONFixtures().fixturesPath = '/public/json';
+    var scope;
+    var state;
+    var fakeListingService;
+    var fakeListings = getJSONFixture('/listings.json').listings;
+    var fakeListing = getJSONFixture('/listings/0.json').listing;
 
-//   beforeEach(module('ccp.controllers', function($provide) {
-//     fakeListingService = {
-//       listings: fakeListings,
-//       listing: fakeListing,
-//     };
+    beforeEach(module('dahlia.controllers', function($provide) {
+      fakeListingService = {
+        listings: fakeListings,
+        listing: fakeListing,
+      };
 
-//     $provide.value('ListingService', fakeListingService);
-//   }));
+      $provide.value('ListingService', fakeListingService);
+    }));
 
-//   beforeEach(inject(function($rootScope, $controller) {
-//     scope = $rootScope.$new();
-//     $controller('ListingController', {
-//       $scope: scope,
-//       $state: state,
-//     });
-//   }));
+    beforeEach(inject(function($rootScope, $controller) {
+      scope = $rootScope.$new();
+      $controller('ListingController', {
+        $scope: scope,
+        $state: state,
+      });
+    }));
 
-//   describe('$scope.listings', function() {
-//     it('populates scope with array of listings', function() {
-//       expect(scope.listings).toEqual(fakeListings);
-//     });
-//   });
+    describe('$scope.listings', function() {
+      it('populates scope with array of listings', function() {
+        expect(scope.listings).toEqual(fakeListings);
+      });
+    });
 
-// });
+    describe('$scope.listings', function() {
+      it('populates scope with a single listing', function() {
+        expect(scope.listing).toEqual(fakeListing);
+      });
+    });
+
+  });
+}());
