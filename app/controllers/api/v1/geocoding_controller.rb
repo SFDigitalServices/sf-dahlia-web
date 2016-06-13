67fa834a5a9231a964ca63c312a2b615e242d267
Dave Kaplan <dave@exygy.com>
2016-06-13 16:41:41 -0700

    [#120474395] - Set up Geocoding service to support city GIS endpoint

---
+++ app/controllers/api/v1/geocoding_controller.rb
@@ -0,0 +1,16 @@
+# RESTful JSON API to query for address geocoding
+class Api::V1::GeocodingController < ApiController
+  def geocode
+    puts address_params
+    service = GeocodingService.new(address_params)
+    # could be nil if no results found
+    @geocoding_data = service.geocode
+    render json: { geocoding_data: @geocoding_data }
+  end
+
+  private
+
+  def address_params
+    params.require(:address).permit(:address1, :city, :zip)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,8 @@ Rails.application.routes.draw do
 
       # address validation
       post 'validate-address' => 'address_validation#validate'
+      # address geocoding
+      post 'geocode-address' => 'geocoding#geocode'
     end
   end
 
