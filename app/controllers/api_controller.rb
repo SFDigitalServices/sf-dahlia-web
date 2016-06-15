3a9b898b29356c9fe0b83937bd2066b33df1aa58
Dave Kaplan <dave@exygy.com>
2016-06-15 09:58:54 -0700

    [#113145571] - More heroku setup for db + postgres

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -2,9 +2,4 @@
 class ApiController < ActionController::API
   include DeviseTokenAuth::Concerns::SetUserByToken
   respond_to :json
-
-  before_action do
-    logger.debug '***********'
-    logger.debug current_user.inspect
-  end
 end
