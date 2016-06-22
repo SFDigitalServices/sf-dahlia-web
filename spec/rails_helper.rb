ff41e2e3727b17cb35e003ef6e0f3d63dc61a2b0
Dave Kaplan <dave@exygy.com>
2016-06-22 10:38:36 -0700

    [#12072532] - updated RSpec tests and added DatabaseCleaner

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -27,7 +27,7 @@ require 'rspec/rails'
 
 # Checks for pending migrations before tests are run.
 # If you are not using ActiveRecord, you can remove this line.
-# ActiveRecord::Migration.maintain_test_schema!
+ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
@@ -36,7 +36,7 @@ RSpec.configure do |config|
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
-  config.use_transactional_fixtures = false
+  config.use_transactional_fixtures = true
 
   # RSpec Rails can automatically mix in different behaviours to your tests
   # based on their file location, for example enabling you to call `get` and
