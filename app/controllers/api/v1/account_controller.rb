fe64abbabcd693d8f51d7ba1e45ac0473543ff5d
Dave Kaplan <dave@exygy.com>
2016-07-28 09:48:49 -0700

    [#120248083] - refactored account_controller.rb

--- app/controllers/api/v1/account_controller.rb
+++ app/controllers/api/v1/account_controller.rb
@@ -3,12 +3,7 @@ class Api::V1::AccountController < ApiController
   before_action :authenticate_user!
 
   def my_applications
-    applications = current_user_applications
-    listing_ids = applications.collect { |a| a['listingID'] }.uniq
-    listings = ListingService.listings(listing_ids.join(','))
-    applications.each do |app|
-      app['listing'] = listings.find { |l| l['listingID'] == app['listingID'] }
-    end
+    applications = map_listings_to_applications(current_user_applications)
     render json: { applications: applications }
   end
 
@@ -16,5 +11,13 @@ class Api::V1::AccountController < ApiController
 
   def current_user_applications
     ShortFormService.get_for_user(current_user.salesforce_contact_id)
+  end
+
+  def map_listings_to_applications(applications)
+    listing_ids = applications.collect { |a| a['listingID'] }.uniq
+    listings = ListingService.listings(listing_ids.join(','))
+    applications.each do |app|
+      app['listing'] = listings.find { |l| l['listingID'] == app['listingID'] }
+    end
   end
 end
