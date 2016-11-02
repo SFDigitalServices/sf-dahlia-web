9b45470988936d2ca7fce4e68ae7a371fe51fa70
Dave Kaplan <kaptron@users.noreply.github.com>
2016-11-01 18:41:30 -0700

    Bugs/assorted bugs (#512)
    * [#133534785] - only send uploadedfiles for current listing when submitting app
    
    * [bugfixes] - fixing various found bugs w/ navigation and file uploads

--- app/controllers/api/v1/short_form_controller.rb
+++ app/controllers/api/v1/short_form_controller.rb
@@ -138,7 +138,10 @@ class Api::V1::ShortFormController < ApiController
 
   def send_attached_files(application_id)
     if user_signed_in?
-      files = UploadedFile.where(user_id: current_user.id)
+      files = UploadedFile.where(
+        user_id: current_user.id,
+        listing_id: application_params[:listingID],
+      )
     else
       files = UploadedFile.where(uploaded_file_params)
     end
