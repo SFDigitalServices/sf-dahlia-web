0b21f3dfcc36fbe2b45332813812915016f827ef
Dave Kaplan <dave@exygy.com>
2016-12-06 00:22:25 -0800

    [#126825353] - fixed specs

--- spec/support/devise_macros.rb
+++ spec/support/devise_macros.rb
@@ -2,7 +2,8 @@
 module DeviseMacros
   def login_user
     before(:each) do
-      user = User.first || FactoryGirl.create(:user)
+      qa_id = '0036C000001sI5oQAE'
+      user = User.first || FactoryGirl.create(:user, salesforce_contact_id: qa_id)
       token = user.create_new_auth_token
       @auth_headers = { Accept: 'application/json' }.merge(token)
     end
