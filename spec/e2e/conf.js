a5deecbbfa463e1fa8032507a1e822ff0c403783
Christine Dolendo <chris.dolendo@gmail.com>
2017-01-03 16:28:00 -0800

    [#134472139] - WIP - CI without saucelabs

--- spec/e2e/conf.js
+++ spec/e2e/conf.js
@@ -1,16 +1,7 @@
 require('coffee-script').register()
-require('dotenv').config()
-
-var baseUrl = 'https://dahlia-qa.herokuapp.com'
-
-if (process.env.PULL_REQUEST_NUMBER) {
-  baseUrl = 'https://dahlia-qa-' + process.env.PULL_REQUEST_NUMBER + '.herokuapp.com'
-}
 
 exports.config = {
- sauceUser: process.env.SAUCE_USERNAME,
- sauceKey: process.env.SAUCE_ACCESS_KEY,
  framework: 'jasmine',
- baseUrl: baseUrl,
+ baseUrl: 'http://localhost:3000/',
  specs: [ 'short-form.coffee' ],
 }
