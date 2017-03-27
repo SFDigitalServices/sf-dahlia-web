2e0b46fcd45bbcde6785d3f762e46d921ad45b30
Dave Kaplan <dave@exygy.com>
2017-03-27 10:30:24 -0700

    [#136132929] Regenerates VCR cassettes for geocoder

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161207011816) do
+ActiveRecord::Schema.define(version: 20170327171854) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,9 +23,10 @@ ActiveRecord::Schema.define(version: 20161207011816) do
     t.string   "listing_id"
     t.jsonb    "member"
     t.jsonb    "applicant"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
     t.string   "listing_name"
+    t.string   "state"
   end
 
   create_table "uploaded_files", force: :cascade do |t|
