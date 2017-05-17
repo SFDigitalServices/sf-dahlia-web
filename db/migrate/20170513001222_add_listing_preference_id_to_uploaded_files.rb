fa1080cbc1b0c6f280a0bced504f94450dbde9b6
Dave Kaplan <dave@exygy.com>
2017-05-17 11:01:06 -0700

    [#143065433] Makes listing_preference_id migration reversible

--- db/migrate/20170513001222_add_listing_preference_id_to_uploaded_files.rb
+++ db/migrate/20170513001222_add_listing_preference_id_to_uploaded_files.rb
@@ -1,6 +1,6 @@
 class AddListingPreferenceIdToUploadedFiles < ActiveRecord::Migration
   def change
     add_column :uploaded_files, :listing_preference_id, :string
-    remove_column :uploaded_files, :preference
+    remove_column :uploaded_files, :preference, :integer
   end
 end
