c0c6743d4aab1144163b8df235ff19c60c0949cd
Dave Kaplan <kaptron@users.noreply.github.com>
2017-05-18 15:34:33 -0700

    MF API Integration: Refactor saving proof of preference file attachments #143065433 (#744)
    * [#143065433] Updates to UploadedFile to support listing_preference_id
    
    * [#143065433] Fixes FileUploadService spec
    
    * [#143065433] Finishes up proof attachment refactor
    
    * [#143065433] Makes listing_preference_id migration reversible

--- app/models/uploaded_file.rb
+++ app/models/uploaded_file.rb
@@ -1,29 +1,8 @@
 # Model for storing temporary uploaded files in the DB
 class UploadedFile < ActiveRecord::Base
-  enum preference: %i(
-    workInSf
-    liveInSf
-    neighborhoodResidence
-    antiDisplacement
-    assistedHousing
-    rentBurden
-  )
   enum rent_burden_type: %i(lease rent)
 
   def descriptive_name
-    "#{preference_name} - #{document_type}#{File.extname(name)}"
-  end
-
-  def preference_name
-    # for neighborhoodResidence we combine NRHP and liveInSf in the filename
-    # because NRHP proof doubles as liveInSf proof
-    if preference.to_s == 'neighborhoodResidence'
-      'NRHP-liveInSf'
-    elsif preference.to_s == 'rentBurden'
-      # NOTE: this won't need to be in the filename after Salesforce attachment refactor
-      "rentBurden-#{rent_burden_type} #{address}"
-    else
-      preference
-    end
+    "#{document_type}#{File.extname(name)}"
   end
 end
