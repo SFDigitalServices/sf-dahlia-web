9790c61efe0aacf124b1e9e116d8ba6cae069947
Dave Kaplan <dave@exygy.com>
2017-05-17 17:22:41 -0700

    [#143065433] Fixes uploaded_file_spec

--- app/models/uploaded_file.rb
+++ app/models/uploaded_file.rb
@@ -3,7 +3,6 @@ class UploadedFile < ActiveRecord::Base
   enum rent_burden_type: %i(lease rent)
 
   def descriptive_name
-    # "#{preference_name} - #{document_type}#{File.extname(name)}"
     "#{document_type}#{File.extname(name)}"
   end
 end
