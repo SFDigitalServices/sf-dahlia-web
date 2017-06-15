d0e8652170404c82dc3f1da282bad31f4500c584
Dave Kaplan <dave@exygy.com>
2017-06-14 22:10:18 -0700

    [#146963669] Adds rspec tests for ShortFormAttachmentJob

--- spec/jobs/short_form_attachment_job_spec.rb
+++ spec/jobs/short_form_attachment_job_spec.rb
@@ -1,5 +1,30 @@
 require 'rails_helper'
 
+# lifted from https://gist.github.com/ChuckJHardy/10f54fc567ba3bd4d6f1
 RSpec.describe ShortFormAttachmentJob, type: :job do
-  pending "add some examples to (or delete) #{__FILE__}"
+  include ActiveJob::TestHelper
+
+  let(:application_id) { 'ABC123' }
+  let(:file) { create(:uploaded_file) }
+
+  subject(:job) { described_class.perform_later(application_id, file.id) }
+
+  it 'queues the job' do
+    expect { job }.to have_enqueued_job(described_class)
+      .with(application_id, file.id)
+      .on_queue('default')
+  end
+
+  it 'executes perform' do
+    args = [application_id, file, file.descriptive_name]
+    expect(ShortFormService).to receive(:attach_file).with(*args)
+    perform_enqueued_jobs { job }
+    # check that file has now been deleted, we are unable to find it in the DB
+    expect { file.reload }.to raise_error(ActiveRecord::RecordNotFound)
+  end
+
+  after do
+    clear_enqueued_jobs
+    clear_performed_jobs
+  end
 end
