539d0eef5815b62c1b2eb045fccb9b3eecb3dd73
Dave Kaplan <dave@exygy.com>
2017-06-14 22:10:18 -0700

    [#146963669] Adds rspec tests for ShortFormAttachmentJob

--- app/jobs/short_form_attachment_job.rb
+++ app/jobs/short_form_attachment_job.rb
@@ -2,13 +2,14 @@
 class ShortFormAttachmentJob < ActiveJob::Base
   queue_as :default
 
+  rescue_from(ActiveRecord::RecordNotFound) do |e|
+    # catch UploadedFile not being found
+  end
+
   def perform(application_id, file_id)
     file = UploadedFile.find(file_id)
-    puts ">> UPLOADING #{application_id}, #{file.descriptive_name}"
     ShortFormService.attach_file(application_id, file, file.descriptive_name)
     # now that file is saved in SF, remove temp uploads
     file.destroy
-  rescue ActiveRecord::RecordNotFound
-    return
   end
 end
