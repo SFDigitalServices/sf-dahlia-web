ce0220e206d897f0b911e3eece46b692a3adfe31
Grant Kinney <grant@exygy.com>
2017-06-29 16:50:11 -0500

    [#147545945] Restore browser specification in protractor config

--- spec/e2e/conf.js
+++ spec/e2e/conf.js
@@ -6,6 +6,9 @@ exports.config = {
   allScriptsTimeout: 20000,
   framework: 'custom',
   frameworkPath: require.resolve('protractor-cucumber-framework'),
+  capabilities: {
+    'browserName': 'chrome'
+  },
   baseUrl: 'http://localhost:3000/',
   // path relative to the current config file
   specs: [ './features/**/*.feature' ],
