200355879031f45977cce1d16a8c52e5b5afd1d3
Dave Kaplan <dave@exygy.com>
2017-07-28 18:14:04 -0700

    [#147236379] Set up asset helper in ApplicationHelper, pulled out of js

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,19 @@
 # View helpers that should be available application-wide.
 module ApplicationHelper
+  def image_asset_paths
+    asset_paths = {}
+    if Rails.env.development?
+      Rails.application.assets.each_file do |f|
+        next if !f.include?('images/') || f.include?('favicon')
+        filename = Pathname(f).basename.to_s
+        asset_paths[filename] = asset_path(filename)
+      end
+    else
+      Rails.application.assets_manifest.assets.each do |f, fullpath|
+        next if !f.match(/jpg|png|svg/) || f.match(/favicon|apple\-icon|android\-icon/)
+        asset_paths[f] = fullpath
+      end
+    end
+    asset_paths
+  end
 end
