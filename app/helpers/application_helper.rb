637514661134c4de8d11ccc57ea78b892f0dc1d9
Dave Kaplan <dave@exygy.com>
2017-08-01 12:54:34 -0700

    [#144250921] Refactors static_asset_paths helper function

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,19 +1,31 @@
 # View helpers that should be available application-wide.
 module ApplicationHelper
   def static_asset_paths
-    asset_paths = {}
     if Rails.env.development?
-      Rails.application.assets.each_file do |f|
-        next if !f.match(%r{images/|json/}) || f.match(%r{favicon/})
-        filename = Pathname(f).basename.to_s
-        asset_paths[filename] = asset_path(filename)
-      end
+      dev_asset_paths
     else
-      Rails.application.assets_manifest.assets.each do |f, hashpath|
-        next if !f.match(/jpg|png|svg|json/) ||
-                f.match(/favicon|apple\-icon|android\-icon/)
-        asset_paths[f] = "/assets/#{hashpath}"
-      end
+      prod_asset_paths
+    end
+  end
+
+  private
+
+  def dev_asset_paths
+    asset_paths = {}
+    Rails.application.assets.each_file do |f|
+      next if f !~ %r{images/|json/} || f =~ %r{favicon/}
+      filename = Pathname(f).basename.to_s
+      asset_paths[filename] = asset_path(filename)
+    end
+    asset_paths
+  end
+
+  def prod_asset_paths
+    asset_paths = {}
+    Rails.application.assets_manifest.assets.each do |f, hashpath|
+      next if f !~ /jpg|png|svg|json/ || f =~ /favicon|apple\-icon|android\-icon/
+      f = f.gsub('translations/', '') if f =~ %r{translations/}
+      asset_paths[f] = "/assets/#{hashpath}"
     end
     asset_paths
   end
