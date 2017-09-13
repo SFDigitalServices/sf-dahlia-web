7deedd53b9a85696fb681c1062f5261a2587748f
Dave Kaplan <dave@exygy.com>
2017-09-12 23:02:34 -0700

    [translations] Latest updates + refactoring to make translations play nicely

--- Gruntfile.js
+++ Gruntfile.js
@@ -65,7 +65,8 @@ module.exports = function(grunt) {
          'translated-error="([A-Z\.\-\_]*)"',
          'translated-description="([A-Z\.\-\_]*)"',
          'translated-short-description="([A-Z\.\-\_]*)"',
-         'translatedLabel: \'([A-Z\.\-\_]*)\''
+         'translatedLabel: \'([A-Z\.\-\_]*)\'',
+         /\', t\(\'([A-Z\.\-\_]*)\'\)/
        ],
       namespace: true,
       lang:     ['locale-en'],
