0388557f0a259832f03a7316fca211a1eb1b025e
Dave Kaplan <dave@exygy.com>
2017-10-17 09:46:24 -0700

    [#151425961] Adds test for UploadedFile.create_and_resize

--- spec/models/uploaded_file_spec.rb
+++ spec/models/uploaded_file_spec.rb
@@ -15,4 +15,24 @@ describe UploadedFile, type: :model do
     file = create(:uploaded_file, attrs)
     expect(file.descriptive_name).to eql('gas bill.png')
   end
+
+  it 'should be able to create and resize the image' do
+    tempfile = double('tempfile', path: '/x/y/z.jpg')
+    file = double('file', content_type: 'jpg', tempfile: tempfile)
+    attrs = {
+      file: file,
+      document_type: 'copy of lease',
+      content_type: 'jpg',
+      name: 'imagename.jpg',
+    }
+
+    fake_image_optimizer = double('fake_image_optimizer', optimize: {})
+    allow(ImageOptimizer).to receive(:new).and_return(fake_image_optimizer)
+
+    allow(File).to receive(:read).and_return({})
+    allow(File).to receive(:basename).and_return('imagename')
+
+    file = UploadedFile.create_and_resize(attrs)
+    expect(file.descriptive_name).to eql('copy of lease.jpg')
+  end
 end
