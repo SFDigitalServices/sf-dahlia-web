bbf373ff16ecd32dc274b45553b44906f19cb129
Grant Kinney <grant@exygy.com>
2018-03-27 12:27:24 -0500

    [#156284730] Fixes cache service so that changed listings are refreshed
    Also adds tests for CacheService#prefetch_listings.

--- app/services/cache_service.rb
+++ app/services/cache_service.rb
@@ -1,23 +1,24 @@
 # service class for pre-fetching + caching salesforce data
 class CacheService
   def prefetch_listings(opts = {})
-    if opts[:refresh_all]
-      # on daily run, don't grab old listings for comparison
-      # to force cache write for all listings
-      @old_listings = []
-    else
-      # grab previous cached result
-      @old_listings = Force::ListingService.raw_listings
-    end
-    # refresh oauth_token before beginnning
+    # refresh oauth_token before beginning
     Force::Request.new.refresh_oauth_token
+    @old_listings = Force::ListingService.raw_listings
     @new_listings = Force::ListingService.raw_listings(refresh_cache: true)
-    check_listings
+    if opts[:refresh_all]
+      cache_listings
+    else
+      check_listings
+    end
   end
 
   private
 
   attr_accessor :old_listings, :new_listings
+
+  def cache_listings
+    new_listings.each { |listing| cache_single_listing(listing) }
+  end
 
   def check_listings
     new_listings.each do |listing|
@@ -32,11 +33,26 @@ class CacheService
         # That's why we more aggressively re-cache open listings.
         unchanged = HashDiff.diff(sorted_old_listing, sorted_listing).empty?
       end
-      maybe_cache_listing(listing) unless unchanged
+      cache_single_listing(listing) unless unchanged
     end
   end
 
-  def maybe_cache_listing(listing, unchanged)
+  def cache_single_listing(listing)
+    id = listing['Id']
+    # cache this listing from API
+    Force::ListingService.listing(id, force: true)
+    Force::ListingService.units(id, force: true)
+    Force::ListingService.preferences(id, force: true)
+    Force::ListingService.lottery_buckets(id, force: true) if listing_closed?(listing)
+    # NOTE: there is no call to Force::ListingService.ami
+    # because it is parameter-based and values will rarely change (1x/year?)
+    image_processor = ListingImageService.new(listing).process_image
+    Rails.logger.error image_processor.errors.join(',') if image_processor.errors.present?
+  rescue Faraday::ClientError => e
+    Raven.capture_exception(e, tags: { 'listing_id' => listing['Id'] })
+  end
+
+  def listing_closed?(listing)
     begin
       due_date_passed = Date.parse(listing['Application_Due_Date']) < Date.today
     rescue ArgumentError => e
@@ -44,23 +60,6 @@ class CacheService
       # if date is invalid, assume we do need to get lottery results
       due_date_passed = true
     end
-    # move on if there is no difference between the old and new listing object
-    # but always refresh open listings
-    cache_single_listing(listing, due_date_passed) unless unchanged && due_date_passed
-  end
-
-  def cache_single_listing(listing, due_date_passed = true)
-    id = listing['Id']
-    # cache this listing from API
-    Force::ListingService.listing(id)
-    Force::ListingService.units(id)
-    Force::ListingService.preferences(id)
-    Force::ListingService.lottery_buckets(id) if due_date_passed
-    # NOTE: there is no call to Force::ListingService.ami
-    # because it is parameter-based and values will rarely change (1x/year?)
-    image_processor = ListingImageService.new(listing).process_image
-    Rails.logger.error image_processor.errors.join(',') if image_processor.errors.present?
-  rescue Faraday::ClientError => e
-    Raven.capture_exception(e, tags: { 'listing_id' => listing['Id'] })
+    due_date_passed
   end
 end
