8e5ed9777102315945d3fcb7475b5ea77af832c1
Andrea Egan <aegan526@gmail.com>
2019-02-21 16:17:50 -0800

    [#164157589] Add disallows to robots.txt for ownership

--- app/controllers/robots_txts_controller.rb
+++ app/controllers/robots_txts_controller.rb
@@ -6,6 +6,9 @@ class RobotsTxtsController < ApplicationController
   def show
     if disallow_all_crawlers?
       render 'disallow_all', content_type: 'text/plain'
+    # TODO: Remove this once we are done with Ownership MVP
+    elsif disallow_ownership_page?
+      render 'disallow_ownership', content_type: 'text/plain'
     else
       render 'allow', content_type: 'text/plain'
     end
@@ -15,5 +18,9 @@ class RobotsTxtsController < ApplicationController
 
   def disallow_all_crawlers?
     ENV['DISALLOW_ALL_WEB_CRAWLERS'].present?
+  end
+
+  def disallow_ownership_page?
+    !ENV['SHOW_SALE_LISTINGS']
   end
 end
