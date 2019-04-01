330df57745045dd9e3d5e6e58c17f680d00fb16b
Marcin Jedras <mjjedras@gmail.com>
2019-04-02 01:40:34 +0200

    [#165028596] Add test for password update

--- spec/controllers/overrides/passwords_controller_spec.rb
+++ spec/controllers/overrides/passwords_controller_spec.rb
@@ -35,4 +35,25 @@ describe Overrides::PasswordsController do
       expect(response.status).to eq 302
     end
   end
+
+  describe '#update' do
+    before(:each) do
+      user.update(allow_password_change: true)
+
+      # Mocking for token authentication
+      allow(controller).to receive(:update_auth_header).and_return(true)
+      allow(controller).to receive(:set_user_by_token).and_return(user)
+      allow(controller).to receive(:set_request_start).and_return(true)
+      controller.instance_variable_set(:@resource, user)
+    end
+
+    it 'should update password' do
+      expect(controller).to receive(:render_update_success).and_return(true)
+      expect do
+        put :update, password: 'newpassword', password_confirmation: 'newpassword'
+      end.to(change { user.encrypted_password })
+
+      expect(response.status).to eq 200
+    end
+  end
 end
