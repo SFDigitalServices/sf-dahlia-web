9e044c8f9d0f8a925f0ad8638286eedb1868eb24
Andrea Egan <aegan526@gmail.com>
2019-07-01 16:04:44 -0700

    [#165863029] Fix failing vcr tests

--- spec/requests/api/v1/listings_spec.rb
+++ spec/requests/api/v1/listings_spec.rb
@@ -10,7 +10,7 @@ describe 'Listings API' do
   describe 'index' do
     save_fixture do
       VCR.use_cassette('listings/rental_listings') do
-        get '/api/v1/listings.json', type: 'rental', subset: 'browse'
+        get '/api/v1/listings.json', type: 'rental'
       end
     end
   end
@@ -47,7 +47,6 @@ describe 'Listings API' do
           householdsize: 2,
           incomelevel: 20_000,
           childrenUnder6: 1,
-          subset: 'browse',
         }
         get '/api/v1/listings/eligibility.json', params
       end
@@ -114,7 +113,6 @@ describe 'Listings API' do
         incomelevel: 20_000,
         childrenUnder6: 1,
         listingsType: 'rental',
-        subset: 'browse',
       }
       get '/api/v1/listings/eligibility.json', params
     end
