923f0c68ed675d7dec7210c7f3abbc66a4f18b9d
chrisdolendo <chris.dolendo@gmail.com>
2020-02-12 07:23:36 -0800

    [#163066582] - Increase timeouts for whole protractor system

--- spec/e2e/conf.js
+++ spec/e2e/conf.js
@@ -3,6 +3,8 @@ var config = require('./conf-shared')
 config.baseUrl = 'http://localhost:3000/'
 config.capabilities = {
   browserName: 'chrome',
+  getPageTimeout: 30000,
+  allScriptsTimeout: 60000,
   chromeOptions: {
     // tall, skinny window. trying to avoid errors where it says button is not visible/clickable
      args: [ '--window-size=800,1200' ]
