7208c43f7f1d5cbe695d03bdd2847a0f622ee036
Andrea Egan <akegan@users.noreply.github.com>
2020-06-18 10:46:47 -0700

    Update refresh script #172460468 (#1355)
    * Update refresh script to circleCI, pull in Heroku app names automatically
    
    * Strip quotes from app names, add full vs qa flags
    
    * Fix circleCI requests

--- refresh.sh
+++ refresh.sh
@@ -7,32 +7,31 @@
 
 # To use this script:
 #   1. Put the updated env vars in a file (.env generally)
-#   2. Add names of apps to update under webapp_apps
-#.  3. If you don't already have it installed, install the [Semaphore CLI](https://semaphoreci.com/docs/cli-overview.html) and login
+#   2. If you don't already have it installed, run `brew install jq` to install the jq tool
+#   3. Get a circleCI token by going to https://app.circleci.com/settings/user/tokens and adding a personal API token.
 #   3. Run the script, passing your path to env vars as an argument
 
-# TODO: Pull in heroku apps from files or at least review app numbers command line arg
+# TODO: add a qa flag to allow updates for QA.
 
 # Argument defaults
 env_file=".env"
-semaphore_secret="sf-dahlia-web-full"
 
-while getopts ":f::s::h" opt; do
+while getopts ":h::e::c:" opt; do
   case $opt in
     h )
       echo "Usage:"
       echo "    refresh.sh -h                           Display this help message."
-      echo "    refresh.sh -f <environment file>        Specify an environment file to load from, defaults to .env."
-      echo "    refresh.sh -s <semaphore secret name>   Specify a Semaphore secret name to update, defaults to sf-dahlia-web-full."
+      echo "    refresh.sh -e <environment>             Specify an environment to update, either full or qa."
+      echo "    refresh.sh -c <circle ci token>         Provide a CircleCI token."
       exit 0
       ;;
-    f )
-      env_file=$OPTARG
+    e )
+      env=$OPTARG
       ;;
-    s )
-      semaphore_secret=$OPTARG
+    c )
+      circle_ci_token=$OPTARG
       ;;
-    \? ) echo "Usage: cmd [-h] [-f]"
+    \? ) echo "Usage: cmd [-h] [-e] [-c]"
       ;;
   esac
 done
@@ -44,17 +43,24 @@ echo "loaded SALESFORCE_CLIENT_SECRET=$SALESFORCE_CLIENT_SECRET"
 echo "loaded SALESFORCE_CLIENT_ID=$SALESFORCE_CLIENT_ID"
 echo "loaded SALESFORCE_INSTANCE_URL=$SALESFORCE_INSTANCE_URL"
 
-echo "Starting Heroku credential update for Webapp"
-# Get these app names from running "heroku apps"
-declare -a webapp_apps=(
-  "dahlia-full"
-  "dahlia-qa-translations"
-  "dahlia-full-pr-1268"
-  "dahlia-full-pr-1272"
-)
+echo "Starting Heroku credential update for Webapp $env"
 
-for app in "${webapp_apps[@]}"
+if [ $env == "full" ]; then
+  # Get all apps that are dahlia-web-full apps.
+  heroku_apps=$(heroku apps --team=sfdigitalservices --json | jq '.[].name | select(test("dahlia-web-full-*"))')
+  heroku_apps=("dahlia-full" ${heroku_apps[@]})
+elif [ $env == "qa" ]; then
+  heroku_apps=('dahlia-qa')
+else
+  echo "Error: environment must be full or qa"
+  exit 1
+fi
+
+
+for app in ${heroku_apps[@]}
   do
+    # Strip out double quotes from app names
+    app=$(echo "$app" | tr -d '"')
     echo "Updating credentials for $app"
     heroku config:set SALESFORCE_PASSWORD=$SALESFORCE_PASSWORD --app $app
     heroku config:set SALESFORCE_SECURITY_TOKEN=$SALESFORCE_SECURITY_TOKEN --app $app
@@ -66,19 +72,23 @@ done
 
 echo "Heroku update complete"
 
+if [ $env == "full" ]; then
+  echo "Starting CircleCI credential update"
+  BASE_CIRCLECI_URL="https://circleci.com/api/v1.1/project/github/SFDigitalServices/sf-dahlia-web/envvar"
 
-echo "Starting Semaphore credential update for $semaphore_secret"
+  # Delete existing env vars
+  curl -X DELETE $BASE_CIRCLECI_URL/SALESFORCE_PASSWORD?circle-token=$circle_ci_token
+  curl -X DELETE $BASE_CIRCLECI_URL/SALESFORCE_SECURITY_TOKEN?circle-token=$circle_ci_token
+  curl -X DELETE $BASE_CIRCLECI_URL/SALESFORCE_CLIENT_SECRET?circle-token=$circle_ci_token
+  curl -X DELETE $BASE_CIRCLECI_URL/SALESFORCE_CLIENT_ID?circle-token=$circle_ci_token
+  curl -X DELETE $BASE_CIRCLECI_URL/SALESFORCE_INSTANCE_URL?circle-token=$circle_ci_token
 
-sem secrets:env-vars:remove exygy/$semaphore_secret  --name SALESFORCE_PASSWORD
-sem secrets:env-vars:remove exygy/$semaphore_secret  --name SALESFORCE_SECURITY_TOKEN
-sem secrets:env-vars:remove exygy/$semaphore_secret  --name SALESFORCE_CLIENT_SECRET
-sem secrets:env-vars:remove exygy/$semaphore_secret  --name SALESFORCE_CLIENT_ID
-sem secrets:env-vars:remove exygy/$semaphore_secret  --name SALESFORCE_INSTANCE_URL
+  # Create new env vars
+  curl -X POST --header "Content-Type: application/json" -d "{\"name\": \"SALESFORCE_PASSWORD\", \"value\": \"$SALESFORCE_PASSWORD\"}" $BASE_CIRCLECI_URL?circle-token=$circle_ci_token
+  curl -X POST --header "Content-Type: application/json" -d "{\"name\": \"SALESFORCE_SECURITY_TOKEN\", \"value\": \"$SALESFORCE_SECURITY_TOKEN\"}" $BASE_CIRCLECI_URL?circle-token=$circle_ci_token
+  curl -X POST --header "Content-Type: application/json" -d "{\"name\": \"SALESFORCE_CLIENT_SECRET\", \"value\": \"$SALESFORCE_CLIENT_SECRET\"}" $BASE_CIRCLECI_URL?circle-token=$circle_ci_token
+  curl -X POST --header "Content-Type: application/json" -d "{\"name\": \"SALESFORCE_CLIENT_ID\", \"value\": \"$SALESFORCE_CLIENT_ID\"}" $BASE_CIRCLECI_URL?circle-token=$circle_ci_token
+  curl -X POST --header "Content-Type: application/json" -d "{\"name\": \"SALESFORCE_INSTANCE_URL\", \"value\": \"$SALESFORCE_INSTANCE_URL\"}" $BASE_CIRCLECI_URL?circle-token=$circle_ci_token
 
-sem secrets:env-vars:add exygy/$semaphore_secret  --name SALESFORCE_PASSWORD --content "$SALESFORCE_PASSWORD"
-sem secrets:env-vars:add exygy/$semaphore_secret  --name SALESFORCE_SECURITY_TOKEN --content "$SALESFORCE_SECURITY_TOKEN"
-sem secrets:env-vars:add exygy/$semaphore_secret  --name SALESFORCE_CLIENT_SECRET --content "$SALESFORCE_CLIENT_SECRET"
-sem secrets:env-vars:add exygy/$semaphore_secret  --name SALESFORCE_CLIENT_ID --content "$SALESFORCE_CLIENT_ID"
-sem secrets:env-vars:add exygy/$semaphore_secret  --name SALESFORCE_INSTANCE_URL --content "$SALESFORCE_INSTANCE_URL"
-
-echo "Credentials updated for Semaphore"
+  echo "Credentials updated for CircleCI"
+fi
