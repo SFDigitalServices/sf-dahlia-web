ed9051ba3e844acbebe9f2c765ba027f60b8c9a3
James Wills <64036574+james-wills-sf@users.noreply.github.com>
2020-07-01 15:44:43 -0700

    Upgrade devise_token_auth to fix emails #173504864 (#1365)
    * Upgrade devise_token_auth to fix emails #173504864
    
    devise_token_auth overrides some methods that devise uses to send
    emails. See this issue: https://github.com/heartcombo/devise/issues/3654
    for more information.
    
    It was fixed by this pr:
    https://github.com/lynndylanhurley/devise_token_auth/pull/1343
    
    In release 1.1.4 of devise_token_auth. This change updates that library
    to include the fix in webapp.
    
    Additionally, the devise library appears to have an issue in 4.7.2, so
    I explicitly downgraded us to 4.7.1.
    
    We are able to get rid of the custom email triggering code with this
    fix.
    
    * Add rspec test to verify email change reconfirmation is sent
    
    Co-authored-by: Andrea Egan <andrea.egan@sfgov.org>

--- spec/controllers/overrides/registrations_controller_spec.rb
+++ spec/controllers/overrides/registrations_controller_spec.rb
@@ -1,49 +1,80 @@
 require 'rails_helper'
 
 describe Overrides::RegistrationsController do
+  let(:salesforce_response) do
+    {
+      firstName: 'Test',
+      lastName: 'lastName',
+      email: 'test@test.com',
+      DOB: '1989-03-29',
+      contactId: '0036C000001sI5oQAE',
+    }.as_json
+  end
+
+  let(:valid_user_params) do
+    {
+      user: {
+        id: 1,
+        email: 'jane@doe.com',
+        password: 'somepassword',
+        password_confirmation: 'somepassword',
+      },
+      contact: {
+        firstName: 'Jane',
+        lastName: 'Doe',
+        DOB: '1985-07-23',
+        email: 'jane@doe.com',
+      },
+      confirm_success_url: 'http://localhost/my-account',
+    }
+  end
+
   before(:each) do
     @request.env['devise.mapping'] = Devise.mappings[:user]
   end
 
   describe 'create' do
-    let(:valid_user_params) do
-      {
-        user: {
-          email: 'jane@doe.com',
-          password: 'somepassword',
-          password_confirmation: 'somepassword',
-        },
-        contact: {
-          firstName: 'Jane',
-          lastName: 'Doe',
-          DOB: '1985-07-23',
-          email: 'jane@doe.com',
-        },
-        confirm_success_url: 'http://localhost/my-account',
-      }
-    end
-
-    let(:salesforce_response) do
-      {
-        firstName: 'Test',
-        lastName: 'lastName',
-        email: 'test@test.com',
-        DOB: '1989-03-29',
-        contactId: '0036C000001sI5oQAE',
-      }.as_json
-    end
-
     it 'saves a salesforce contact id on user' do
       allow(Force::AccountService)
         .to receive(:create_or_update)
         .and_return(salesforce_response)
 
-      VCR.use_cassette('account/register') do
-        post :create, params: valid_user_params
-      end
+      post :create, params: valid_user_params
 
       expect(assigns(:resource).salesforce_contact_id)
         .to eq('0036C000001sI5oQAE')
+    end
+  end
+
+  describe '#update' do
+    # We no longer override the update method, but we want to confirm that email
+    # change confirmation emails are being sent.
+    let(:user_update_params) do
+      {
+        user: {
+          id: 1,
+          email: 'jane2@doe.com',
+        },
+      }
+    end
+
+    it 'sends a reconfirmation email when email address is updated' do
+      allow(Force::AccountService)
+        .to receive(:create_or_update)
+        .and_return(salesforce_response)
+
+      message_delivery = instance_double(ActionMailer::MessageDelivery)
+      # Expect 2 emails, once for original confirmation, and once for email change.
+      expect(Emailer)
+        .to receive(:confirmation_instructions)
+        .twice
+        .and_return(message_delivery)
+      allow(message_delivery).to receive(:deliver_later)
+      # First, create the valid user
+      post :create, params: valid_user_params
+      @resource = assigns(:resource)
+      # Then update the user
+      put :update, params: user_update_params
     end
   end
 end
