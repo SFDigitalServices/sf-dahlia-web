93a2ae27763f62a0e076271cc780f49ce88bed6b
Andrea Egan <andrea.egan@sfgov.org>
2020-09-08 14:28:14 -0700

    Remove serverBaseUrl and assertions

--- lighthouserc.js
+++ lighthouserc.js
@@ -7,7 +7,6 @@ try {
 const {
   LHCI_BUILD_TOKEN: token,
   LHCI_COLLECT_BASE_URL: baseUrl = 'http://localhost:3000',
-  LHCI_SERVER_BASE_URL: serverBaseUrl
 } = process.env
 
 module.exports = {
@@ -17,15 +16,9 @@ module.exports = {
       staticDistDir: false
     },
     assert: {
-      assertions: {
-        'categories:accessibility': [
-          'error', { minScore: 0.90 }
-        ]
-      }
     },
     upload: {
       target: 'lhci',
-      serverBaseUrl,
       token,
       ignoreDuplicateBuildFailure: true,
       urlReplacementPatterns: [
