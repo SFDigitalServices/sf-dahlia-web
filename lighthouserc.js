574da8d7d2bbd1951c050e6e60105f02cb832a39
Andrea Egan <andrea.egan@sfgov.org>
2020-09-10 13:13:37 -0700

    Use urljoin to join urls to avoid double slashes

--- lighthouserc.js
+++ lighthouserc.js
@@ -4,6 +4,8 @@ try {
   // this can fail in CI, where there is no .env
 }
 
+const join = require('url-join')
+
 const {
   LHCI_BUILD_TOKEN: token,
   LHCI_COLLECT_BASE_URL: baseUrl = 'http://localhost:3000',
@@ -12,10 +14,10 @@ const {
 const testListingId = 'a0W0P00000F8YG4UAN'
 
 const urlsToTest = [
-  `${baseUrl}/`,
-  `${baseUrl}/listings/for-rent`,
-  `${baseUrl}/listings/${testListingId}`,
-  `${baseUrl}/listings/${testListingId}/apply/name`
+  join(baseUrl, '/'),
+  join(baseUrl, '/listings/for-rent'),
+  join(baseUrl, `/listings/${testListingId}`),
+  join(baseUrl, `/listings/${testListingId}/apply/name`)
 ]
 
 module.exports = {
