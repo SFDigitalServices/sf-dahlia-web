87f783b58a4710e354ef0ac4793fac10a33673c2
Marcin Jedras <mjjedras@gmail.com>
2021-02-22 22:07:23 +0100

    Fix translations

--- app/javascript/packs/react_application.tsx
+++ app/javascript/packs/react_application.tsx
@@ -29,4 +29,12 @@ import WebpackerReact from 'webpacker-react'
 
 import index from '../pages/basic'
 
+import * as translation from "@bloom-housing/ui-components/src/locales/general.json"
+import * as customTranslations from "../page_content/locale_overrides/general.json"
+import { addTranslation } from '@bloom-housing/ui-components'
+addTranslation(translation)
+if (customTranslations) {
+  addTranslation(customTranslations)
+}
+
 WebpackerReact.setup({ index }) // ES6 shorthand for {ApplicationEditPage: ApplicationEditPage}
