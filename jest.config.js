1a1850f2f2b73f24b20f6b4105e7974fa6461d56
Marcin Jedras <mjjedras@gmail.com>
2021-03-03 23:36:00 +0100

    Setup jest tests

---
+++ jest.config.js
@@ -0,0 +1,38 @@
+/*eslint no-undef: "error"*/
+/*eslint-env node*/
+
+process.env.TZ = "UTC"
+
+module.exports = {
+  testRegex: "/*.test.tsx$",
+  collectCoverageFrom: ["**/*.tsx"],
+  coverageReporters: ["lcov", "text"],
+  coverageDirectory: "test-coverage",
+  coverageThreshold: {
+    global: {
+      branches: 0,
+      functions: 0,
+      lines: 0,
+      statements: 0,
+    },
+  },
+  preset: "ts-jest",
+  globals: {
+    "ts-jest": {
+      tsconfig: "tsconfig.json",
+    },
+  },
+  rootDir: "./app/javascript",
+  roots: ["<rootDir>/"],
+  transform: {
+    "^.+\\.[t|j]sx?$": "ts-jest",
+  },
+  setupFiles: ["dotenv/config"],
+  setupFilesAfterEnv: ["<rootDir>/.jest/setup-tests.ts"],
+  moduleNameMapper: {
+    "\\.(scss|css|less)$": "identity-obj-proxy",
+  },
+  "transformIgnorePatterns": [
+    "node_modules/?!(@bloom-housing/ui-components)"
+  ]
+}
