401cff4fb97a17dbc1c79490c2ab3d8d06dd6575
James Wills <james.wills@sfgov.org>
2021-03-23 09:01:01 -0700

    update controller comments

--- app/controllers/angular_controller.rb
+++ app/controllers/angular_controller.rb
@@ -1,11 +1,7 @@
-# Handles static pages
-# Currently all pages except for the homepage use AngularController,
-# as pages get migrated to react we should do the following:
-# 1. Add a new controller for the new page
-#   a. Implement use_react_app for the new controller
-# 2. Add a new route for that page in routes.rb
+# Renders the angular single-page app. All routing is handled
+# on the client.
+# By default, all pages render with AngularController,
+# the README contains instructions for how to migrate a new page
+# to render via react instead.
 class AngularController < ApplicationController
-  def use_react_app
-    false
-  end
 end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,4 @@
-# Handles static pages
+# Controller for the DAHLIA homepage.
 class HomeController < ApplicationController
   def use_react_app
     ENV['HOME_PAGE_REACT'].to_s.casecmp('true').zero?
