b04263c9c453dafb751f4d67c21b9f202fd3f20f
James Wills <james.wills@sfgov.org>
2021-03-24 11:46:51 -0700

    remove testing-specific files

--- cypress/integration/navigation.ts
+++
@@ -1,8 +0,0 @@
-describe("Navigating around the site", () => {
-  it("Loads the homepage directly", () => {
-    cy.visit("/")
-
-    // Check that the homepage banner text is present on the page
-    cy.contains("Apply for affordable housing")
-  })
-})
