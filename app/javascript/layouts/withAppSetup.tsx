d189b50ec79f5a06219380ff57f63ff2ee4fd732
James Wills <james.wills@sfgov.org>
2021-04-23 16:05:55 -0700

    rename file to fix typo

--- app/javascript/layouts/withAppSetup.tsx
+++ app/javascript/layouts/withAppSetup.tsx
@@ -1,6 +1,6 @@
 import React from "react"
 
-import IdleTimeout from "../authentication/components/IdeTimeout"
+import IdleTimeout from "../authentication/components/IdleTimeout"
 import UserProvider from "../authentication/context/UserProvider"
 
 // Ignore linting error on 'object' type, because we can't use Record<string, unknown> here.
