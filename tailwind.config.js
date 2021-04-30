5b1f8dc7771a973b6b2ee593615ca10c387e4f39
Anthony Kong <anthony.kong@sfgov.org>
2021-04-29 22:18:33 -0700

    DAH-726 - add comment for tailwind purge, only when NODE_ENV set to production

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,8 @@ const bloomTheme = cloneDeep(require("@bloom-housing/ui-components/tailwind.conf
 // For example:
 // bloomTheme.theme.colors.white = "#f0f0e9"
 
+
+// tailwind will automatically purge unused styles when `NODE_ENV` is set to `production`
 bloomTheme.purge = [
   './src/**/*.tsx',
   __dirname + '/app/assets/javascripts/**/*.html.slim',
