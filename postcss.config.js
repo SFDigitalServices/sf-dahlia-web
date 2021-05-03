aac505486b51c22dc736da4994ae5bf39a661a97
Anthony Kong <anthony.kong@sfgov.org>
2021-05-03 12:10:37 -0700

    DAH-726 - minify in production

--- postcss.config.js
+++ postcss.config.js
@@ -1,15 +1,24 @@
+const cssnano = require("cssnano")({preset: "default"})
+let minimize = false
+
+const plugins = [
+  require("postcss-import"),
+  require("tailwindcss")("./tailwind.config.js"),
+  require("postcss-flexbugs-fixes"),
+  require("postcss-preset-env")({
+    autoprefixer: {
+      flexbox: "no-2009"
+    },
+    stage: 3
+  })
+]
+
+if (process.env.NODE_ENV === 'production') {
+  plugins.push(cssnano)
+  minimize = true
+}
+
 module.exports = {
-  plugins: [
-    require("postcss-import"),
-    require("tailwindcss")("./tailwind.config.js"),
-    require("postcss-flexbugs-fixes"),
-    require("postcss-preset-env")({
-      autoprefixer: {
-        flexbox: "no-2009"
-      },
-      stage: 3
-    }),
-    require("cssnano")({preset: "default"})
-  ],
-  minimize: true
+  plugins: plugins,
+  minimize: minimize
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,6 @@ const bloomTheme = cloneDeep(require("@bloom-housing/ui-components/tailwind.conf
 // For example:
 // bloomTheme.theme.colors.white = "#f0f0e9"
 
-
 // tailwind will automatically purge unused styles when `NODE_ENV` is set to `production`
 bloomTheme.purge = [
   './src/**/*.tsx',
