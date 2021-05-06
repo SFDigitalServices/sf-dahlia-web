380f9a1a06b8d34e4f061891c37676f8d3e0b8a4
Marcin Jędras <mjjedras@gmail.com>
2021-05-06 03:20:15 +0200

    Features/rewrite footer [DAH-757] (#1460)
    * Initial footer work [DAH-757]
    
    * Add config context with assets [DAH-757]
    
    Co-authored-by: James Wills <james.wills@sfgov.org>
    Co-authored-by: James Wills <64036574+james-wills-sf@users.noreply.github.com>

--- app/javascript/layouts/withAppSetup.tsx
+++ app/javascript/layouts/withAppSetup.tsx
@@ -2,17 +2,25 @@ import React from "react"
 
 import IdleTimeout from "../authentication/components/IdleTimeout"
 import UserProvider from "../authentication/context/UserProvider"
+import { ConfigProvider } from "../lib/ConfigContext"
+
+interface ObjectWithAssets {
+  assetPaths: unknown
+  [key: string]: unknown
+}
 
 // Ignore linting error on 'object' type, because we can't use Record<string, unknown> here.
 // eslint-disable-next-line @typescript-eslint/ban-types
-const withAppSetup = <P extends object>(
+const withAppSetup = <P extends ObjectWithAssets>(
   Component: React.ComponentType<P>,
   useFormTimeout?: boolean
 ) => (props: P) => (
-  <UserProvider>
-    <IdleTimeout onTimeout={() => console.log("Logout")} useFormTimeout={useFormTimeout} />
-    <Component {...props} />
-  </UserProvider>
+  <ConfigProvider assetPaths={props.assetPaths}>
+    <UserProvider>
+      <IdleTimeout onTimeout={() => console.log("Logout")} useFormTimeout={useFormTimeout} />
+      <Component {...props} />
+    </UserProvider>
+  </ConfigProvider>
 )
 
 export default withAppSetup
