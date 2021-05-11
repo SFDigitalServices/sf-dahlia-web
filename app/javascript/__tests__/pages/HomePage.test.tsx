57de584546e7f4d34361ad3b46a8074a3041076b
Marcin Jedras <mjjedras@gmail.com>
2021-05-11 17:21:55 +0200

    Add MainNav [DAH-748]

--- app/javascript/__tests__/pages/HomePage.test.tsx
+++ app/javascript/__tests__/pages/HomePage.test.tsx
@@ -1,12 +1,24 @@
 import React from "react"
 
+import { cleanup } from "@testing-library/react"
+
 import HomePage from "../../pages/HomePage"
 import { renderAndLoadAsync } from "../__util__/renderUtils"
 
+afterEach(cleanup)
 describe("<HomePage />", () => {
   it("shows the correct header text", async () => {
     const { getByText } = await renderAndLoadAsync(<HomePage assetPaths={{}} />)
 
     expect(getByText("Apply for affordable housing")).not.toBeNull()
+  })
+
+  it("shows the correct footer logo path", async () => {
+    const { getByAltText } = await renderAndLoadAsync(
+      <HomePage assetPaths={{ "logo-city.png": "/public/logo.png" }} />
+    )
+    const sfLogo = getByAltText("City & County of San Francisco Logo")
+    expect(sfLogo).not.toBeNull()
+    expect(sfLogo).toHaveAttribute("src", "/public/logo.png")
   })
 })
