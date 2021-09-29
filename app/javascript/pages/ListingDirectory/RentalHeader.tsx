d4b7363ce8ff80e8eceaafc3725959be2ce9a302
Andrea Egan <andrea.egan@sfgov.org>
2021-09-29 12:19:18 -0700

    First pass of RentalHeader

---
+++ app/javascript/pages/ListingDirectory/RentalHeader.tsx
@@ -0,0 +1,21 @@
+import { LinkButton, PageHeader, t } from "@bloom-housing/ui-components"
+import React from "react"
+import { getEligibilityEstimatorLink, getHelpCalculatingIncomeLink } from "../../util/routeUtil"
+import Markdown from "markdown-to-jsx"
+
+const RentalHeader = () => (
+  <>
+    <PageHeader title={t("rentalDirectory.title")} subtitle={t("rentalDirectory.ifYouTellUs")}>
+      <p className="mt-8 mb-2">
+        <LinkButton href={getEligibilityEstimatorLink()}>
+          {t("rentalDirectory.findMatchingListings")}
+        </LinkButton>
+      </p>
+      <Markdown className="text-sm">
+        {t("rentalDirectory.orGetHelpCalculating", { incomeLink: getHelpCalculatingIncomeLink() })}
+      </Markdown>
+    </PageHeader>
+  </>
+)
+
+export default RentalHeader
