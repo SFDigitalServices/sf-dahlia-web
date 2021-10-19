1d64d92d1a266baff74b511534690e9be4aec48d
Andrea Egan <andrea.egan@sfgov.org>
2021-10-19 11:40:22 -0700

    Add external prop to Link component

--- app/javascript/navigation/Link.tsx
+++ app/javascript/navigation/Link.tsx
@@ -1,16 +1,18 @@
 import React, { AnchorHTMLAttributes, DetailedHTMLProps } from "react"
 import { localizedPath } from "../util/routeUtil"
 
-type LinkProps = DetailedHTMLProps<AnchorHTMLAttributes<HTMLAnchorElement>, HTMLAnchorElement>
+type LinkProps = DetailedHTMLProps<AnchorHTMLAttributes<HTMLAnchorElement>, HTMLAnchorElement> & {
+  external?: boolean
+}
 
 /**
  * Right now this is a very simple link component, but we keep it in
  * its own file so that if we add functionality to it we can do it in one place only.
  */
 const Link = ({ ...props }: LinkProps) => {
-  const localizedLink = localizedPath(props.href)
+  const link = props.external ? props.href : localizedPath(props.href)
   return (
-    <a {...props} href={localizedLink}>
+    <a {...props} href={link}>
       {props.children}
     </a>
   )
