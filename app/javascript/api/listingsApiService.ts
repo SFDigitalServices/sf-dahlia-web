80b8d8854ac7293c8f205db69fa64becd2ecc112
Emily Jablonski <emily.jablonski@exygy.com>
2021-12-09 19:50:03 -0700

    feat: react eligibility data, v2

--- app/javascript/api/listingsApiService.ts
+++ app/javascript/api/listingsApiService.ts
@@ -3,8 +3,48 @@ import RailsRentalListing from "./types/rails/listings/RailsRentalListing"
 
 type ListingsResponse = { listings: RailsRentalListing[] }
 
-export const getRentalListings = async (): Promise<RailsRentalListing[]> =>
-  get<ListingsResponse>("/api/v1/listings.json?type=rental").then(({ data }) => data.listings)
+export type EligibilityFilters = {
+  household_size: string
+  income_timeframe: string
+  income_total: number
+  include_children_under_6: boolean
+  children_under_6: string
+  type: string
+}
+
+export type QueryParams = { [key: string]: string | boolean | number }
+
+const formatQueryString = (params: QueryParams) => {
+  return Object.keys(params)
+    .map((key) => {
+      return encodeURIComponent(key) + "=" + encodeURIComponent(params[key])
+    })
+    .join("&")
+}
+
+export const getRentalListings = async (
+  filters?: EligibilityFilters
+): Promise<RailsRentalListing[]> => {
+  const getIncomeLevel = () => {
+    return filters.income_timeframe === "per_month"
+      ? filters.income_total * 12
+      : filters.income_total
+  }
+
+  const appliedFilters = {
+    householdsize: filters.household_size ?? "",
+    incomelevel: getIncomeLevel() ?? "",
+    includeChildrenUnder6: filters.include_children_under_6 ?? false,
+    childrenUnder6: filters.children_under_6 ?? "",
+    listingsType: "rental",
+  }
+
+  return Object.keys(filters).length > 0
+    ? get<ListingsResponse>(
+        `/api/v1/listings/eligibility.json?${formatQueryString(appliedFilters)}`
+      ).then(({ data }) => data.listings)
+    : get<ListingsResponse>("/api/v1/listings.json?type=rental").then(({ data }) => data.listings)
+}
 
 export const getSaleListings = async (): Promise<RailsRentalListing[]> =>
   get<ListingsResponse>("/api/v1/listings.json?type=ownership").then(({ data }) => data.listings)
