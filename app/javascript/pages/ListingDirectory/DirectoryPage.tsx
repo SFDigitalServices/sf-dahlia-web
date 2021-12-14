86caf5fecdeb0d2eb40b78acff3741aa785d776b
Andrea Egan <akegan@users.noreply.github.com>
2021-12-14 15:45:30 -0800

    Link sale directory action block to correct links DAH-839 (#1549)

--- app/javascript/pages/ListingDirectory/DirectoryPage.tsx
+++ app/javascript/pages/ListingDirectory/DirectoryPage.tsx
@@ -341,10 +341,20 @@ export const DirectoryPage = (props: DirectoryProps) => {
                       background="primary-darker"
                       layout={ActionBlockLayout.inline}
                       actions={[
-                        <Link className="button" key="action-1" href={getAdditionalResourcesPath()}>
+                        <Link
+                          className="button"
+                          key="action-1"
+                          external
+                          href={"https://sfmohcd.org/current-bmr-homeownership-listings"}
+                        >
                           {t("saleDirectory.callout.firstComeFirstServed")}
                         </Link>,
-                        <Link className="button" key="action-2" href={getAdditionalResourcesPath()}>
+                        <Link
+                          className="button"
+                          key="action-2"
+                          external
+                          href={"https://sfmohcd.org/current-listings-city-second-program"}
+                        >
                           {t("saleDirectory.callout.citySecondLoan")}
                         </Link>,
                       ]}
