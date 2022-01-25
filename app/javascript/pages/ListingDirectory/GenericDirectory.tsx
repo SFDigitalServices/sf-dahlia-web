ccb518966429d5d73df29b6007dad8437d8b56b0
Andrea Egan <akegan@users.noreply.github.com>
2022-01-25 15:10:50 -0800

    Fix eligibility calculator styling DAH-997 (#1563)
    * Fix styling issues with eligibility calculator
    
    * Move housing counselor link to No match body
    
    * Run grunt translations to sort translations
    
    * Add translations for updated find a housing counselor content

--- app/javascript/pages/ListingDirectory/GenericDirectory.tsx
+++ app/javascript/pages/ListingDirectory/GenericDirectory.tsx
@@ -14,7 +14,6 @@ import {
   additionalView,
   signUpActionBlock,
   sortListings,
-  housingCounselorActionBlock,
 } from "./DirectoryHelpers"
 import "./ListingDirectory.scss"
 
@@ -28,7 +27,7 @@ interface RentalDirectoryProps {
     setFilters: Dispatch<SetStateAction<EligibilityFilters>>,
     match: boolean
   ) => JSX.Element
-  findMoreActionBlock: (filters: EligibilityFilters, match: boolean) => JSX.Element
+  findMoreActionBlock: () => JSX.Element
 }
 
 export const GenericDirectory = (props: RentalDirectoryProps) => {
@@ -77,9 +76,7 @@ export const GenericDirectory = (props: RentalDirectoryProps) => {
                 hasFiltersSet
               )}
 
-              {props.findMoreActionBlock(filters, match)}
-
-              {filters && housingCounselorActionBlock()}
+              {props.findMoreActionBlock()}
               {filters &&
                 additionalView(
                   listings.additional,
