038bbe42a9ab84fe62942b5d1d063fa0af95dabc
Andrea Egan <andrea.egan@sfgov.org>
2022-02-04 10:45:32 -0800

    Add log for debugging react ie11

--- app/javascript/layouts/Layout.tsx
+++ app/javascript/layouts/Layout.tsx
@@ -120,6 +120,12 @@ const Layout = (props: LayoutProps) => {
   const { profile, signOut } = useContext(UserContext)
 
   // eslint-disable-next-line dot-notation
+  console.log(
+    "IE11 document mode",
+    window.document["documentMode"],
+    "DIRECTORY_PAGE_REACT",
+    process.env.DIRECTORY_PAGE_REACT
+  )
   if (window.document["documentMode"] && process.env.DIRECTORY_PAGE_REACT === "true") {
     window.location.href = "/ie-deprecated.html"
     return
