060b6515ba8ff29607d7d0522bc5590bb85681a6
Emily Jablonski <emily.jablonski@exygy.com>
2022-02-10 12:14:30 -0700

    fix: update import statements

--- app/javascript/__tests__/pages/SignIn.test.tsx
+++ app/javascript/__tests__/pages/SignIn.test.tsx
@@ -1,6 +1,6 @@
 import React from "react"
 
-import SignIn from "../../pages/SignIn"
+import SignIn from "../../pages/sign-in"
 import { renderAndLoadAsync } from "../__util__/renderUtils"
 
 describe("<SignIn />", () => {
