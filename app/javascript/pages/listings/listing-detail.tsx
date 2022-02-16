2f199fb4cb0632559a917aafe0ecd67a5e32c694
Emily Jablonski <emily.jablonski@exygy.com>
2022-02-16 12:08:50 -0700

    feat: pull in newest ui-components

--- app/javascript/pages/listings/listing-detail.tsx
+++ app/javascript/pages/listings/listing-detail.tsx
@@ -12,6 +12,15 @@ import {
   Waitlist,
   GetApplication,
   SubmitApplication,
+  EventSection,
+  ListSection,
+  StandardTable,
+  PreferencesList,
+  InfoCard,
+  ExpandableText,
+  Description,
+  AdditionalFees,
+  MarkdownSection,
 } from "@bloom-housing/ui-components"
 
 import Layout from "../../layouts/Layout"
@@ -25,6 +34,22 @@ const ListingDetail = (_props: ListingDetailProps) => {
   const alertClasses = "flex-grow mt-6 max-w-6xl w-full"
   // const { getAssetPath, listingsAlertUrl } = useContext(ConfigContext)
   // const { router } = useContext(NavigationContext)
+
+  const mockHeaders = {
+    name: "Name",
+    dob: "Date of Birth",
+  }
+
+  const mockData = [
+    {
+      name: "Jim Halpert",
+      dob: "05/01/1985",
+    },
+    {
+      name: "Michael Scott",
+      dob: "05/01/1975",
+    },
+  ]
 
   const getImage = () => {
     return (
@@ -47,10 +72,10 @@ const ListingDetail = (_props: ListingDetailProps) => {
     return (
       <>
         <GetApplication
-          onlineApplicationURL={""}
+          onlineApplicationURL={"https://www.example.com"}
           applicationsOpen={true}
           applicationsOpenDate={"January 1st"}
-          paperApplications={null}
+          paperApplications={[{ fileURL: "https://www.example.com", languageString: "English" }]}
           paperMethod={false}
           postmarkedApplicationsReceivedByDate={"January 2nd"}
           applicationPickUpAddressOfficeHours={"Pick up office hours"}
@@ -98,26 +123,44 @@ const ListingDetail = (_props: ListingDetailProps) => {
         hideHeader={true}
         desktopClass="header-hidden"
       >
-        <aside className="w-full static md:absolute md:right-0 md:w-1/3 md:top-0 sm:w-2/3 md:ml-2 h-full md:border border-gray-400 bg-white">
+        <aside className="w-full static md:absolute md:right-0 md:w-1/3 md:top-0 sm:w-2/3 md:ml-2 h-full md:border border-solid bg-white">
           <div className="hidden md:block">
-            <ApplicationStatus content={"content"} subContent={"subContent"} />
-            <DownloadLotteryResults event={null} pdfUrl={""} />
+            <ApplicationStatus content={"Application Deadline Dec 31, 2022 at 6:00 PM"} />
+            <EventSection
+              events={[
+                {
+                  dateString: "January 1st, 2022",
+                  timeString: "2:00pm",
+                  note: "1234 Dahlia Avenue, San Francisco",
+                },
+              ]}
+              headerText={"Information Sessions"}
+            />
+            <EventSection
+              events={[
+                {
+                  dateString: "January 1st, 2022",
+                  timeString: "2:00pm",
+                  note: "1234 Dahlia Avenue, San Francisco",
+                },
+              ]}
+              headerText={"Open Houses"}
+            />
             <Waitlist
               isWaitlistOpen={true}
               waitlistMaxSize={100}
               waitlistCurrentSize={25}
               waitlistOpenSpots={75}
             />
+
+            <DownloadLotteryResults resultsDate={"January 1st, 2022"} pdfURL={""} />
+
             {getSidebarApplySection()}
           </div>
 
-          {/* <div className="mb-2 md:hidden">
-            <OpenHouseEvent events={null} />
-          </div> */}
-          {/* <PublicLotteryEvent event={null} /> */}
-          {/* <LotteryResultsEvent event={null} /> */}
-          {/* <WhatToExpect listing={null} /> */}
-          {/* <LeasingAgent listing={null} /> */}
+          {/* Needs Bloom backend dependencies removed, generalized
+          <WhatToExpect listing={null} />
+          <LeasingAgent listing={null} /> */}
         </aside>
       </ListingDetailItem>
     )
@@ -133,24 +176,121 @@ const ListingDetail = (_props: ListingDetailProps) => {
           subtitle={"Income, occupancy, preferences, subsides"}
           desktopClass="bg-primary-lighter"
         >
-          Eligibility content
+          <ListSection
+            title={"Household Maximum Income"}
+            subtitle={
+              "For income calculations, household size includes everyone (all ages) living in the unit."
+            }
+          >
+            <StandardTable headers={mockHeaders} data={mockData} responsiveCollapse={true} />
+          </ListSection>
+          <ListSection
+            title={"Occupancy"}
+            subtitle={
+              "Occupancy limits for this building differ from household size, and do not include children under 6."
+            }
+          >
+            <StandardTable headers={mockHeaders} data={mockData} responsiveCollapse={true} />
+          </ListSection>
+          <ListSection
+            title={"Rental Assistance"}
+            subtitle={
+              "Section 8 housing vouchers and other valid rental assistance programs can be used for this property."
+            }
+          />
+          <ListSection
+            title={"Lottery Preferences"}
+            subtitle={
+              "Anyone may enter the housing lottery for this listing. If your household has one of the following preferences, you will be considered in the order shown here. Each preference holder will be reviewed in lottery rank order."
+            }
+          >
+            <>
+              <PreferencesList
+                listingPreferences={[
+                  { title: "Title", subtitle: "Subtitle", description: "Description", ordinal: 1 },
+                  { title: "Title", subtitle: "Subtitle", description: "Description", ordinal: 2 },
+                ]}
+              />
+              <p className="text-gray-700 text-tiny">
+                {
+                  "After all preference holders have been considered, any remaining units will be available to qualified applicants in lottery order."
+                }
+              </p>
+            </>
+          </ListSection>
+          <ListSection
+            title={"Rental Assistance"}
+            subtitle={
+              "Section 8 housing vouchers and other valid rental assistance programs can be used for this property."
+            }
+          />
+          <ListSection
+            title={"Additional Eligibility Rules"}
+            subtitle={"Applicants must also qualify under the rules of the building."}
+          >
+            <InfoCard title={"Credit History"}>
+              <ExpandableText className="text-sm text-gray-700">
+                {"Credit history information"}
+              </ExpandableText>
+            </InfoCard>
+            <InfoCard title={"Rental History"}>
+              <ExpandableText className="text-sm text-gray-700">
+                {"Rental history information"}
+              </ExpandableText>
+            </InfoCard>
+            <InfoCard title={"Criminal Background"}>
+              <ExpandableText className="text-sm text-gray-700">
+                {"Criminal background information"}
+              </ExpandableText>
+            </InfoCard>
+            <p>
+              <a href={"https://www.example.com"}>
+                {"Find out more about Building Selection Criteria"}
+              </a>
+            </p>
+          </ListSection>
         </ListingDetailItem>
         <ListingDetailItem
-          imageAlt={""}
+          imageAlt={"Image Alt"}
           imageSrc={""}
           title={"Features"}
           subtitle={"Amenities, unit details and additional fees"}
         >
-          Features content
-        </ListingDetailItem>
-        <ListingDetailItem
-          imageAlt={""}
-          imageSrc={""}
-          title={"Neighborhood"}
-          subtitle={"Location and transportation"}
-          desktopClass="bg-primary-lighter"
-        >
-          Neighborhood content
+          <div className="listing-detail-panel">
+            <dl className="column-definition-list">
+              <Description term={"Neighborhood"} description={"My Neighborhood"} />
+              <Description term={"Built"} description={"2022"} />
+              <Description term={"Smoking Policy"} description={"Non-smoking building"} />
+              <Description term={"Pets Policy"} description={"Service animals allowed"} />
+              <Description
+                term={"Property Amenities"}
+                description={"Underground parking, courtyard, bike room, business center"}
+              />
+              <Description term={"Unit Amenities"} description={"In-unit wahser/dryer"} />
+              <Description term={"Accessibility"} description={"Elevator to all floors"} />
+              {/*
+              Needs Bloom backend dependencies removed, generalized
+              <Description
+                term={"Unit Features"}
+                description={
+                  <UnitTables
+                    units={listing.units}
+                    unitSummaries={listing?.unitsSummarized?.byUnitType}
+                    disableAccordion={listing.disableUnitsAccordion}
+                  />
+                }
+              /> */}
+            </dl>
+            <AdditionalFees
+              depositMin={"2,102"}
+              depositMax={"2,355"}
+              applicationFee={"50"}
+              costsNotIncluded={
+                "Tenants pay for gas, electricity. For pet fees: Cat is allowed with a $500 refundable deposit, $250 non-refundable cleaning fee and a pet addendum. Dogs are not allowed in the building. One parking space per unit available for $175 a month."
+              }
+              depositHelperText={"or one month's rent"}
+            />
+          </div>
         </ListingDetailItem>
         <ListingDetailItem
           imageAlt={""}
@@ -158,7 +298,18 @@ const ListingDetail = (_props: ListingDetailProps) => {
           title={"Additional information"}
           subtitle={"Required documents and selection criteria"}
         >
-          Additional information content
+          <div className="listing-detail-panel">
+            <div className="info-card">
+              <h3 className="text-serif-lg">Required Documents</h3>
+              <p className="text-sm text-gray-700 m-0 p-0">
+                <MarkdownSection>
+                  Lottery winners will be required to fill out a building application and provide a
+                  copy of your current credit report, 3 most recent paystubs, current tax returns
+                  and W-2, and 3 most recent bank statements.
+                </MarkdownSection>
+              </p>
+            </div>
+          </div>
         </ListingDetailItem>
         {getSidebar()}
       </ListingDetails>
@@ -169,31 +320,14 @@ const ListingDetail = (_props: ListingDetailProps) => {
     return (
       <div className="w-full md:w-2/3 md:mt-6 md:mb-6 md:px-3 md:pr-8">
         <Message warning={true}>Reserved Type</Message>
-        <GroupedTable
-          headers={{ col1: "col1", col2: "col2" }}
-          data={[
-            {
-              data: [
-                {
-                  col1: "row 1",
-                  col2: "row 1",
-                },
-                {
-                  col1: "row 2",
-                  col2: "row 2",
-                },
-              ],
-            },
-          ]}
-          responsiveCollapse={true}
-        />
+        <GroupedTable headers={mockHeaders} data={[{ data: mockData }]} responsiveCollapse={true} />
       </div>
     )
   }
 
   return (
     <Layout>
-      <div className="flex absolute w-full flex-col items-center">
+      <div className="flex absolute w-full flex-col items-center border-0 border-t border-solid">
         <SiteAlert type="alert" className={alertClasses} />
         <SiteAlert type="success" className={alertClasses} timeout={30_000} />
       </div>
