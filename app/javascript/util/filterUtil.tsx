a8b4bbe709878c2f5e9a0810fdd55b8cdc31a1aa
jacksongonzales <jackson.gonzales@gmail.com>
2022-03-22 18:48:05 -0700

    fix(listings): comment for posterity

--- app/javascript/util/filterUtil.tsx
+++ app/javascript/util/filterUtil.tsx
@@ -1,3 +1,7 @@
+// This is adapted from the filter implemented in customFilters.js.coffee
+// for doing a barebones sanitization of incoming raw html. We should probably
+// find a better solution with community support at some point
+
 export const stripMostTags = (input, allowed?: string) => {
   if (!input) return ""
   allowed = (((allowed || "<br><a>") + "").toLowerCase().match(/<[a-z][a-z0-9]*>/g) || []).join("")
