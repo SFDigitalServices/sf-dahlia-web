6ee25ec41a51d932bba07c6b4fdbe023ac45c33e
jacksongonzales <jackson.gonzales@gmail.com>
2022-04-18 15:22:34 -0700

    DAH-1134 finish additional info section

---
+++ app/javascript/components/TextTruncate.tsx
@@ -0,0 +1,39 @@
+/*
+  This is a solution for truncating raw HTML markup strings that we receive
+  from the salesforce listing API. The main use-case is currently the Additional
+  Information section on listings
+*/
+import React, { useState } from "react"
+import { t } from "@bloom-housing/ui-components"
+import truncate from "truncate-html"
+import { renderInlineWithInnerHTML } from "../util/languageUtil"
+import { stripMostTags } from "../util/filterUtil"
+
+export interface TextTruncateProps {
+  text: string
+}
+
+export const TextTruncate = ({ text }: TextTruncateProps) => {
+  const [isExpanded, setIsExpanded] = useState(false)
+
+  truncate.setup({ byWords: true, ellipsis: " ... ", length: 60 })
+  const truncatedText = renderInlineWithInnerHTML(stripMostTags(truncate(text)))
+  const untruncatedText = renderInlineWithInnerHTML(stripMostTags(text))
+
+  return (
+    <div className="text-sm">
+      {isExpanded ? untruncatedText : truncatedText}
+      {truncate(text) !== text && (
+        <span
+          className="button-toggle"
+          onClick={() => setIsExpanded(!isExpanded)}
+          onKeyPress={() => setIsExpanded(!isExpanded)}
+          role="button"
+          tabIndex={0}
+        >
+          {isExpanded ? t("label.less") : t("label.more")}
+        </span>
+      )}
+    </div>
+  )
+}
