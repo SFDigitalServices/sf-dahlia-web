2458e510da63b80655c988aee3ebcec0f77f929a
christine-sfg <98352600+christine-sfg@users.noreply.github.com>
2022-04-27 17:01:24 -0700

    DAH-1135 Right Panel Lottery Results (#1624)

--- app/javascript/api/listingApiService.ts
+++ app/javascript/api/listingApiService.ts
@@ -2,12 +2,25 @@ import { get } from "./apiService"
 import RailsRentalListing from "./types/rails/listings/RailsRentalListing"
 import { RailsListing } from "../modules/listings/SharedHelpers"
 import { RailsListingPreference } from "./types/rails/listings/RailsListingPreferences"
+import { RailsLotteryBucketsDetails } from "./types/rails/listings/RailsLotteryBucketsDetails"
 
 type ListingsResponse = { listing: RailsRentalListing }
 type ListingPreferencesResponse = { preferences: RailsListingPreference[] }
 
 export const getListing = async (listingId?: string): Promise<RailsListing> =>
   get<ListingsResponse>(`/api/v1/listings/${listingId}.json`).then(({ data }) => data.listing)
+
+/**
+ * Get the lottery buckets with rankings for the given listing
+ * @param {string} listingId
+ * @returns {RailsLotteryBuckets} lottery bucket info
+ */
+export const getLotteryBucketDetails = async (
+  listingId?: string
+): Promise<RailsLotteryBucketsDetails> =>
+  get<RailsLotteryBucketsDetails>(`/api/v1/listings/${listingId}/lottery_buckets`).then(
+    (response) => response.data
+  )
 
 /**
  * Get the lottery preferences for the given listing
