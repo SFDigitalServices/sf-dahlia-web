f8591accbeef0726b81c6c19ab5d4faba4437590
christine-sfg <98352600+christine-sfg@users.noreply.github.com>
2022-04-29 15:11:43 -0700

    DAH-1147 Listing Details Page in All Languages (#1628)
    * DAH-1147 Listing Details Page in All Languages
    
    * DAH-1147 add translations for details page
    
    * DAH-1147 sort lines
    
    * DAH-1147 clean up remaining snake case keys in favor of camel case
    
    * DAH-1147 fix param

--- app/javascript/pages/listings/listing-detail.tsx
+++ app/javascript/pages/listings/listing-detail.tsx
@@ -19,6 +19,7 @@ import { ListingDetailsFeatures } from "../../modules/listingDetails/ListingDeta
 import { ListingDetailsNeighborhood } from "../../modules/listingDetails/ListingDetailsNeighborhood"
 import { ListingDetailsAdditionalInformation } from "../../modules/listingDetails/ListingDetailsAdditionalInformation"
 import { ConfigContext } from "../../lib/ConfigContext"
+import { getPathWithoutLanguagePrefix } from "../../util/languageUtil"
 
 const ListingDetail = () => {
   const alertClasses = "flex-grow mt-6 max-w-6xl w-full"
@@ -27,7 +28,8 @@ const ListingDetail = () => {
   const [listing, setListing] = useState<RailsListing>(null)
 
   useEffect(() => {
-    void getListing(router.pathname.split("/")[2]).then((listing: RailsListing) => {
+    const path = getPathWithoutLanguagePrefix(router.pathname)
+    void getListing(path.split("/")[2]).then((listing: RailsListing) => {
       setListing(listing)
     })
   }, [router.pathname])
@@ -61,9 +63,9 @@ const ListingDetail = () => {
               <div className="listing-detail-panel">
                 <div className="info-card flex">
                   {/* TODO: do we have a class for serifs but smaller we can enable? */}
-                  <p className="text-serif-lg">{t("listings.monitored_by_mohcd")}</p>
+                  <p className="text-serif-lg">{t("listings.monitoredByMohcd")}</p>
                   <img
-                    alt={t("listings.equal_housing_opportunity_logo")}
+                    alt={t("listings.equalHousingOpportunityLogo")}
                     src={getAssetPath("logo-equal.png")}
                   />
                 </div>
