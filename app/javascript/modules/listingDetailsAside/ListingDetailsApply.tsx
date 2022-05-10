7fd291a5bf95a48c8b8b82d90c79846236ef45f8
Emily Jablonski <emily.jablonski@exygy.com>
2022-05-10 16:40:27 -0400

    fix: pull over unavailable logic from angular

--- app/javascript/modules/listingDetailsAside/ListingDetailsApply.tsx
+++ app/javascript/modules/listingDetailsAside/ListingDetailsApply.tsx
@@ -1,7 +1,6 @@
 import React from "react"
 import { RailsListing } from "../listings/SharedHelpers"
 import { QuantityRowSection, t } from "@bloom-housing/ui-components"
-import { renderInlineWithInnerHTML } from "../../util/languageUtil"
 
 export interface ListingDetailsApplyProps {
   listing: RailsListing
@@ -13,36 +12,86 @@ export interface ListingDetailsApplyProps {
   Need Help
 */
 export const ListingDetailsApply = ({ listing }: ListingDetailsApplyProps) => {
-  const formattedDescription = renderInlineWithInnerHTML(
-    t("listings.availableUnitsAndWaitlistDescription", {
-      number: listing.Units_Available,
-      unit:
-        listing.Units_Available > 1
-          ? t("listings.availableUnitsAndWaitlistDescription.pluralUnits")
-          : t("listings.availableUnitsAndWaitlistDescription.singularUnit"),
-    })
+  const descriptionValues = {
+    number: listing.Total_number_of_building_units,
+    unit:
+      listing.Total_number_of_building_units > 1
+        ? t("listings.availableUnitsAndWaitlistDescription.pluralUnits")
+        : t("listings.availableUnitsAndWaitlistDescription.singularUnit"),
+  }
+  const availableDescription = (
+    <p>
+      {t("listings.availableUnitsAndWaitlistDescription", {
+        ...descriptionValues,
+      })}
+    </p>
   )
+
+  const availableRows = [
+    {
+      amount: listing.Units_Available,
+      text: t("listings.availableUnits"),
+      emphasized: true,
+    },
+    {
+      amount: listing.Total_waitlist_openings,
+      text: t("listings.openWaitlistSlots"),
+      emphasized: true,
+    },
+  ]
+
+  const unavailableDescription = (
+    <>
+      <p>{t("listings.noAvailableUnits")}</p>
+      <p>
+        {t("listings.enterLotteryForWaitlist", {
+          ...descriptionValues,
+        })}
+      </p>
+    </>
+  )
+
+  const unavailableRows = [
+    {
+      amount: listing.Number_of_people_currently_on_waitlist,
+      text: t("listings.currentWaitlistSize"),
+      emphasized: true,
+    },
+    {
+      amount: listing.Total_waitlist_openings,
+      text: t("listings.openWaitlistSlots"),
+      emphasized: true,
+    },
+    {
+      amount: listing.Maximum_waitlist_size,
+      text: t("listings.finalWaitlistSize"),
+      emphasized: true,
+    },
+  ]
+
   return (
     <>
-      {listing.Total_waitlist_openings > 0 && listing.Units_Available > 0 && (
-        <QuantityRowSection
-          quantityRows={[
-            {
-              amount: listing.Units_Available,
-              text: t("listings.availableUnits"),
-              emphasized: true,
-            },
-            {
-              amount: listing.Total_waitlist_openings,
-              text: t("listings.openWaitlistSlots"),
-              emphasized: true,
-            },
-          ]}
-          strings={{
-            sectionTitle: t("listings.availableUnitsAndWaitlist"),
-            description: formattedDescription,
-          }}
-        />
+      {listing.hasWaitlist && (
+        <>
+          {listing.Units_Available === 0 && (
+            <QuantityRowSection
+              quantityRows={unavailableRows}
+              strings={{
+                sectionTitle: t("listings.waitlistIsOpen"),
+                description: unavailableDescription,
+              }}
+            />
+          )}
+          {listing.Units_Available > 0 && (
+            <QuantityRowSection
+              quantityRows={availableRows}
+              strings={{
+                sectionTitle: t("listings.availableUnitsAndWaitlist"),
+                description: availableDescription,
+              }}
+            />
+          )}
+        </>
       )}
     </>
   )
