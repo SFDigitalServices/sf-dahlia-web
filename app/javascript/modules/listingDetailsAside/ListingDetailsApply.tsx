433b1de9607a003a9a4894a21321982e3f5f7dbc
Emily Jablonski <emily.jablonski@exygy.com>
2022-05-10 12:46:53 -0400

    DAH-1136 feat: uptake new waitlist component

--- app/javascript/modules/listingDetailsAside/ListingDetailsApply.tsx
+++ app/javascript/modules/listingDetailsAside/ListingDetailsApply.tsx
@@ -1,22 +1,49 @@
 import React from "react"
 import { RailsListing } from "../listings/SharedHelpers"
-import { Waitlist } from "@bloom-housing/ui-components"
+import { QuantityRowSection, t } from "@bloom-housing/ui-components"
+import { renderInlineWithInnerHTML } from "../../util/languageUtil"
 
 export interface ListingDetailsApplyProps {
   listing: RailsListing
 }
 
+/*
+  TODO: this component will also include
+  How to Apply
+  Need Help
+*/
 export const ListingDetailsApply = ({ listing }: ListingDetailsApplyProps) => {
+  const formattedDescription = renderInlineWithInnerHTML(
+    t("listings.availableUnitsAndWaitlistDescription", {
+      number: listing.Units_Available,
+      unit:
+        listing.Units_Available > 1
+          ? t("listings.availableUnitsAndWaitlistDescription.pluralUnits")
+          : t("listings.availableUnitsAndWaitlistDescription.singularUnit"),
+    })
+  )
   return (
-    <Waitlist
-      isWaitlistOpen={!!listing.Maximum_waitlist_size || !!listing.Total_waitlist_openings}
-      waitlistCurrentSize={listing.Units_Available}
-      waitlistOpenSpots={listing.Total_waitlist_openings}
-      waitlistMaxSize={listing.Maximum_waitlist_size}
-    />
-    /* TODO: this component will also include
-        How to Apply
-        Need Help
-     */
+    <>
+      {listing.Total_waitlist_openings > 0 && listing.Units_Available > 0 && (
+        <QuantityRowSection
+          quantityRows={[
+            {
+              amount: listing.Units_Available,
+              text: t("listings.availableUnits"),
+              emphasized: true,
+            },
+            {
+              amount: listing.Total_waitlist_openings,
+              text: t("listings.openWaitlistSlots"),
+              emphasized: true,
+            },
+          ]}
+          strings={{
+            sectionTitle: t("listings.availableUnitsAndWaitlist"),
+            description: formattedDescription,
+          }}
+        />
+      )}
+    </>
   )
 }
