f5ea5e28e65639e2ae6f65f716f5219cd88e2ea3
jacksongonzales <jackson.gonzales@gmail.com>
2022-05-10 16:44:45 -0700

    DAH-1097 bring sf dessys colors into tailwind theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 const cloneDeep = require("clone-deep")
 const bloomTheme = cloneDeep(require("@bloom-housing/ui-components/tailwind.config.js"))
+const sfdsTheme = cloneDeep(require("sfgov-design-system/tailwind.preset"))
 
 // Modify bloomTheme to override any Tailwind vars
 // For example:
@@ -17,4 +18,13 @@ bloomTheme.theme.fontSize["4xl"] = ['2rem', {
 bloomTheme.theme.fontSize["5xl"] = ['2.5rem', {
   lineHeight: '1.25',
 }]
+
+const config = Object.assign({}, sfdsTheme, bloomTheme)
+Object.assign(config.theme.colors, sfdsTheme.theme.colors)
+
+config.theme.colors.green['700'] = bloomTheme.theme.colors.green['700']
+config.theme.colors.blue['700'] = bloomTheme.theme.colors.blue['700']
+config.theme.colors.red['700'] = bloomTheme.theme.colors.red['700']
+config.theme.colors.yellow['700'] = bloomTheme.theme.colors.yellow['700']
+
 module.exports = bloomTheme
