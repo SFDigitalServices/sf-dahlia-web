af58fce78dfb997273cf7c6dbf6ffe45f8311c3c
christine-sfg <98352600+christine-sfg@users.noreply.github.com>
2022-06-23 16:41:54 -0700

    DAH-1160 use process component

--- app/javascript/modules/listingDetailsLottery/MobileListingDetailsLottery.tsx
+++ app/javascript/modules/listingDetailsLottery/MobileListingDetailsLottery.tsx
@@ -1,8 +1,9 @@
 import React from "react"
+import { ListingDetailItem, Mobile, t } from "@bloom-housing/ui-components"
 import { RailsListing } from "../listings/SharedHelpers"
-import { Contact, ListingDetailItem, Mobile, t } from "@bloom-housing/ui-components"
 import { ListingDetailsLotteryResults } from "./ListingDetailsLotteryResults"
-import { isLotteryComplete } from "../../util/listingUtil"
+import { ListingDetailsProcess } from "../listingDetailsAside/ListingDetailsProcess"
+import { isOpen } from "../../util/listingUtil"
 
 export interface ListingDetailsLotteryProps {
   imageSrc: string
@@ -12,7 +13,7 @@ export interface ListingDetailsLotteryProps {
 export const MobileListingDetailsLottery = ({ imageSrc, listing }: ListingDetailsLotteryProps) => {
   return (
     listing &&
-    isLotteryComplete(listing) && (
+    !isOpen(listing) && (
       <Mobile>
         <ListingDetailItem
           imageAlt={""}
@@ -21,37 +22,7 @@ export const MobileListingDetailsLottery = ({ imageSrc, listing }: ListingDetail
           subtitle={t("lottery.lotteryInfoSubheader")}
         >
           <ListingDetailsLotteryResults listing={listing} />
-          {/* TODO: add open houses */}
-          {/* TODO: add what to expect */}
-          <Contact
-            sectionTitle={t("contactAgent.contact")}
-            contactAddress={{
-              street: listing.Leasing_Agent_Street,
-              city: listing.Leasing_Agent_City,
-              state: listing.Leasing_Agent_State,
-              zipCode: listing.Leasing_Agent_Zip,
-            }}
-            additionalInformation={
-              listing.Office_Hours
-                ? [
-                    {
-                      title: t("contactAgent.officeHours"),
-                      content: listing.Office_Hours,
-                    },
-                  ]
-                : undefined
-            }
-            contactEmail={listing.Leasing_Agent_Email}
-            contactName={listing.Leasing_Agent_Name}
-            contactPhoneNumber={t("listings.call", { phoneNumber: listing.Leasing_Agent_Phone })}
-            contactPhoneNumberNote={t("contactAgent.dueToHighCallVolume")}
-            contactTitle={listing.Leasing_Agent_Title}
-            strings={{
-              email: t("label.emailAddress"),
-              getDirections: t("label.getDirections"),
-            }}
-          />
-          {/* TODO: add housing program */}
+          <ListingDetailsProcess listing={listing} />
         </ListingDetailItem>
       </Mobile>
     )
