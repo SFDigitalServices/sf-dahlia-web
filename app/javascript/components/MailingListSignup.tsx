ec234d60625cc98f2d2015f3abcc8a81ac04c4d2
christine-sfg <98352600+christine-sfg@users.noreply.github.com>
2022-07-11 15:15:20 -0700

    Features/dah 1176 mailing list (#1674)
    * DAH-1176 Add mailing list sign up to details page
    
    * DAH-1176 Center text on the MOHCD component

---
+++ app/javascript/components/MailingListSignup.tsx
@@ -0,0 +1,28 @@
+import React, { useContext } from "react"
+import { ActionBlock, Icon, t } from "@bloom-housing/ui-components"
+import Link from "../navigation/Link"
+import ConfigContext from "../lib/ConfigContext"
+
+export const MailingListSignup = () => {
+  const { listingsAlertUrl } = useContext(ConfigContext)
+
+  return (
+    <ActionBlock
+      className={"mt-4"}
+      header={t("welcome.newListingEmailAlert")}
+      background="primary-lighter"
+      icon={<Icon size="3xl" symbol="mailThin" fill="transparent" />}
+      actions={[
+        <Link
+          className="button"
+          key="action-1"
+          external={true}
+          href={listingsAlertUrl}
+          target="_blank"
+        >
+          {t("welcome.signUpToday")}
+        </Link>,
+      ]}
+    />
+  )
+}
