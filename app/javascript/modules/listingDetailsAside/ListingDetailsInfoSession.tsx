c6b625df177ac6738df9f49196ced5af06af61b8
Mark Buckner <markbuckner@Marks-MacBook-Pro.local>
2022-07-19 20:46:09 -0600

    DAH-1153 Add tests for OpenHouses Component

--- app/javascript/modules/listingDetailsAside/ListingDetailsInfoSession.tsx
+++ app/javascript/modules/listingDetailsAside/ListingDetailsInfoSession.tsx
@@ -2,36 +2,23 @@ import React from "react"
 import { getEventNote, RailsListing } from "../listings/SharedHelpers"
 import { EventSection, t } from "@bloom-housing/ui-components"
 import { ListingEvent } from "../../api/types/rails/listings/BaseRailsListing"
-import { localizedFormat } from "../../util/languageUtil"
+import { localizedFormat, listingEventHasDate, getEventTimeString } from "../../util/languageUtil"
 import OpenHouses from "../../components/OpenHouses"
 
 export interface ListingDetailsInfoSessionProps {
   listing: RailsListing
 }
 
-const getEventTimeString = (listingEvent: ListingEvent) => {
-  if (listingEvent.Start_Time) {
-    return listingEvent.End_Time
-      ? `${listingEvent.Start_Time} - ${listingEvent.End_Time}`
-      : listingEvent.Start_Time
-  }
-  return ""
-}
-
-const filterListingEventsWithoutDates = (listingEvent: ListingEvent) => {
-  return listingEvent?.Date
-}
-
 export const ListingDetailsInfoSession = ({ listing }: ListingDetailsInfoSessionProps) => {
   return (
     <>
       {listing.Information_Sessions?.filter((informationSession: ListingEvent) => {
-        return filterListingEventsWithoutDates(informationSession)
+        return listingEventHasDate(informationSession)
       })?.length > 0 ? (
         <EventSection
           sectionHeader={true}
           events={listing.Information_Sessions?.filter((informationSession: ListingEvent) => {
-            return filterListingEventsWithoutDates(informationSession)
+            return listingEventHasDate(informationSession)
           }).map((informationSession) => {
             return {
               dateString: localizedFormat(informationSession.Date, "LL"),
