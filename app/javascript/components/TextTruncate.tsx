225e6804bb8231c6d93044c28660a1f110e0af90
christine-sfg <98352600+christine-sfg@users.noreply.github.com>
2022-07-21 11:24:52 -0700

    DAH-1200 add space for more and less links

--- app/javascript/components/TextTruncate.tsx
+++ app/javascript/components/TextTruncate.tsx
@@ -19,17 +19,13 @@ export const TextTruncate = ({ text }: TextTruncateProps) => {
   truncate.setup({ ellipsis: " ... ", length: 400, reserveLastWord: true })
   const truncatedText = renderInlineWithInnerHTML(stripMostTags(truncate(text)))
   const untruncatedText = renderInlineWithInnerHTML(stripMostTags(text))
-  const linkStyles = ["button-toggle"]
-  if (!isExpanded) {
-    linkStyles.push("mx-1")
-  }
 
   return (
     <div className="text-sm">
       {isExpanded ? untruncatedText : truncatedText}
       {truncate(text)?.length !== text?.length && (
         <span
-          className={linkStyles.join(" ")}
+          className="button-toggle ml-1"
           onClick={() => setIsExpanded(!isExpanded)}
           onKeyPress={() => setIsExpanded(!isExpanded)}
           role="button"
