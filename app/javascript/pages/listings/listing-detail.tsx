640211c19f8afe4d6d239689e453b0fc97d0624b
jacksongonzales <jackson.gonzales@gmail.com>
2022-09-02 16:41:20 -0700

    DAH-1094 Additional translation and styling fixes

--- app/javascript/pages/listings/listing-detail.tsx
+++ app/javascript/pages/listings/listing-detail.tsx
@@ -24,10 +24,11 @@ import { ConfigContext } from "../../lib/ConfigContext"
 import { getPathWithoutLanguagePrefix } from "../../util/languageUtil"
 import { ListingDetailsReservedBanner } from "../../modules/listingDetails/ListingDetailsReservedBanner"
 import { ListingDetailsApplicationDate } from "../../modules/listingDetailsAside/ListingDetailsApplicationDate"
-import { isOpen, isPluralSRO } from "../../util/listingUtil"
+import { isOpen, isPluralSRO, listingHasSROUnits } from "../../util/listingUtil"
 import { MobileListingDetailsLottery } from "../../modules/listingDetailsLottery/MobileListingDetailsLottery"
 import { MailingListSignup } from "../../components/MailingListSignup"
 import { ListingDetailsWaitlist } from "../../modules/listingDetailsAside/ListingDetailsWaitlist"
+import { ListingDetailsSROInfo } from "../../modules/listingDetails/ListingDetailsSROInfo"
 
 const ListingDetail = () => {
   const alertClasses = "flex-grow mt-6 max-w-6xl w-full"
@@ -66,11 +67,14 @@ const ListingDetail = () => {
               reservedCommunityType={listing.Reserved_community_type}
             />
             <ListingDetailsPricingTable />
-            {isPluralSRO && (
-              <InfoCard title={t("listings.singleRoomOccupancy")}>
-                {t("listings.singleRoomOccupancyDescription")}
-              </InfoCard>
-            )}
+            {listingHasSROUnits(listing) &&
+              !(
+                isPluralSRO("1335 Folsom Street", listing) || isPluralSRO("750 Harrison", listing)
+              ) && (
+                <div className="md:w-2/3 md:pr-8">
+                  <ListingDetailsSROInfo listing={listing} />
+                </div>
+              )}
             {isApplicationOpen && (
               <Mobile>
                 <ListingDetailsApplicationDate
