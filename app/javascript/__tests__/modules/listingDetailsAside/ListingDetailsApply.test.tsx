144abbdd9850b218ac37eaf7d58525d3538f12c0
christine-sfg <98352600+christine-sfg@users.noreply.github.com>
2022-10-03 12:22:32 -0700

    DAH-1254 add eligibility requirement link for habitat

--- app/javascript/__tests__/modules/listingDetailsAside/ListingDetailsApply.test.tsx
+++ app/javascript/__tests__/modules/listingDetailsAside/ListingDetailsApply.test.tsx
@@ -3,6 +3,7 @@ import renderer from "react-test-renderer"
 import { ListingDetailsApply } from "../../../modules/listingDetailsAside/ListingDetailsApply"
 import { openSaleListing } from "../../data/RailsSaleListing/listing-sale-open"
 import { closedRentalListing } from "../../data/RailsRentalListing/listing-rental-closed"
+import { habitatListing } from "../../data/RailsSaleListing/listing-sale-habitat"
 
 describe("ListingDetailsApply", () => {
   it("does not render if listing is closed", () => {
@@ -10,9 +11,24 @@ describe("ListingDetailsApply", () => {
 
     expect(tree).toMatchSnapshot()
   })
+
   it("renders if listing is open", () => {
     const tree = renderer.create(<ListingDetailsApply listing={openSaleListing} />).toJSON()
 
     expect(tree).toMatchSnapshot()
   })
+
+  it(
+    "displays habitat link for eligibility requirements when listing is habitat and is" + " open",
+    () => {
+      const openHabitatListing = {
+        ...habitatListing,
+        Application_Due_Date: "2032-12-02T01:00:00.000+0000",
+      }
+
+      const tree = renderer.create(<ListingDetailsApply listing={openHabitatListing} />).toJSON()
+
+      expect(tree).toMatchSnapshot()
+    }
+  )
 })
