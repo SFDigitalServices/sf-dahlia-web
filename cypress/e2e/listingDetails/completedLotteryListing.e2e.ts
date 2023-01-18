3b06f8670c23e6771b9bfeec731d328f9d6adfd1
Mark Buckner <markbuckner@Marks-MacBook-Pro.local>
2023-01-17 20:02:44 -0700

    DAH-1248 remove mobile tests

--- cypress/e2e/listingDetails/completedLotteryListing.e2e.ts
+++ cypress/e2e/listingDetails/completedLotteryListing.e2e.ts
@@ -35,17 +35,17 @@ describe("Listing Details for Completed Lottery Listing", () => {
   })
 
   describe("Completed Lottery Rental Listing " + listings.COMPLETED_LOTTERY.id, () => {
-    it("clicking the View Lottery Results button opens the lottery results modal on mobile devices", () => {
-      visitListing(true, "")
-      clickLotteryResultsButton(true)
-      cy.contains("Lottery results are divided into multiple lists.")
-    })
+    // it("clicking the View Lottery Results button opens the lottery results modal on mobile devices", () => {
+    //   visitListing(true, "")
+    //   clickLotteryResultsButton(true)
+    //   cy.contains("Lottery results are divided into multiple lists.")
+    // })
 
-    it("clicking the View Lottery Results button opens the lottery results modal on mobile devices", () => {
-      visitListing(true, "")
-      clickLotteryResultsButton(true)
-      cy.contains("Lottery results are divided into multiple lists.")
-    })
+    // it("clicking the View Lottery Results button opens the lottery results modal on mobile devices", () => {
+    //   visitListing(true, "")
+    //   clickLotteryResultsButton(true)
+    //   cy.contains("Lottery results are divided into multiple lists.")
+    // })
 
     // it("searching for a lottery number returns results on mobile devices", () => {
     //   visitListing(true, "")
