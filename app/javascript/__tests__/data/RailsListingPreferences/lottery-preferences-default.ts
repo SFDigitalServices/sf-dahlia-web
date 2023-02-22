87d1ea59acd6a92123167a7314df60ea3efcc782
Chad Brokaw <chad.brokaw@exygy.com>
2023-02-22 12:31:25 -0700

    Add Custom Preference Description

--- app/javascript/__tests__/data/RailsListingPreferences/lottery-preferences-default.ts
+++ app/javascript/__tests__/data/RailsListingPreferences/lottery-preferences-default.ts
@@ -17,6 +17,7 @@ export const preferences: RailsListingPreference[] = [
     description:
       "For households in which at least one member holds a Certificate of Preference from the former San Francisco Redevelopment Agency. COP holders were displaced by Agency action generally during the 1960s and 1970s.",
     appTotal: null,
+    customPreferenceDescription: false,
   },
   {
     unitsAvailable: 1,
@@ -33,6 +34,7 @@ export const preferences: RailsListingPreference[] = [
     description:
       "For households in which at least one member holds a Displaced Tenant Housing Preference Certificate. DTHP Certificate holders are tenants who were evicted through either an Ellis Act Eviction or an Owner Move In Eviction, have been displaced by a fire, or who will experience an unaffordable rent increase due to affordability restrictions expiring. Once all units reserved for this preference are filled, remaining DTHP holders will receive Live/Work preference, regardless of their current residence or work location.",
     appTotal: null,
+    customPreferenceDescription: false,
   },
   {
     unitsAvailable: 6,
@@ -47,7 +49,8 @@ export const preferences: RailsListingPreference[] = [
     listingPreferenceID: "a0l4U00001c7x5wQAA",
     listingId: "a0W4U00000KnJOuUAN",
     description:
-      "For households in which at least one member lives or works in San Francisco.  Requires submission of proof.  Please note in order to claim Work Preference, the applicant currently work in San Francisco at least 75% of their working hours.",
+      "This is a custom description - For households in which at least one member lives or works in San Francisco.  Requires submission of proof.  Please note in order to claim Work Preference, the applicant currently work in San Francisco at least 75% of their working hours.",
     appTotal: null,
+    customPreferenceDescription: true,
   },
 ]
