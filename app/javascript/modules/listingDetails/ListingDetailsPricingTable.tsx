11d1434989316b4216808f041015daaa7e4f78b7
Mark Buckner <markbuckner@Marks-MacBook-Pro.local>
2023-02-22 09:35:24 -0700

    DAH-1175 code clean up

--- app/javascript/modules/listingDetails/ListingDetailsPricingTable.tsx
+++ app/javascript/modules/listingDetails/ListingDetailsPricingTable.tsx
@@ -9,7 +9,7 @@ export interface ListingDetailsPricingTableProps {
   listing: RailsListing
 }
 
-export interface PricingDataState {
+export interface ListingDetailsPricingTableState {
   units: RailsListingPricingTableUnit[]
   hasFetched: boolean
 }
@@ -27,150 +27,150 @@ export interface MappedUnitsByOccupancy {
   summaryByAMI: SummaryByAMI[]
 }
 
-export const ListingDetailsPricingTable = ({ listing }: ListingDetailsPricingTableProps) => {
-  const listingIsSale = isSale(listing)
-  const listingIsHabitat = isHabitatListing(listing)
-  let pricingDataContent
+const buildSaleCells = (unitSummary: RailsListingPricingTableUnit) => {
+  return {
+    units: {
+      cellText: unitSummary.unitType,
+      cellSubText: `${unitSummary.availability} ${t("t.available")}`,
+    },
+    income: {
+      cellText: `$${unitSummary.absoluteMinIncome} to $${unitSummary.absoluteMaxIncome}`,
+      cellSubText: t("t.perMonth"),
+    },
+    sale: [
+      {
+        cellText: `$${unitSummary.maxPriceWithParking}`,
+        cellSubText: "with parking",
+      },
+      {
+        cellText: `$${unitSummary.maxPriceWithoutParking}`,
+        cellSubText: "without parking",
+      },
+    ],
+    monthlyHoaDues: [
+      {
+        cellText: `$${unitSummary.maxHoaDuesWithParking}`,
+        cellSubText: "with parking",
+      },
+      {
+        cellText: `$${unitSummary.maxHoaDuesWithoutParking}`,
+        cellSubText: "without parking",
+      },
+    ],
+  }
+}
 
-  const responsiveTableHeaders = listingIsSale
-    ? {
-        units: { name: "t.unitType" },
-        income: { name: "t.incomeRange" },
-        sale: { name: "listings.stats.salesPrice" },
-        monthlyHoaDues: { name: "Monthly HOA Dues" },
-      }
-    : {
-        units: { name: "t.unitType" },
-        income: { name: "t.incomeRange" },
-        rent: { name: "t.rent" },
-      }
+const buildRentalCells = (unitSummary: RailsListingPricingTableUnit) => {
+  return {
+    units: {
+      cellText: unitSummary.unitType,
+      cellSubText: `${unitSummary.availability} ${t("t.available")}`,
+    },
+    income: {
+      cellText: `$${unitSummary.absoluteMinIncome} to $${unitSummary.absoluteMaxIncome}`,
+      cellSubText: t("t.perMonth"),
+    },
+    rent: {
+      cellText: `$${unitSummary.maxMonthlyRent}`,
+      cellSubText: t("t.perMonth"),
+    },
+  }
+}
 
-  const [pricingData, setPricingData] = useState<PricingDataState>({
-    units: [],
-    hasFetched: false,
-  })
-
+const buildAccordions = (units: RailsListingPricingTableUnit[], listingIsSale: boolean) => {
   let mappedUnitsByOccupancy: MappedUnitsByOccupancy[] = []
 
-  useEffect(() => {
-    if (listing.Id) {
-      void getListingPricingTableUnits(listing.Id).then((units: RailsListingPricingTableUnit[]) => {
-        setPricingData({ units, hasFetched: true })
-      })
-    }
-
-    return () => {
-      setPricingData({ units: [], hasFetched: false })
-    }
-  }, [listing.Id])
-
-  if (pricingData.units?.length) {
-    mappedUnitsByOccupancy = classifyPricingDataByOccupancy(pricingData.units)
+  if (units?.length) {
+    mappedUnitsByOccupancy = classifyPricingDataByOccupancy(units)
   }
 
-  if (!listingIsHabitat) {
-    pricingDataContent = mappedUnitsByOccupancy?.map(
-      (unitsByOccupancy: MappedUnitsByOccupancy, index, array) => {
-        const accordionLength = array.length
+  return mappedUnitsByOccupancy?.map((unitsByOccupancy: MappedUnitsByOccupancy, index, array) => {
+    const accordionLength = array.length
 
-        const categoryData = unitsByOccupancy.summaryByAMI.map(
-          (unitsSummaryByAMI: SummaryByAMI) => {
-            const responsiveTableRows = unitsSummaryByAMI.summaryByType.map(
-              (unitSummary: RailsListingPricingTableUnit) => {
-                return listingIsSale
-                  ? {
-                      units: {
-                        cellText: unitSummary.unitType,
-                        cellSubText: `${unitSummary.availability} ${t("t.available")}`,
-                      },
-                      income: {
-                        cellText: `$${unitSummary.absoluteMinIncome} to $${unitSummary.absoluteMaxIncome}`,
-                        cellSubText: t("t.perMonth"),
-                      },
-                      sale: [
-                        {
-                          cellText: `$${unitSummary.maxPriceWithParking}`,
-                          cellSubText: "with parking",
-                        },
-                        {
-                          cellText: `$${unitSummary.maxPriceWithoutParking}`,
-                          cellSubText: "without parking",
-                        },
-                      ],
-                      monthlyHoaDues: [
-                        {
-                          cellText: `$${unitSummary.maxHoaDuesWithParking}`,
-                          cellSubText: "with parking",
-                        },
-                        {
-                          cellText: `$${unitSummary.maxHoaDuesWithoutParking}`,
-                          cellSubText: "without parking",
-                        },
-                      ],
-                    }
-                  : {
-                      units: {
-                        cellText: unitSummary.unitType,
-                        cellSubText: `${unitSummary.availability} ${t("t.available")}`,
-                      },
-                      income: {
-                        cellText: `$${unitSummary.absoluteMinIncome} to $${unitSummary.absoluteMaxIncome}`,
-                        cellSubText: t("t.perMonth"),
-                      },
-                      rent: {
-                        cellText: `$${unitSummary.maxMonthlyRent}`,
-                        cellSubText: t("t.perMonth"),
-                      },
-                    }
-              }
-            )
-            return {
-              header: t("listings.stats.upToPercentAmi", {
-                amiPercent: unitsSummaryByAMI.unitMaxAMI,
-              }),
-              tableData: {
-                stackedData: responsiveTableRows,
-                headers: responsiveTableHeaders,
-              },
-            }
+    const categoryData = unitsByOccupancy.summaryByAMI.map((unitsSummaryByAMI: SummaryByAMI) => {
+      const responsiveTableRows = unitsSummaryByAMI.summaryByType.map(
+        (unitSummary: RailsListingPricingTableUnit) => {
+          return listingIsSale ? buildSaleCells(unitSummary) : buildRentalCells(unitSummary)
+        }
+      )
+
+      const responsiveTableHeaders = listingIsSale
+        ? {
+            units: { name: "t.unitType" },
+            income: { name: "t.incomeRange" },
+            sale: { name: "listings.stats.salesPrice" },
+            monthlyHoaDues: { name: "Monthly HOA Dues" },
           }
-        )
+        : {
+            units: { name: "t.unitType" },
+            income: { name: "t.incomeRange" },
+            rent: { name: "t.rent" },
+          }
 
-        return (
-          <ContentAccordion
-            key={index}
-            initialOpen={accordionLength === 1}
-            customBarContent={
-              <span className={"flex w-full justify-between items-center"}>
-                <span className={"flex items-center"}>
-                  {unitsByOccupancy.occupancy > 1
-                    ? `${unitsByOccupancy.occupancy} ${t("listings.stats.numInHouseholdPlural")}`
-                    : `${unitsByOccupancy.occupancy} ${t("listings.stats.numInHouseholdSingular")}`}
-                </span>
-                <span className={"flex items-center mr-2"}>
-                  {`Income $${unitsByOccupancy.absoluteMinIncome} ${t("t.to")} $${
-                    unitsByOccupancy.absoluteMaxIncome
-                  } ${t("t.perMonth")}`}
-                </span>
-              </span>
-            }
-            customExpandedContent={
-              <div className={"p-4 border-2 border-gray-400"}>
-                <CategoryTable categoryData={categoryData} />
-              </div>
-            }
-            accordionTheme={"gray"}
-            barClass={"mt-4"}
-          />
-        )
+      return {
+        header: t("listings.stats.upToPercentAmi", {
+          amiPercent: unitsSummaryByAMI.unitMaxAMI,
+        }),
+        tableData: {
+          stackedData: responsiveTableRows,
+          headers: responsiveTableHeaders,
+        },
       }
+    })
+
+    return (
+      <ContentAccordion
+        key={index}
+        initialOpen={accordionLength === 1}
+        customBarContent={
+          <span className={"flex w-full justify-between items-center"}>
+            <span className={"flex items-center"}>
+              {unitsByOccupancy.occupancy > 1
+                ? `${unitsByOccupancy.occupancy} ${t("listings.stats.numInHouseholdPlural")}`
+                : `${unitsByOccupancy.occupancy} ${t("listings.stats.numInHouseholdSingular")}`}
+            </span>
+            <span className={"flex items-center mr-2"}>
+              {`Income $${unitsByOccupancy.absoluteMinIncome} to  $${unitsByOccupancy.absoluteMaxIncome} per month`}
+            </span>
+          </span>
+        }
+        customExpandedContent={
+          <div className={"p-4 border-2 border-gray-400"}>
+            <CategoryTable categoryData={categoryData} />
+          </div>
+        }
+        accordionTheme={"gray"}
+        barClass={"mt-4"}
+      />
     )
-  } else {
+  })
+}
+
+const buildContent = (
+  pricingDataState: ListingDetailsPricingTableState,
+  listingIsSale: boolean,
+  listingIsHabitat: boolean
+) => {
+  let mappedUnitsByOccupancy: MappedUnitsByOccupancy[] = []
+
+  if (!pricingDataState.hasFetched) {
+    return <Icon symbol="spinner" size="large" />
+  }
+
+  if (pricingDataState.units?.length) {
+    mappedUnitsByOccupancy = classifyPricingDataByOccupancy(pricingDataState.units)
+  }
+
+  if (listingIsSale) {
+    return buildAccordions(pricingDataState.units, true)
+  }
+
+  if (listingIsHabitat) {
     const habitatStrings = mappedUnitsByOccupancy.map((unitByOccupancy) => {
       return `${unitByOccupancy.occupancy} people household: $${unitByOccupancy.absoluteMinIncome} to $${unitByOccupancy.absoluteMaxIncome}`
     })
 
-    pricingDataContent = (
+    return (
       <>
         <p>{t("listings.habitat.incomeRange.p4")}</p>
         <ul>
@@ -185,16 +185,33 @@ export const ListingDetailsPricingTable = ({ listing }: ListingDetailsPricingTab
       </>
     )
   }
+  return buildAccordions(pricingDataState.units, false)
+}
 
-  const loadingContent = (
-    <div className="flex justify-center md:my-6 md:pr-8 md:px-0 md:w-2/3 px-3 w-full">
-      <Icon symbol="spinner" size="large" />
-    </div>
-  )
+export const ListingDetailsPricingTable = ({ listing }: ListingDetailsPricingTableProps) => {
+  const listingIsSale = isSale(listing)
+  const listingIsHabitat = isHabitatListing(listing)
+
+  const [pricingDataState, setPricingDataState] = useState<ListingDetailsPricingTableState>({
+    units: [],
+    hasFetched: false,
+  })
+
+  useEffect(() => {
+    if (listing.Id) {
+      void getListingPricingTableUnits(listing.Id).then((units: RailsListingPricingTableUnit[]) => {
+        setPricingDataState({ units, hasFetched: true })
+      })
+    }
+
+    return () => {
+      setPricingDataState({ units: [], hasFetched: false })
+    }
+  }, [listing.Id])
 
   return (
-    <div className="md:my-6 md:pr-8 md:px-0 md:w-2/3 px-3 w-full">
-      {pricingData.hasFetched ? pricingDataContent : loadingContent}
+    <div className={`${!pricingDataState.hasFetched ? "flex justify-center" : ""} md:my-6 md:pr-8 md:px-0 md:w-2/3 px-3 w-full`}>
+      {buildContent(pricingDataState, listingIsSale, listingIsHabitat)}
     </div>
   )
 }
