6fe4565f72d6f3fd916bc0bf6d958b82dc2a9067
Chad Brokaw <chad.brokaw@exygy.com>
2023-02-23 16:48:47 -0700

    Add anchor test for doc checklist

--- app/javascript/pages/getAssistance/document-checklist.tsx
+++ app/javascript/pages/getAssistance/document-checklist.tsx
@@ -3,7 +3,17 @@ import Layout from "../../layouts/Layout"
 import withAppSetup from "../../layouts/withAppSetup"
 
 const DocumentChecklist = () => {
-  return <Layout title={"Document Checklist"}>{<h1>Document Checklist</h1>}</Layout>
+  return (
+    <Layout title={"Document Checklist"}>
+      {
+        <div className="flex flex-col justify-around h-screen">
+          <h1>Document Checklist</h1>
+
+          <h2 id="anchorMe">I'm an anchor</h2>
+        </div>
+      }
+    </Layout>
+  )
 }
 
 export default withAppSetup(DocumentChecklist)
