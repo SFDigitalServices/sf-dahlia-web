3b24defb9625a9d6e2e88bdbeb24d86427226a84
Chad Brokaw <chad.brokaw@exygy.com>
2023-03-02 09:29:46 -0700

    Revert test back

--- cypress/e2e/listingDetails/machineTranslations.e2e.ts
+++ cypress/e2e/listingDetails/machineTranslations.e2e.ts
@@ -1,6 +1,5 @@
 function verifyMachineTranslations(language, id, translations) {
   cy.visit(`${language}/listings/${id}?react=true`)
-  cy.wait(3000)
   translations.forEach((text) => {
     cy.contains(text)
   })
@@ -43,7 +42,7 @@ const PARKING_TEXT = {
 }
 
 describe("Listing Details Machine Translations", () => {
-  beforeEach(() => {
+  afterEach(() => {
     // TODO: remove me once this is fixed. we shouldn't have to wait in between tests, but
     // there is a rogue loading issue beyond the scope of this story
     cy.wait(3000)
