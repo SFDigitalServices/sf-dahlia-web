a955004da4b391ce082c47882856a80464795ccd
Mark Buckner <markbuckner@Marks-MacBook-Pro.local>
2023-03-06 09:41:57 -0700

    DAH-1175 uptake bloom; add optional operators

--- app/javascript/modules/listingDetails/ListingDetailsPricingTable.tsx
+++ app/javascript/modules/listingDetails/ListingDetailsPricingTable.tsx
@@ -31,10 +31,10 @@ const buildSaleCells = (unitSummary: RailsListingPricingTableUnit) => {
   return {
     units: {
       cellText: unitSummary.unitType,
-      cellSubText: `${unitSummary.availability} ${t("t.available")}`,
+      cellSubText: `${unitSummary?.availability} ${t("t.available")}`,
     },
     income: {
-      cellText: `$${unitSummary.absoluteMinIncome.toLocaleString()} to $${unitSummary.absoluteMaxIncome.toLocaleString()}`,
+      cellText: `$${unitSummary.absoluteMinIncome?.toLocaleString()} to $${unitSummary.absoluteMaxIncome?.toLocaleString()}`,
       cellSubText: t("t.perMonth"),
     },
     // sale: {
@@ -47,21 +47,21 @@ const buildSaleCells = (unitSummary: RailsListingPricingTableUnit) => {
     // },
     sale: [
       {
-        cellText: `$${unitSummary.maxPriceWithParking.toLocaleString()}`,
+        cellText: `$${unitSummary.maxPriceWithParking?.toLocaleString()}`,
         cellSubText: "with parking",
       },
       {
-        cellText: `$${unitSummary.maxPriceWithoutParking.toLocaleString()}`,
+        cellText: `$${unitSummary.maxPriceWithoutParking?.toLocaleString()}`,
         cellSubText: "without parking",
       },
     ],
     monthlyHoaDues: [
       {
-        cellText: `$${unitSummary.maxHoaDuesWithParking.toLocaleString()}`,
+        cellText: `$${unitSummary.maxHoaDuesWithParking?.toLocaleString()}`,
         cellSubText: "with parking",
       },
       {
-        cellText: `$${unitSummary.maxHoaDuesWithoutParking.toLocaleString()}`,
+        cellText: `$${unitSummary.maxHoaDuesWithoutParking?.toLocaleString()}`,
         cellSubText: "without parking",
       },
     ],
@@ -92,68 +92,70 @@ const buildAccordions = (units: RailsListingPricingTableUnit[], listingIsSale: b
     mappedUnitsByOccupancy = classifyPricingDataByOccupancy(units)
   }
 
-  return mappedUnitsByOccupancy?.map((unitsByOccupancy: MappedUnitsByOccupancy, index: number) => {
-    // const accordionLength = array.length
-    const categoryData = unitsByOccupancy.summaryByAMI.map((unitsSummaryByAMI: SummaryByAMI) => {
-      const responsiveTableRows = unitsSummaryByAMI.summaryByType.map(
-        (unitSummary: RailsListingPricingTableUnit) => {
-          return listingIsSale ? buildSaleCells(unitSummary) : buildRentalCells(unitSummary)
+  return mappedUnitsByOccupancy?.map(
+    (unitsByOccupancy: MappedUnitsByOccupancy, index: number, array) => {
+      const accordionLength = array.length
+      const categoryData = unitsByOccupancy.summaryByAMI.map((unitsSummaryByAMI: SummaryByAMI) => {
+        const responsiveTableRows = unitsSummaryByAMI.summaryByType.map(
+          (unitSummary: RailsListingPricingTableUnit) => {
+            return listingIsSale ? buildSaleCells(unitSummary) : buildRentalCells(unitSummary)
+          }
+        )
+
+        const responsiveTableHeaders = listingIsSale
+          ? {
+              units: { name: "t.unitType" },
+              income: { name: "shortFormNav.income" },
+              sale: { name: "listings.stats.salesPrice" },
+              monthlyHoaDues: { name: "Monthly HOA Dues" },
+            }
+          : {
+              units: { name: "t.unitType" },
+              income: { name: "t.incomeRange" },
+              rent: { name: "t.rent" },
+            }
+
+        return {
+          header: t("listings.stats.upToPercentAmi", {
+            amiPercent: unitsSummaryByAMI.unitMaxAMI,
+          }),
+          tableData: {
+            stackedData: responsiveTableRows,
+            headers: responsiveTableHeaders,
+          },
         }
+      })
+
+      return (
+        <ContentAccordion
+          key={index}
+          initialOpen={accordionLength === 1}
+          customBarContent={
+            <span className={"flex w-full justify-between items-center"}>
+              <span className={"flex items-center"}>
+                {unitsByOccupancy.occupancy > 1
+                  ? `${unitsByOccupancy.occupancy} ${t("listings.stats.numInHouseholdPlural")}`
+                  : `${unitsByOccupancy.occupancy} ${t("listings.stats.numInHouseholdSingular")}`}
+              </span>
+              <span className={"flex items-center mr-2"}>
+                {t("listings.incomeRange.minMaxPerMonth", {
+                  min: unitsByOccupancy.absoluteMinIncome?.toLocaleString(),
+                  max: unitsByOccupancy.absoluteMaxIncome?.toLocaleString(),
+                })}
+              </span>
+            </span>
+          }
+          customExpandedContent={
+            <div className={"p-4 border-2 border-gray-400"}>
+              <CategoryTable categoryData={categoryData} />
+            </div>
+          }
+          accordionTheme={"gray"}
+          barClass={"mt-4"}
+        />
       )
-
-      const responsiveTableHeaders = listingIsSale
-        ? {
-            units: { name: "t.unitType" },
-            income: { name: "shortFormNav.income" },
-            sale: { name: "listings.stats.salesPrice" },
-            monthlyHoaDues: { name: "Monthly HOA Dues" },
-          }
-        : {
-            units: { name: "t.unitType" },
-            income: { name: "t.incomeRange" },
-            rent: { name: "t.rent" },
-          }
-
-      return {
-        header: t("listings.stats.upToPercentAmi", {
-          amiPercent: unitsSummaryByAMI.unitMaxAMI,
-        }),
-        tableData: {
-          stackedData: responsiveTableRows,
-          headers: responsiveTableHeaders,
-        },
-      }
-    })
-
-    return (
-      <ContentAccordion
-        key={index}
-        // initialOpen={accordionLength === 1}
-        customBarContent={
-          <span className={"flex w-full justify-between items-center"}>
-            <span className={"flex items-center"}>
-              {unitsByOccupancy.occupancy > 1
-                ? `${unitsByOccupancy.occupancy} ${t("listings.stats.numInHouseholdPlural")}`
-                : `${unitsByOccupancy.occupancy} ${t("listings.stats.numInHouseholdSingular")}`}
-            </span>
-            <span className={"flex items-center mr-2"}>
-              {t("listings.incomeRange.minMaxPerMonth", {
-                min: unitsByOccupancy.absoluteMinIncome.toLocaleString(),
-                max: unitsByOccupancy.absoluteMaxIncome.toLocaleString(),
-              })}
-            </span>
-          </span>
-        }
-        customExpandedContent={
-          <div className={"p-4 border-2 border-gray-400"}>
-            <CategoryTable categoryData={categoryData} />
-          </div>
-        }
-        accordionTheme={"gray"}
-        barClass={"mt-4"}
-      />
-    )
-  })
+    }
+  )
 }
 
 const buildContent = (
@@ -177,7 +179,9 @@ const buildContent = (
 
   if (listingIsHabitat) {
     const habitatStrings = mappedUnitsByOccupancy.map((unitByOccupancy) => {
-      return `${unitByOccupancy.occupancy} people household: $${unitByOccupancy.absoluteMinIncome.toLocaleString()} to $${unitByOccupancy.absoluteMaxIncome.toLocaleString()}`
+      return `${
+        unitByOccupancy.occupancy
+      } people household: $${unitByOccupancy.absoluteMinIncome?.toLocaleString()} to $${unitByOccupancy.absoluteMaxIncome?.toLocaleString()}`
     })
 
     return (
