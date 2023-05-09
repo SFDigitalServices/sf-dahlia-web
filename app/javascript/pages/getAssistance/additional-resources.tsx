c36d7c80e979ca873ae2c2bb680a2b5674e6eb20
Cade Wolcott <cade.wolcott@exygy.com>
2023-05-09 13:42:14 -0500

    DAH-1336 in progress changes to InfoCardGrid

--- app/javascript/pages/getAssistance/additional-resources.tsx
+++ app/javascript/pages/getAssistance/additional-resources.tsx
@@ -1,7 +1,9 @@
 import React from "react"
 import withAppSetup from "../../layouts/withAppSetup"
-import { t } from "@bloom-housing/ui-components"
+import { InfoCard, t } from "@bloom-housing/ui-components"
 import AssistanceLayout from "../../layouts/AssistanceLayout"
+import InfoCardGridAdditionalResources from "./InfoCardGridAdditionalResources"
+import additionalResources from "./additional-resources.json"
 
 const AdditionalResources = () => {
   return (
@@ -9,7 +11,48 @@ const AdditionalResources = () => {
       title={t("assistance.title.additionalHousingOpportunities")}
       subtitle={t("assistance.subtitle.additionalHousingOpportunities")}
     >
-      <h1>{t("assistance.title.additionalHousingOpportunities")}</h1>
+      <div className="flex flex-col mr-8">
+        {/* <h1>{t("assistance.title.additionalHousingOpportunities")}</h1> */}
+        <InfoCardGridAdditionalResources
+          title="San Fransciso Housing Programs"
+          subtitle="Programs run and funded by the City and County of San Francisco"
+          className="mb-4"
+        >
+          {additionalResources.sanFranciscoHousingPrograms.map((resource) => {
+            return (
+              <InfoCard
+                title={resource.title}
+                subtitle={resource.agency}
+                externalHref={resource.externalUrl}
+                className="is-normal-primary-lighter"
+                key={resource.title}
+              >
+                {<div className="text-gray-700 text-xs">{resource.description}</div>}
+              </InfoCard>
+            )
+          })}
+        </InfoCardGridAdditionalResources>
+
+        <InfoCardGridAdditionalResources
+          title="Non-MOHCD housing programs and resources"
+          subtitle="These are resources from other cities and nonprofits. They are not sponsored by the city of San Francisco. We are not able to answer any questions about these resources. Please contact the organizations listed if you have any questions."
+          className="mb-4"
+        >
+          {additionalResources.nonMOHCDHousingPrograms.map((resource) => {
+            return (
+              <InfoCard
+                title={resource.title}
+                subtitle={resource.agency}
+                externalHref={resource.externalUrl}
+                className="is-normal-primary-lighter"
+                key={resource.title}
+              >
+                {<div className="text-gray-700 text-xs translate">{resource.description}</div>}
+              </InfoCard>
+            )
+          })}
+        </InfoCardGridAdditionalResources>
+      </div>
     </AssistanceLayout>
   )
 }
