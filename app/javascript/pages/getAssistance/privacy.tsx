396c82734afcdf1a2bed5ace259e22e4bba7f891
Chad Brokaw <chad.brokaw@exygy.com>
2023-05-18 11:39:24 -0600

    Add pages

---
+++ app/javascript/pages/getAssistance/privacy.tsx
@@ -0,0 +1,14 @@
+import React from "react"
+import Layout from "../../layouts/Layout"
+import withAppSetup from "../../layouts/withAppSetup"
+
+const Privacy = () => {
+  return (
+    <Layout title={"Privacy Policy"}>
+      {/* Add PageHeader Component (Look in AssistanceLayout) */}
+      <h1>Privacy Policy Content :)</h1>
+    </Layout>
+  )
+}
+
+export default withAppSetup(Privacy)
