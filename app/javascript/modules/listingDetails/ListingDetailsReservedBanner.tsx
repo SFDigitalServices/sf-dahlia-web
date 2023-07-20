dba0f1275825635f08d56f0ffcf9a1659310b6b2
Alula Beshue <alula.beshue@sfgov.org>
2023-07-20 09:12:50 -0700

    multiline reserved banner

--- app/javascript/modules/listingDetails/ListingDetailsReservedBanner.tsx
+++ app/javascript/modules/listingDetails/ListingDetailsReservedBanner.tsx
@@ -1,6 +1,7 @@
 import React from "react"
 import { Message, t } from "@bloom-housing/ui-components"
 import { RESERVED_COMMUNITY_TYPES } from "../constants"
+import { renderInlineMarkup, renderMarkup } from "../../util/languageUtil"
 
 export interface ListingDetailsReservedBannerProps {
   reservedCommunityMinimumAge?: number
@@ -25,9 +26,32 @@ export const ListingDetailsReservedBanner = ({
         })
       : t(`listings.allUnitsReservedFor.${reservedCommunityType}`)
 
+  const content = () => (
+    <div style={{textTransform: "none"}}>
+      <p>This is my list</p>
+      <br />
+      <ul style={{listStyleType: "disc", paddingLeft: "1em"}}>
+        <li>hello <b>world</b></li>
+        <li>Good Bye</li>
+      </ul>
+    </div>
+  )
+
+  const contentString = () => (
+     // "<div style={{textTransform: \"none\"}}><p>This is my list</p><br /><ul style={{listStyleType: \"disc\", paddingLeft: \"1em\"}}><li>hello <b>world</b></li><li>Good Bye</li></ul></div>"
+    `
+    This is my list
+
+    - item one
+    - item two
+    `
+  )
+
+  const renderString = () => renderMarkup(contentString(), "<div><p><br><ul><li><b>")
+
   return (
     <div className="md:pr-8 md:w-2/3 mt-4 w-full">
-      <Message warning={true}>{message}</Message>
+      <Message warning={true}>{content()}</Message>
     </div>
   )
 }
