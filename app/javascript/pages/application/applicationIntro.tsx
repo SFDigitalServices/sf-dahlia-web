2cfa036b6dfd1ed74bebfde809229ff83a43f444
Alula Beshue <alula.beshue@sfgov.org>
2023-07-21 08:19:00 -0700

    calling cms from react

--- app/javascript/pages/application/applicationIntro.tsx
+++ app/javascript/pages/application/applicationIntro.tsx
@@ -1,16 +1,33 @@
-import React from "react"
+import React, { useContext, useEffect, useState } from "react"
 import withAppSetup from "../../layouts/withAppSetup"
+import { Cms, CmsItem } from "../../api/types/rails/listings/BaseRailsListing"
+import axios from "axios"
+import { NavigationContext } from "@bloom-housing/ui-components"
+import { getPathWithoutLanguagePrefix } from "../../util/languageUtil"
 
 interface ApplicationIntroPageProps {
   assetPaths: unknown
 }
 
 const ApplicationIntroPage = (_props: ApplicationIntroPageProps) => {
-    console.log("applicatons")
+  const { router } = useContext(NavigationContext)
+  const [cmsItem, setCmsItem] = useState<CmsItem>(null)
 
+  useEffect(() => {
+    const fetchData = async (listingId) => {
+      const response: Cms = await axios.get("http://localhost:8000/api/v2/pages/?locale=en").then(({ data }) => data)
+      const matching: CmsItem[] = response.items.filter(item => item.title === listingId)
+      setCmsItem(matching[0])
+    }
+
+    const listingId: string = getPathWithoutLanguagePrefix(router.pathname).split("/")[2]
+    fetchData(listingId)
+  }, [])
+
+  const display: string = cmsItem ? `The next step is to call ${cmsItem.meta.detail_url}` : "Data not found in CMS"
   return (
     <div>
-        "hello world"
+        {display}
     </div>
   )
 }
