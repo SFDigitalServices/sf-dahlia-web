a65185c941e51d8d7fd6a69454d1c02a882f55ea
Chad Brokaw <chad.brokaw@exygy.com>
2023-07-25 14:20:58 -0600

    fix: Add JSON and fix UI

--- app/javascript/pages/getAssistance/housing-counselors.tsx
+++ app/javascript/pages/getAssistance/housing-counselors.tsx
@@ -1,28 +1,35 @@
-import { t, Icon, IconFillColors, Tag, AppearanceSizeType } from "@bloom-housing/ui-components"
+import {
+  t,
+  Icon,
+  IconFillColors,
+  Tag,
+  AppearanceSizeType,
+  Desktop,
+  Mobile,
+  Button,
+} from "@bloom-housing/ui-components"
 import React from "react"
 import AssistanceLayout from "../../layouts/AssistanceLayout"
 import withAppSetup from "../../layouts/withAppSetup"
 import { faEnvelope } from "@fortawesome/free-solid-svg-icons"
 import "./housing-counselors.scss"
-// Counselors can help you with your DAHLIA application and housing search.
+import housingCounselors from "../../../assets/json/housing_counselors_react.json"
 
-export interface HousingCounselorProps {
-  addressCityState?: string
-  addressStreet?: string
-  languages: string[]
+interface CounselorData {
+  fullName: string
+  shortName?: string
   services: string[]
-  name: string
-  phoneNumber: string
-  emailAddress: string
+  languages: string[]
+  address: string
+  cityState: string
   website: string
-  callButtonText: string
-  emailButtonText: string
-  visitButtonText: string
+  email: string
+  phone: string
 }
 
 const Label = (type: "language" | "service", text: string) => {
   return (
-    <span className="mx-1" key={text}>
+    <span key={text}>
       <Tag className={type} size={AppearanceSizeType.small}>
         {text}
       </Tag>
@@ -30,80 +37,114 @@ const Label = (type: "language" | "service", text: string) => {
   )
 }
 
-const HousingCounselor = (props: HousingCounselorProps) => {
+const HousingCounselor = (housingCounselor: CounselorData) => {
   return (
-    <div className="resource-item text-base m-4">
-      <h3 className="font-sans text-lg pb-2">{props.name}</h3>
-      <p className="text-xs text-gray-800 pb-2">
-        Services Offered:
-        {props.services.map((service) => Label("service", service))}
+    <div className="resource-item text-base m-4" key={housingCounselor.fullName}>
+      <h3 className="font-sans text-lg pb-2">{housingCounselor.fullName}</h3>
+      <p className="text-xs text-gray-800 pb-2 flex flex-wrap gap-1">
+        {t("assistance.housingCounselors.servicesOffered")}
+        {housingCounselor.services.map((service) =>
+          Label("service", t(`assistance.housingCounselors.services.${service}`))
+        )}
       </p>
-      <p className="text-xs text-gray-800 pb-4">
-        Languages Spoken:
-        {props.languages.map((language) => Label("language", language))}
+      <p className="text-xs text-gray-800 pb-4 flex flex-wrap gap-1">
+        {t("assistance.housingCounselors.languagesSpoken")}
+        {housingCounselor.languages.map((language) =>
+          Label("language", t(`assistance.housingCounselors.services.languages.${language}`))
+        )}
       </p>
 
       <p className="icon-item pb-2 flex">
         <Icon className="address-icon" symbol="map" size="medium" />
         <span>
-          {props.addressStreet} <br /> {props.addressCityState}
+          {housingCounselor.address} <br /> {housingCounselor.cityState}
         </span>
       </p>
-
-      <a className="icon-item pb-2" href={`tel:+1${props.phoneNumber}`}>
-        <Icon
-          className="counselor-icon"
-          symbol="phone"
-          size="medium"
-          fill={IconFillColors.primary}
-        />
-        {props.callButtonText}
-      </a>
-
-      <a className="icon-item pb-2" href={`mailto:${props.emailAddress}`}>
-        <Icon
-          className="counselor-icon"
-          symbol={faEnvelope}
-          size="medium"
-          fill={IconFillColors.primary}
-        />
-        {props.emailButtonText}
-      </a>
-
-      {props.website && (
-        <a className="icon-item" target="_blank" href={props.website}>
+      <Mobile>
+        <div className="flex flex-col gap-2 mt-4">
+          <Button
+            icon="phone"
+            iconPlacement="left"
+            size={AppearanceSizeType.small}
+            iconSize="medium"
+          >
+            {t("assistance.housingCounselors.counselor.call.upper", {
+              phoneNumber: housingCounselor.phone,
+            })}
+          </Button>
+          <Button
+            size={AppearanceSizeType.small}
+            icon={faEnvelope}
+            iconPlacement="left"
+            iconSize="medium"
+          >
+            {t("assistance.housingCounselors.counselor.email.upper", {
+              counselorName: housingCounselor.shortName || housingCounselor.fullName,
+            })}
+          </Button>
+          <Button
+            size={AppearanceSizeType.small}
+            icon="link"
+            iconPlacement="left"
+            iconSize="medium"
+          >
+            {t("assistance.housingCounselors.counselor.visitWebsite.upper", {
+              counselorName: housingCounselor.shortName || housingCounselor.fullName,
+            })}
+          </Button>
+        </div>
+      </Mobile>
+      <Desktop>
+        <a className="icon-item pb-2" href={`tel:+1${housingCounselor.phone}`}>
           <Icon
             className="counselor-icon"
-            symbol="link"
+            symbol="phone"
             size="medium"
             fill={IconFillColors.primary}
           />
-          {props.visitButtonText}
+          {t("assistance.housingCounselors.counselor.call.lower", {
+            phoneNumber: housingCounselor.phone,
+          })}
         </a>
-      )}
+
+        <a className="icon-item pb-2" href={`mailto:${housingCounselor.email}`}>
+          <Icon
+            className="counselor-icon"
+            symbol={faEnvelope}
+            size="medium"
+            fill={IconFillColors.primary}
+          />
+          {t("assistance.housingCounselors.counselor.email.lower", {
+            counselorEmail: housingCounselor.email,
+          })}
+        </a>
+
+        {housingCounselor.website && (
+          <a className="icon-item" target="_blank" href={housingCounselor.website}>
+            <Icon
+              className="counselor-icon"
+              symbol="link"
+              size="medium"
+              fill={IconFillColors.primary}
+            />
+            {t("assistance.housingCounselors.counselor.visitWebsite.lower", {
+              counselorName: housingCounselor.shortName || housingCounselor.fullName,
+            })}
+          </a>
+        )}
+      </Desktop>
     </div>
   )
 }
 
 const HousingCounselors = () => {
+  console.log(housingCounselors)
   return (
     <AssistanceLayout
       title={t("assistance.title.housingCouneslors")}
       subtitle={t("assistance.subtitle.housingCouneslors")}
     >
-      <HousingCounselor
-        name="ASIAN INC"
-        languages={["Cantonese", "English", "Mandarin"]}
-        services={["Rental", "Ownership"]}
-        callButtonText="Call (415) 928–5910"
-        phoneNumber={"4159285910"}
-        emailAddress="housing.counseling@asianinc.org"
-        website="https://www.asianinc.org/housing/"
-        emailButtonText="Email housing.counseling@asianinc.org"
-        visitButtonText="Visit ASIAN INC"
-        addressCityState="San Francisco, CA 94103"
-        addressStreet="1167 Mission Street, 4th Floor"
-      />
+      {housingCounselors.counselors.filter(() => true).map((item) => HousingCounselor(item))}
     </AssistanceLayout>
   )
 }
