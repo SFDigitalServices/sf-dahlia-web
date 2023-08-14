dac2d5bfa45dba6007fc81e2abf0df438f2e9976
chadbrokaw-exygy <122493877+chadbrokaw-exygy@users.noreply.github.com>
2023-08-14 13:16:19 -0600

    DAH-1446: Multiple Images on Listing Detail Page (#1890)

--- app/javascript/__tests__/pages/listings/ForRent.test.tsx
+++ app/javascript/__tests__/pages/listings/ForRent.test.tsx
@@ -1,6 +1,7 @@
 import React from "react"
-import { render, cleanup } from "@testing-library/react"
+import { render, cleanup, screen, waitFor } from "@testing-library/react"
 import ForRent from "../../../../javascript/pages/listings/for-rent"
+import { sroRentalListing } from "../../data/RailsRentalListing/listing-rental-sro"
 
 const axios = require("axios")
 
@@ -20,6 +21,29 @@ describe("For Rent", () => {
 
     expect(await findByText("Rent affordable housing")).toBeDefined()
     expect(asFragment()).toMatchSnapshot()
+    done()
+  })
+
+  it("listings with multiple listings render the first image in the array", async (done) => {
+    axios.get.mockResolvedValue({ data: { listings: [sroRentalListing] } })
+
+    const { findByAltText } = render(<ForRent assetPaths="/" />)
+
+    await waitFor(
+      () => {
+        return screen
+          .getByRole("button", {
+            name: /show upcoming lotteries \(1\)/i,
+          })
+          .click()
+      },
+      {
+        timeout: 10000,
+      }
+    )
+
+    const image = await findByAltText(`${sroRentalListing.Building_Name} Building`)
+    expect(image.getAttribute("src")).toBe(sroRentalListing.Listing_Images[0].Image_URL)
     done()
   })
 })
--- app/javascript/__tests__/pages/listings/ForSale.test.tsx
+++ app/javascript/__tests__/pages/listings/ForSale.test.tsx
@@ -1,6 +1,7 @@
 import React from "react"
 import { render, cleanup } from "@testing-library/react"
 import ForSale from "../../../pages/listings/for-sale"
+import { openSaleListing } from "../../data/RailsSaleListing/listing-sale-open"
 
 const axios = require("axios")
 
@@ -20,6 +21,16 @@ describe("For Sale", () => {
 
     expect(await findByText("Buy affordable housing")).toBeDefined()
     expect(asFragment()).toMatchSnapshot()
+    done()
+  })
+
+  it("listings with multiple listings render the first image in the array", async (done) => {
+    axios.get.mockResolvedValue({ data: { listings: [openSaleListing] } })
+
+    const { findByAltText } = render(<ForSale assetPaths="/" />)
+
+    const image = await findByAltText(`${openSaleListing.Building_Name} Building`)
+    expect(image.getAttribute("src")).toBe(openSaleListing.Listing_Images[0].Image_URL)
     done()
   })
 })
