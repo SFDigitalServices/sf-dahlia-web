dcf8b14a5c6b3c306a120c440a27b94c2025ac99
cade-exygy <131277283+cade-exygy@users.noreply.github.com>
2023-08-18 14:29:54 -0500

    DAH-1617 Fix a11y contrast for listing detail HMI table (#1898)
    * fix color contrast
    
    * exclude snapshots from rails lint

--- app/javascript/modules/listingDetails/ListingDetailsHMITable.tsx
+++ app/javascript/modules/listingDetails/ListingDetailsHMITable.tsx
@@ -231,7 +231,7 @@ export const ListingDetailsHMITable = ({ listing }: ListingDetailsEligibilityPro
       {toShowButton(maxOccupancy, minOccupancy, hmiCutoff) && (
         <Button
           inlineIcon="right"
-          className="font-medium md:bg-primary-lighter mt-3"
+          className="font-medium md:bg-primary-lighter mt-3 text-primary-dark"
           iconSize="small"
           icon={tableCollapsed ? faAngleDown : faAngleUp}
           onClick={expandTableHandler}
