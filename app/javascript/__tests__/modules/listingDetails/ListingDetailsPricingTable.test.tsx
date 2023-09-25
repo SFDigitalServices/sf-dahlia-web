8849f7a271aab9996f16521ef7cbd2c76fe68660
Chad Brokaw <36685920+chadbrokaw@users.noreply.github.com>
2023-09-25 14:49:33 -0600

    Add test for open rental listing

--- app/javascript/__tests__/modules/listingDetails/ListingDetailsPricingTable.test.tsx
+++ app/javascript/__tests__/modules/listingDetails/ListingDetailsPricingTable.test.tsx
@@ -1,5 +1,5 @@
 import React from "react"
-import { render, cleanup } from "@testing-library/react"
+import { render, cleanup, screen, fireEvent } from "@testing-library/react"
 import { ListingDetailsPricingTable } from "../../../modules/listingDetails/ListingDetailsPricingTable"
 import { closedRentalListing } from "../../data/RailsRentalListing/listing-rental-closed"
 import { habitatListing } from "../../data/RailsSaleListing/listing-sale-habitat"
@@ -7,12 +7,46 @@ import { openSaleListing } from "../../data/RailsSaleListing/listing-sale-open"
 import { units, unitsWithOneOccupant } from "../../data/RailsListingUnits/listing-units"
 import { amiCharts } from "../../data/RailsAmiCharts/ami-charts"
 import ListingDetailsContext from "../../../contexts/listingDetails/listingDetailsContext"
+import {
+  openRentalListing,
+  openRentalListingUnits,
+  openRentalListingAmis,
+} from "../../data/RailsRentalListing/listing-rental-open"
 
 describe("ListingDetailsPricingTable", () => {
   afterEach(() => {
     cleanup()
     jest.clearAllMocks()
     jest.resetAllMocks()
+  })
+
+  describe("open rental listing", () => {
+    it("renders ListingDetailsPricingTable component with open rental listing", () => {
+      const { getByText } = render(
+        <ListingDetailsContext.Provider
+          value={{
+            units: openRentalListingUnits,
+            amiCharts: openRentalListingAmis,
+            fetchingUnits: false,
+            fetchedUnits: true,
+            fetchingAmiCharts: false,
+            fetchedAmiCharts: true,
+            fetchingAmiChartsError: undefined,
+            fetchingUnitsError: undefined,
+          }}
+        >
+          <ListingDetailsPricingTable listing={openRentalListing} />
+        </ListingDetailsContext.Provider>
+      )
+
+      fireEvent.click(getByText("$2,364 to $6,725"))
+
+      screen.logTestingPlaygroundURL()
+
+      expect(getByText("$2,364 to $6,725")).toBeInTheDocument()
+
+      // expect(asFragment()).toMatchSnapshot()
+    })
   })
 
   // eslint-disable-next-line @typescript-eslint/require-await
