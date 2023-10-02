d5c418e5577f3ee5e6d31a06c808dedd128ee66c
cliu02 <32694266+cliu02@users.noreply.github.com>
2023-10-02 09:07:22 -0700

    Add test

--- app/javascript/__tests__/modules/listings/TableSubHeader.test.tsx
+++ app/javascript/__tests__/modules/listings/TableSubHeader.test.tsx
@@ -1,10 +1,17 @@
 import React from "react"
 import { render } from "@testing-library/react"
 import TableSubHeader from "../../../modules/listings/TableSubHeader"
+import { t } from "@bloom-housing/ui-components"
+import {
+  rentalEducatorListing1,
+  rentalEducatorListing2,
+} from "../../data/RailsRentalListing/listing-rental-educator"
 
 describe("TableSubHeader", () => {
-  it("renders TableSubHeader component", () => {
-    const { asFragment } = render(<TableSubHeader priorityTypes={["test"]} />)
+  it("renders the priority units for Shirley Chisholm listing 2", async (done) => {
+    const { asFragment, findByText } = render(<TableSubHeader listing={rentalEducatorListing2} />)
+    expect(await findByText(t("listings.customListingType.educator.priorityUnits"))).toBeDefined()
     expect(asFragment()).toMatchSnapshot()
+    done()
   })
 })
