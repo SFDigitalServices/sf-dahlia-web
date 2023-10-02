027f55e598089069c04d9e0ee07ff3d7646d0897
cliu02 <32694266+cliu02@users.noreply.github.com>
2023-10-02 12:20:04 -0700

    Add unit test

--- app/javascript/__tests__/modules/listings/TableSubHeader.test.tsx
+++ app/javascript/__tests__/modules/listings/TableSubHeader.test.tsx
@@ -3,11 +3,30 @@ import { render } from "@testing-library/react"
 import TableSubHeader from "../../../modules/listings/TableSubHeader"
 import { t } from "@bloom-housing/ui-components"
 import { rentalEducatorListing2 } from "../../data/RailsRentalListing/listing-rental-educator"
+import RailsRentalListing from "../../../api/types/rails/listings/RailsRentalListing"
 
 describe("TableSubHeader", () => {
   it("renders the priority units for Shirley Chisholm listing 2", async (done) => {
     const { asFragment, findByText } = render(<TableSubHeader listing={rentalEducatorListing2} />)
     expect(await findByText(t("listings.customListingType.educator.priorityUnits"))).toBeDefined()
+    expect(asFragment()).toMatchSnapshot()
+    done()
+  })
+
+  it("renders the priority units for Shirley Chisholm listing 2 with other priority units", async (done) => {
+    const testListing = {
+      Custom_Listing_Type: "Educator 2: SFUSD employees & public",
+      prioritiesDescriptor: [
+        {
+          name: "Mobility/Hearing/Vision impairments",
+        },
+      ],
+    }
+    const { asFragment, findByText } = render(
+      <TableSubHeader listing={testListing as RailsRentalListing} />
+    )
+    expect(await findByText(t("listings.customListingType.educator.priorityUnits"))).toBeDefined()
+    expect(await findByText(t("listings.prioritiesDescriptor.mobilityHearingVision"))).toBeDefined()
     expect(asFragment()).toMatchSnapshot()
     done()
   })
