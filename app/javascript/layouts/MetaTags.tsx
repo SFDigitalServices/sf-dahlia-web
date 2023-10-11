5c61c57dcf4f4f83ce4b4c097b2cbdfbd795b8eb
Jim Lin <jim.lin@sfgov.org>
2023-10-11 14:12:01 -0700

    fix: move apple favicons to server-side

--- app/javascript/layouts/MetaTags.tsx
+++ app/javascript/layouts/MetaTags.tsx
@@ -33,7 +33,7 @@ const MetaTags = (props: MetaTagsProps) => {
           content={props.image || getAssetPath("dahlia_social-media-preview.jpg")}
         />
         <meta property="og:type" content="website" />
-        <link href={getAssetPath("favicon.ico")} rel="icon" type="image/x-icon" />
+        {/* react-helmet-async does not work with Safari, rely on server-side favicon tags for now in app/views/layouts/application-react.html.slim
         <link href={getAssetPath("favicon-32x32.png")} rel="icon" sizes="32x32" type="image/png" />
         <link href={getAssetPath("favicon-96x96.png")} rel="icon" sizes="96x96" type="image/png" />
         <link href={getAssetPath("favicon-16x16.png")} rel="icon" sizes="16x16" type="image/png" />
@@ -66,6 +66,7 @@ const MetaTags = (props: MetaTagsProps) => {
           rel="apple-touch-icon"
           sizes="180x180"
         />
+        */}
         <link
           href={getAssetPath("android-icon-192x192.png")}
           rel="icon"
