11213b3eafbbd60f1a8c0457bc08ec3ee4be0c8a
Cade Wolcott <cade.wolcott@exygy.com>
2023-11-15 14:29:53 -0600

    DAH-1869 fix: sort open houses with sortBy

--- app/javascript/modules/listingDetailsAside/ListingDetailsOpenHouses.tsx
+++ app/javascript/modules/listingDetailsAside/ListingDetailsOpenHouses.tsx
@@ -5,6 +5,7 @@ import type { ListingEvent } from "../../api/types/rails/listings/BaseRailsListi
 import { localizedFormat } from "../../util/languageUtil"
 import { getEventTimeString } from "../../util/listingUtil"
 import "./ListingDetailsOpenHouses.scss"
+import { sortBy } from "lodash"
 
 export interface OpenHousesProps {
   listing: RailsListing
@@ -17,13 +18,15 @@ export const ListingDetailsOpenHouses = ({ listing }: OpenHousesProps) => {
         <div className="border-b border-gray-400 md:border-b-0">
           <EventSection
             dateClassName="normal-case"
-            events={listing.Open_Houses?.map((openHouse: ListingEvent) => {
-              return {
-                dateString: openHouse.Date && localizedFormat(openHouse.Date, "LL"),
-                timeString: getEventTimeString(openHouse),
-                note: getEventNote(openHouse),
+            events={sortBy(listing.Open_Houses, ["Date", "Start_Time"])?.map(
+              (openHouse: ListingEvent) => {
+                return {
+                  dateString: openHouse.Date && localizedFormat(openHouse.Date, "LL"),
+                  timeString: getEventTimeString(openHouse),
+                  note: getEventNote(openHouse),
+                }
               }
-            })}
+            )}
             sectionHeader={true}
             headerText={t("label.openHouses")}
           />
