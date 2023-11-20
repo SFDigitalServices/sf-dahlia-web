1c27a3fbe816feb58d547ac783c4d66b6f84a78d
Cade Wolcott <cade.wolcott@exygy.com>
2023-11-20 10:17:57 -0600

    DAH-1869 add null check

--- app/javascript/util/listingUtil.ts
+++ app/javascript/util/listingUtil.ts
@@ -169,11 +169,14 @@ export const getEventTimeString = (listingEvent: ListingEvent) => {
   return ""
 }
 
-const formatEventTime = (startTime: string) => {
-  const hour = Number.parseInt(startTime, 10)
-  const suffix = hour >= 12 ? "PM" : "AM"
-  const formattedHour = hour > 12 ? hour - 12 : hour
-  return `${formattedHour}:00 ${suffix}`
+const formatEventTime = (eventTime: string) => {
+  if (eventTime) {
+    const hour = Number.parseInt(eventTime, 10)
+    const suffix = hour >= 12 ? "PM" : "AM"
+    const formattedHour = hour > 12 ? hour - 12 : hour
+    return `${formattedHour}:00 ${suffix}`
+  }
+  return ""
 }
 
 export const getEventDateTime = (eventDate: string, eventTime: string) => {
