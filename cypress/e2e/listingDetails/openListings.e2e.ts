6818ed8f43d681aa9a6a2fc20135995772afb876
cade-exygy <131277283+cade-exygy@users.noreply.github.com>
2023-11-20 09:24:54 -0600

    DAH-1856 fix: add translate class to ImageCard, fix tests (#2004)
    * DAH-1856 fix: add translate class to ImageCard, fix tests
    
    * DAH-1856 fix: change alt text to equal prod listing image alt text
    
    * DAH-1856 fix: snapshot tests
    
    * DAH-1856 fix: alt text for sales listing
    
    * DAH-1856 fix: clean up unit tests
    
    * DAH-1856 fix: update snapshot after yarn install
    
    * DAH-1856 pass translate class to innerClassName prop
    
    * DAH-1856 fix: update snapshot test
    
    * DAH-1856 fix: correct language
    
    * DAH-1856 fix: get latest bloom uic
    
    * update yarn lock and fix snapshot
    
    ---------
    
    Co-authored-by: Chad Brokaw <36685920+chadbrokaw@users.noreply.github.com>

--- cypress/e2e/listingDetails/openListings.e2e.ts
+++ cypress/e2e/listingDetails/openListings.e2e.ts
@@ -30,25 +30,18 @@ const testListings = {
   OPEN_RENTAL: {
     id: "a0W0P00000F8YG4UAN",
     title: "TEST Automated Listing (do not modify)",
+    alt: "Listing Name:TEST Automated Listing (do not modify), Address:San Francisco CA,",
   },
   OPEN_SALE: {
     id: "a0W0P00000GlKfBUAV",
     address: "1 South Van Ness Ave, San Francisco, CA 94103",
     title: "TEST Sale Listing (do not modify) - Homeownership Acres",
+    alt: "Listing Name:TEST Sale Listing (do not modify) - Homeownership Acres, Address:1 South Van Ness Ave, San Francisco CA, 94103",
   },
-}
-const ALT_PHOTO_TEXT = {
-  en: "A picture of the building",
-  es: "Una foto del edificio",
-  tl: "Isang larawan ng gusali",
-  zh: "建築物照片",
 }
 
 const APPLY_BUTTON_TEXT = {
   en: "Apply Online",
-  es: "Presente su solicitud en línea",
-  tl: "Mag-aplay Online",
-  zh: "網上申請",
 }
 
 describe("Listing Details for Open Listings", () => {
@@ -63,7 +56,7 @@ describe("Listing Details for Open Listings", () => {
       verifyListing(
         null,
         testListings.OPEN_RENTAL.id,
-        ALT_PHOTO_TEXT.en,
+        testListings.OPEN_RENTAL.alt,
         testListings.OPEN_RENTAL.title,
         null,
         APPLY_BUTTON_TEXT.en
@@ -71,36 +64,33 @@ describe("Listing Details for Open Listings", () => {
     })
 
     it("displays in Spanish", () => {
-      verifyListing(
-        "es",
-        testListings.OPEN_RENTAL.id,
-        ALT_PHOTO_TEXT.es,
-        testListings.OPEN_RENTAL.title,
-        null,
-        APPLY_BUTTON_TEXT.es
-      )
+      const langPart = "es"
+      cy.visit(`${langPart}/listings/${testListings.OPEN_RENTAL.id}?react=true`)
+
+      cy.get(".image-card__inner > img")
+        .should("be.visible")
+        .should("have.attr", "alt")
+        .should("not.be", testListings.OPEN_RENTAL.alt)
     })
 
     it("displays in Chinese", () => {
-      verifyListing(
-        "zh",
-        testListings.OPEN_RENTAL.id,
-        ALT_PHOTO_TEXT.zh,
-        testListings.OPEN_RENTAL.title,
-        "",
-        APPLY_BUTTON_TEXT.zh
-      )
+      const langPart = "zh"
+      cy.visit(`${langPart}/listings/${testListings.OPEN_RENTAL.id}?react=true`)
+
+      cy.get(".image-card__inner > img")
+        .should("be.visible")
+        .should("have.attr", "alt")
+        .should("not.be", testListings.OPEN_RENTAL.alt)
     })
 
     it("displays in Filipino", () => {
-      verifyListing(
-        "tl",
-        testListings.OPEN_RENTAL.id,
-        ALT_PHOTO_TEXT.tl,
-        testListings.OPEN_RENTAL.title,
-        null,
-        APPLY_BUTTON_TEXT.tl
-      )
+      const langPart = "tl"
+      cy.visit(`${langPart}/listings/${testListings.OPEN_RENTAL.id}?react=true`)
+
+      cy.get(".image-card__inner > img")
+        .should("be.visible")
+        .should("have.attr", "alt")
+        .should("not.be", testListings.OPEN_RENTAL.alt)
     })
   })
 
@@ -109,7 +99,7 @@ describe("Listing Details for Open Listings", () => {
       verifyListing(
         null,
         testListings.OPEN_SALE.id,
-        ALT_PHOTO_TEXT.en,
+        testListings.OPEN_SALE.alt,
         testListings.OPEN_SALE.title,
         testListings.OPEN_SALE.address,
         APPLY_BUTTON_TEXT.en
@@ -117,36 +107,33 @@ describe("Listing Details for Open Listings", () => {
     })
 
     it("displays in Spanish", () => {
-      verifyListing(
-        "es",
-        testListings.OPEN_SALE.id,
-        ALT_PHOTO_TEXT.es,
-        testListings.OPEN_SALE.title,
-        testListings.OPEN_SALE.address,
-        APPLY_BUTTON_TEXT.es
-      )
+      const langPart = "es"
+      cy.visit(`${langPart}/listings/${testListings.OPEN_SALE.id}?react=true`)
+
+      cy.get(".image-card__inner > img")
+        .should("be.visible")
+        .should("have.attr", "alt")
+        .should("not.be", testListings.OPEN_SALE.alt)
     })
 
     it("displays in Chinese", () => {
-      verifyListing(
-        "zh",
-        testListings.OPEN_SALE.id,
-        ALT_PHOTO_TEXT.zh,
-        testListings.OPEN_SALE.title,
-        testListings.OPEN_SALE.address,
-        APPLY_BUTTON_TEXT.zh
-      )
+      const langPart = "zh"
+      cy.visit(`${langPart}/listings/${testListings.OPEN_SALE.id}?react=true`)
+
+      cy.get(".image-card__inner > img")
+        .should("be.visible")
+        .should("have.attr", "alt")
+        .should("not.be", testListings.OPEN_SALE.alt)
     })
 
     it("displays in Filipino", () => {
-      verifyListing(
-        "tl",
-        testListings.OPEN_SALE.id,
-        ALT_PHOTO_TEXT.tl,
-        testListings.OPEN_SALE.title,
-        testListings.OPEN_SALE.address,
-        APPLY_BUTTON_TEXT.tl
-      )
+      const langPart = "tl"
+      cy.visit(`${langPart}/listings/${testListings.OPEN_SALE.id}?react=true`)
+
+      cy.get(".image-card__inner > img")
+        .should("be.visible")
+        .should("have.attr", "alt")
+        .should("not.be", testListings.OPEN_SALE.alt)
     })
   })
 })
