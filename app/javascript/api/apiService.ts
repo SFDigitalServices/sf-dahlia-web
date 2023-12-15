fdbba2988f239c7df98791320ccde66a3769de36
cliu02 <32694266+cliu02@users.noreply.github.com>
2023-12-15 15:13:19 -0800

    Use default axios over setAuthHeaders

--- app/javascript/api/apiService.ts
+++ app/javascript/api/apiService.ts
@@ -1,63 +1,58 @@
-import axios, { AxiosInstance, AxiosRequestConfig, AxiosResponse } from "axios"
+import axios, { AxiosInstance, InternalAxiosRequestConfig, AxiosResponse } from "axios"
 
-import { setAuthHeaders, getHeaders } from "../authentication/token"
+import { getHeaders } from "../authentication/token"
 
 // Use this function for authenticated calls
 const createAxiosInstance = (): AxiosInstance => {
   if (!getHeaders()) {
     throw new Error("Unauthorized. Sign in first")
   }
-
-  return axios.create({
-    headers: getHeaders(),
-    transformResponse: (res, headers) => {
-      if (headers["access-token"]) {
-        setAuthHeaders(headers)
-      }
-      return JSON.parse(res)
-    },
-  })
+  return axios.create()
 }
 
 export const put = <T>(
   url: string,
   data?: unknown,
-  config?: AxiosRequestConfig
+  config?: InternalAxiosRequestConfig
 ): Promise<AxiosResponse<T>> => axios.put(url, data, config)
 
 export const post = <T>(
   url: string,
   data?: unknown,
-  config?: AxiosRequestConfig
+  config?: InternalAxiosRequestConfig
 ): Promise<AxiosResponse<T>> => axios.post(url, data, config)
 
-export const get = <T>(url: string, config?: AxiosRequestConfig): Promise<AxiosResponse<T>> =>
-  axios.get(url, config)
+export const get = <T>(
+  url: string,
+  config?: InternalAxiosRequestConfig
+): Promise<AxiosResponse<T>> => axios.get(url, config)
 
 /*
  * "delete" is a reserved word in TS so we couldn't name it that
  */
-export const apiDelete = <T>(url: string, config?: AxiosRequestConfig): Promise<AxiosResponse<T>> =>
-  axios.delete(url, config)
+export const apiDelete = <T>(
+  url: string,
+  config?: InternalAxiosRequestConfig
+): Promise<AxiosResponse<T>> => axios.delete(url, config)
 
 export const authenticatedPut = <T>(
   url: string,
   data?: unknown,
-  config?: AxiosRequestConfig
+  config?: InternalAxiosRequestConfig
 ): Promise<AxiosResponse<T>> => createAxiosInstance().put(url, data, config)
 
 export const authenticatedPost = <T>(
   url: string,
   data?: unknown,
-  config?: AxiosRequestConfig
+  config?: InternalAxiosRequestConfig
 ): Promise<AxiosResponse<T>> => createAxiosInstance().post(url, data, config)
 
 export const authenticatedGet = <T>(
   url: string,
-  config?: AxiosRequestConfig
+  config?: InternalAxiosRequestConfig
 ): Promise<AxiosResponse<T>> => createAxiosInstance().get(url, config)
 
 export const authenticatedDelete = <T>(
   url: string,
-  config?: AxiosRequestConfig
+  config?: InternalAxiosRequestConfig
 ): Promise<AxiosResponse<T>> => createAxiosInstance().delete(url, config)
