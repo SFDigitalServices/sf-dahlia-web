455a212d672ad00d681a12153d28ea9f9ef4ad97
Jim Lin <jim.lin@sfgov.org>
2024-01-30 11:43:07 -0800

    fix: build error

--- config/initializers/jasmine_dependencies.rb
+++ config/initializers/jasmine_dependencies.rb
@@ -1,11 +1,14 @@
-Jasmine::Dependencies.module_eval do
-  class << self
-    # override method in the Jasmine gem as a work around for Rails 7 compatibility
-    # Specifically, work around these lines:
-    #   https://github.com/jasmine/jasmine-gem/blob/d1d2ed7be8443b9ab86cadf0685f50bd2e2402de/lib/jasmine/dependencies.rb#L21
-    #   https://github.com/jasmine/jasmine-gem/blob/d1d2ed7be8443b9ab86cadf0685f50bd2e2402de/lib/jasmine/asset_expander.rb#L16
-    def rails6?
-      rails? && (Rails.version.to_i == 6 || Rails.version.to_i == 7)
+# Only run if the Jasmine gem is installed, e.g. in test and dev environments
+if defined?(Jasmine::Dependencies)
+  Jasmine::Dependencies.module_eval do
+    class << self
+      # override method in the Jasmine gem as a work around for Rails 7 compatibility
+      # Specifically, work around these lines:
+      #   https://github.com/jasmine/jasmine-gem/blob/d1d2ed7be8443b9ab86cadf0685f50bd2e2402de/lib/jasmine/dependencies.rb#L21
+      #   https://github.com/jasmine/jasmine-gem/blob/d1d2ed7be8443b9ab86cadf0685f50bd2e2402de/lib/jasmine/asset_expander.rb#L16
+      def rails6?
+        rails? && (Rails.version.to_i == 6 || Rails.version.to_i == 7)
+      end
     end
   end
 end
