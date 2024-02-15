f4298cd1b2490c62b9584a0bb5f48e67a69a50a6
tallulah <tallulah.kay@gmail.com>
2024-02-15 13:45:56 -0800

    DAH-2057 update

--- cypress/e2e/listingDetails/completedLotteryListing.e2e.ts
+++ cypress/e2e/listingDetails/completedLotteryListing.e2e.ts
@@ -1,25 +1,32 @@
+import { RouteHandler } from "cypress/types/net-stubbing"
+
 const listingId = "fake-listing-id"
 const lotteryNumber = "123456"
 
 const MOBILE_VIEWPORT_HEIGHT = 680
 const MOBILE_VIEWPORT_WIDTH = 420
 
-const visitListing = (mobile, language) => {
-  cy.task("log", "This will be output to the terminal")
+let listingDetailsFixture: RouteHandler
+let amiFixture: RouteHandler
+let unitsFixture: RouteHandler
+let preferencesFixture: RouteHandler
+let lotteryRankingFixture: RouteHandler
 
+const visitListing = (mobile, language) => {
   const langPart = language ? `/${language}` : ""
   if (mobile) {
     cy.viewport(MOBILE_VIEWPORT_WIDTH, MOBILE_VIEWPORT_HEIGHT)
   }
 
-  cy.intercept(`/api/v1/listings/${listingId}.json`, { fixture: "listingDetails.json" })
-  cy.intercept(`/listings/${listingId}?react=true`, { fixture: "listing.html" })
-  cy.intercept("ami.json**", { fixture: "ami.json" })
-  cy.intercept("units", { fixture: "units.json" })
-  cy.intercept("preferences", { fixture: "preferences.json" })
-  cy.intercept(`lottery_ranking?lottery_number=${lotteryNumber}`, {
-    fixture: "lotteryRanking.json",
-  })
+  cy.intercept("GET", `/api/v1/listings/${listingId}.json`, listingDetailsFixture).as(
+    "listingDetails"
+  )
+  cy.intercept("GET", "ami.json**", amiFixture).as("ami")
+  cy.intercept("GET", "units", unitsFixture).as("units")
+  cy.intercept("GET", "preferences", preferencesFixture).as("preferences")
+  cy.intercept("GET", `lottery_ranking?lottery_number=${lotteryNumber}`, lotteryRankingFixture).as(
+    "lotteryRanking"
+  )
 
   cy.visit(`${langPart}/listings/${listingId}?react=true`)
 }
@@ -43,6 +50,23 @@ describe("Listing Details for Completed Lottery Listing", () => {
   //   // there is a rogue loading issue beyond the scope of this story
   //   cy.wait(6000)
   // })
+  beforeEach(() => {
+    cy.fixture("listingDetails.json").then((listingDetails) => {
+      listingDetailsFixture = listingDetails
+    })
+    cy.fixture("ami.json").then((ami) => {
+      amiFixture = ami
+    })
+    cy.fixture("units.json").then((unitsJson) => {
+      unitsFixture = unitsJson
+    })
+    cy.fixture("preferences.json").then((preferencesJson) => {
+      preferencesFixture = preferencesJson
+    })
+    cy.fixture("lotteryRanking.json").then((lotteryRanking) => {
+      lotteryRankingFixture = lotteryRanking
+    })
+  })
 
   describe("Completed Lottery Rental Listing", () => {
     it("clicking the View Lottery Results button opens the lottery results modal on mobile devices", () => {
@@ -53,6 +77,10 @@ describe("Listing Details for Completed Lottery Listing", () => {
 
     it("searching for a lottery number returns results on mobile devices", () => {
       visitListing(true, "")
+      cy.wait("@listingDetails")
+      cy.wait("@units")
+      cy.wait("@ami")
+      cy.wait("@preferences")
       clickLotteryResultsButton(true)
       searchForLotteryResults()
       cy.contains("Your preference ranking")
@@ -60,17 +88,29 @@ describe("Listing Details for Completed Lottery Listing", () => {
 
     it("renders on desktop devices", () => {
       visitListing(false, "")
+      cy.wait("@listingDetails")
+      cy.wait("@units")
+      cy.wait("@ami")
+      cy.wait("@preferences")
       cy.contains("View Lottery Results")
     })
 
     it("clicking the View Lottery Results button opens the lottery results modal on desktop devices", () => {
       visitListing(false, "")
+      cy.wait("@listingDetails")
+      cy.wait("@units")
+      cy.wait("@ami")
+      cy.wait("@preferences")
       clickLotteryResultsButton(false)
       cy.contains("Lottery results are divided into multiple lists.")
     })
 
     it("searching for a lottery number returns results on desktop devices", () => {
       visitListing(false, "")
+      cy.wait("@listingDetails")
+      cy.wait("@units")
+      cy.wait("@ami")
+      cy.wait("@preferences")
       clickLotteryResultsButton(false)
       searchForLotteryResults()
       cy.contains("Your preference ranking")
