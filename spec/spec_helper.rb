877d3b057a31563dc48f443da6ba9ee4f2dc5278
Alula Beshue <alula.beshue@sfgov.org>
2024-02-18 15:10:58 -0800

    rspec coverage

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -27,6 +27,8 @@ CodeClimate::TestReporter.start
 require 'webmock/rspec'
 WebMock.disable_net_connect!(allow_localhost: true)
 
+SimpleCov.start 'rails'
+
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
