fab205228b1d9df6811f28316a9f1b96e8aa9556
tallulah <tallulah.kay@gmail.com>
2024-02-28 14:52:07 -0800

    DAH-2122 completed lottery listing

--- cypress/e2e/listingDetails/completedLotteryListing.e2e.ts
+++ cypress/e2e/listingDetails/completedLotteryListing.e2e.ts
@@ -1,16 +1,8 @@
-import { RouteHandler } from "cypress/types/net-stubbing"
-
 const listingId = "fake-listing-id"
 const lotteryNumber = "123456"
 
 const MOBILE_VIEWPORT_HEIGHT = 680
 const MOBILE_VIEWPORT_WIDTH = 420
-
-let listingDetailsFixture: RouteHandler
-let amiFixture: RouteHandler
-let unitsFixture: RouteHandler
-let preferencesFixture: RouteHandler
-let lotteryRankingFixture: RouteHandler
 
 const visitListing = (mobile, language) => {
   const langPart = language ? `/${language}` : ""
@@ -18,15 +10,11 @@ const visitListing = (mobile, language) => {
     cy.viewport(MOBILE_VIEWPORT_WIDTH, MOBILE_VIEWPORT_HEIGHT)
   }
 
-  cy.intercept("GET", `/api/v1/listings/${listingId}.json`, listingDetailsFixture).as(
-    "listingDetails"
-  )
-  cy.intercept("GET", "ami.json**", amiFixture).as("ami")
-  cy.intercept("GET", "units", unitsFixture).as("units")
-  cy.intercept("GET", "preferences", preferencesFixture).as("preferences")
-  cy.intercept("GET", "lottery_ranking**", lotteryRankingFixture).as("lotteryRanking")
-
   cy.visit(`${langPart}/listings/${listingId}?react=true`)
+  cy.wait("@listingDetails")
+  cy.wait("@units")
+  cy.wait("@ami")
+  cy.wait("@preferences")
 }
 
 const clickLotteryResultsButton = (mobile: boolean) => {
@@ -49,23 +37,15 @@ describe("Listing Details for Completed Lottery Listing", () => {
   //   cy.wait(6000)
   // })
   beforeEach(() => {
-    cy.fixture("listingDetails.json").then((listingDetails) => {
-      listingDetailsFixture = listingDetails
-    })
-    cy.fixture("ami.json").then((ami) => {
-      amiFixture = ami
-    })
-    cy.fixture("units.json").then((unitsJson) => {
-      unitsFixture = unitsJson
-    })
-    cy.fixture("preferences.json").then((preferencesJson) => {
-      preferencesFixture = preferencesJson
-    })
-    cy.fixture("lotteryRanking.json").then((lotteryRanking) => {
-      lotteryRankingFixture = lotteryRanking
-    })
+    cy.intercept(`/api/v1/listings/${listingId}.json`, { fixture: "listingDetails.json" }).as(
+      "listingDetails"
+    )
+    cy.intercept("lottery_buckets", { fixture: "lotteryRanking.json" }).as("lotteryBuckets")
+    cy.intercept("ami.json**", { fixture: "ami.json" }).as("ami")
+    cy.intercept("units", { fixture: "units.json" }).as("units")
+    cy.intercept("preferences", { fixture: "preferences.json" }).as("preferences")
+    cy.intercept("lottery_ranking**", { fixture: "lotteryRanking.json" }).as("lotteryRanking")
   })
-
   describe("Completed Lottery Rental Listing", () => {
     it("clicking the View Lottery Results button opens the lottery results modal on mobile devices", () => {
       visitListing(true, "")
@@ -75,10 +55,6 @@ describe("Listing Details for Completed Lottery Listing", () => {
 
     it("searching for a lottery number returns results on mobile devices", () => {
       visitListing(true, "")
-      cy.wait("@listingDetails")
-      cy.wait("@units")
-      cy.wait("@ami")
-      cy.wait("@preferences")
       clickLotteryResultsButton(true)
       searchForLotteryResults()
       cy.contains("Your preference ranking")
@@ -86,29 +62,17 @@ describe("Listing Details for Completed Lottery Listing", () => {
 
     it("renders on desktop devices", () => {
       visitListing(false, "")
-      cy.wait("@listingDetails")
-      cy.wait("@units")
-      cy.wait("@ami")
-      cy.wait("@preferences")
       cy.contains("View Lottery Results")
     })
 
     it("clicking the View Lottery Results button opens the lottery results modal on desktop devices", () => {
       visitListing(false, "")
-      cy.wait("@listingDetails")
-      cy.wait("@units")
-      cy.wait("@ami")
-      cy.wait("@preferences")
       clickLotteryResultsButton(false)
       cy.contains("Lottery results are divided into multiple lists.")
     })
 
     it("searching for a lottery number returns results on desktop devices", () => {
       visitListing(false, "")
-      cy.wait("@listingDetails")
-      cy.wait("@units")
-      cy.wait("@ami")
-      cy.wait("@preferences")
       clickLotteryResultsButton(false)
       searchForLotteryResults()
       cy.contains("Your preference ranking")
