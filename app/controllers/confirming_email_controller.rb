25f89340b3b998af8708ef32036d8c6c81968c92
Alula Beshue <135058064+alulabeshue-sfgov@users.noreply.github.com>
2024-05-02 03:17:47 -0700

    chore: merging production back into main (#2153)
    hotfix: adding email response api and webpage (#2150)
    
    * feature: adding email response api and webpage
    
    * fix: adding .rb extension
    
    * chore: adding logs and test listing
    
    * feat: update yes page content
    
    * chore: following figma styling
    
    Co-authored-by: Tallulah Kay <43218114+tallulahkay@users.noreply.github.com>
    Co-authored-by: Jim Lin <133722877+jimlin-sfgov@users.noreply.github.com>

---
+++ app/controllers/confirming_email_controller.rb
@@ -0,0 +1,15 @@
+# Controller for the get assistance pages
+class ConfirmingEmailController < ApplicationController
+  def confirming_email
+    @confirming_email_props = { assetPaths: static_asset_paths,
+                                urlParams: { listing: params['listing'],
+                                             response: params['response'] } }
+    render 'confirming_email'
+  end
+
+  protected
+
+  def use_react_app
+    ENV['GET_ASSISTANCE_PAGES_REACT'].to_s.casecmp('true').zero?
+  end
+end
