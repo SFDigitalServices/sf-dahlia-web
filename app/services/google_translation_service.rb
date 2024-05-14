6b227256a7aa9d7c3073d0bdec5d218d03eca430
Cade Wolcott <cade.wolcott@exygy.com>
2024-05-14 15:04:35 -0500

    fix: translate auth

--- app/services/google_translation_service.rb
+++ app/services/google_translation_service.rb
@@ -6,7 +6,7 @@ module GoogleTranslate
     puts 'Connecting to Google Cloud Translate...'
 
     translate = Google::Cloud::Translate::V2.new("project_id": 'housing-393518',
-                                                 "key": 'AIzaSyDezm2LJY4CY9wtUWcQsw7Ldu93kBfFjP4')
+                                                 "key": ENV['GOOGLE_TRANSLATE_KEY'])
 
     translation = translate.translate 'Hello world!', 'Good Morning', to: 'es'
     puts translation #=> "Salve mundi!"
