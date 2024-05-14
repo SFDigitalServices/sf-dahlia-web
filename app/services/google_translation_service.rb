b455a07aecdc68d53872dcfdacf9848e84b7ffab
Cade Wolcott <cade.wolcott@exygy.com>
2024-05-14 16:05:12 -0500

    add function

--- app/services/google_translation_service.rb
+++ app/services/google_translation_service.rb
@@ -3,12 +3,14 @@ require 'google/cloud/translate/v2'
 module GoogleTranslate
   # Translate
   class GoogleTranslationService
-    puts 'Connecting to Google Cloud Translate...'
+    def translate
+      puts 'Connecting to Google Cloud Translate...'
 
-    translate = Google::Cloud::Translate::V2.new("project_id": 'housing-393518',
-                                                 "key": ENV['GOOGLE_TRANSLATE_KEY'])
+      translate = Google::Cloud::Translate::V2.new("project_id": 'housing-393518',
+                                                   "key": ENV['GOOGLE_TRANSLATE_KEY'])
 
-    translation = translate.translate 'Hello world!', 'Good Morning', to: 'es'
-    puts translation #=> "Salve mundi!"
+      translation = translate.translate 'Hello world!', 'Good Morning', to: 'es'
+      puts translation #=> "Salve mundi!"
+    end
   end
 end
