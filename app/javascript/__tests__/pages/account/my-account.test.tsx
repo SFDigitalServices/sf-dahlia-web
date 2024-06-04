1bb2dd87d21b3c54d2c0a293df31d5e1a4b16f76
Chad Brokaw <36685920+chadbrokaw@users.noreply.github.com>
2024-06-04 11:56:11 -0600

    feat: account dashboard

--- app/javascript/__tests__/pages/account/my-account.test.tsx
+++ app/javascript/__tests__/pages/account/my-account.test.tsx
@@ -3,9 +3,35 @@ import MyAccount from "../../../pages/account/my-account"
 import React from "react"
 
 describe("<MyAccount />", () => {
-  it("shows the correct header text", async () => {
-    const { getByText } = await renderAndLoadAsync(<MyAccount assetPaths={{}} />)
+  beforeEach(() => {
+    // The below line prevents @axe-core from throwing an error
+    // when the html tag does not have a lang attribute
+    document.documentElement.lang = "en"
+  })
 
-    expect(getByText("My Account")).not.toBeNull()
+  it("contains two links within the main content", async () => {
+    const { getByTestId } = await renderAndLoadAsync(<MyAccount assetPaths={{}} />)
+    const mainContent = getByTestId("main-content-test-id")
+
+    const links = mainContent.querySelectorAll("a")
+    expect(links).toHaveLength(2)
+  })
+
+  it("first link has title 'My Applications'", async () => {
+    const { getByTestId } = await renderAndLoadAsync(<MyAccount assetPaths={{}} />)
+    const mainContent = getByTestId("main-content-test-id")
+
+    const links = mainContent.querySelectorAll("a")
+    const linkHeader = links[0].querySelector("h1")
+    expect(linkHeader?.textContent).toBe("My Applications")
+  })
+
+  it("second link has title 'Account Settings'", async () => {
+    const { getByTestId } = await renderAndLoadAsync(<MyAccount assetPaths={{}} />)
+    const mainContent = getByTestId("main-content-test-id")
+
+    const links = mainContent.querySelectorAll("a")
+    const linkHeader = links[1].querySelector("h1")
+    expect(linkHeader?.textContent).toBe("Account Settings")
   })
 })
--- app/javascript/pages/account/my-account.tsx
+++ app/javascript/pages/account/my-account.tsx
@@ -2,6 +2,7 @@ import React from "react"
 import Layout from "../../layouts/Layout"
 import withAppSetup from "../../layouts/withAppSetup"
 import { Card } from "@bloom-housing/ui-seeds"
+import { Icon, t, type UniversalIconType } from "@bloom-housing/ui-components"
 
 import "./my-account.scss"
 
@@ -9,25 +10,40 @@ interface MyAccountProps {
   assetPaths: unknown
 }
 
-const AccountCard = ({
+const AccountDashCard = ({
   title,
   description,
   link,
+  icon,
+  removeBottomBorder = false,
 }: {
   title: string
   description: string
   link: string
-  icon: string
+  icon: UniversalIconType
+  removeBottomBorder?: boolean
 }) => {
   return (
-    <a href={link} className="text-black block outline-none rounded_card focus:underline flex-1">
-      <Card spacing="md" className="cursor-pointer h-full">
-        <Card.Header>
-          <h1>{title}</h1>
+    <a href={link} className="text-black block outline-none focus:underline flex-1 w-full md:w-1/2">
+      <Card
+        spacing="md"
+        className={`cursor-pointer flex justify-center items-center text-center h-60 rounded-none md:rounded-lg ${
+          removeBottomBorder ? "border-b-0 md:border-b" : ""
+        }`}
+      >
+        <Card.Header className="flex justify-center w-full flex-col items-center">
+          <div
+            className="pb-4 border-blue-500 w-min px-4 md:px-8 mb-6"
+            style={{ borderBottom: "3px solid" }}
+          >
+            <Icon size="xlarge" className="md:hidden block" symbol={icon} />
+            <Icon size="2xl" className="md:block hidden" symbol={icon} />
+          </div>
+          <h1 className="text-xl md:text-2xl">{title}</h1>
         </Card.Header>
 
         <Card.Section>
-          <p>{description}</p>
+          <p className="text-sm">{description}</p>
         </Card.Section>
       </Card>
     </a>
@@ -38,20 +54,21 @@ const MyAccount = (_props: MyAccountProps) => {
   return (
     <Layout title={"My Account"}>
       <section className="bg-gray-300 flex justify-center">
-        <div className="max-w-5xl w-full md:w-auto md:mx-auto md:py-16">
-          <div className="bg-gray-300 h-full flex flex-grow flex-col md:flex-row md:gap-6">
-            <AccountCard
-              title="My Applications"
-              description="See lottery dates and listings for properties for which you've applied"
+        <div className="w-full md:py-16 max-w-5xl">
+          <div className="bg-gray-300 h-full flex flex-grow flex-col md:flex-row md:gap-7">
+            <AccountDashCard
+              title={t("myApplications.title")}
+              description={t("accountDashboard.myApplications.description")}
               link="/my-applications"
-              icon="applications"
+              icon="application"
+              removeBottomBorder
             />
 
-            <AccountCard
-              title="Account Settings"
-              description="Account settings, email and password"
+            <AccountDashCard
+              title={t("accountSettings.title")}
+              description={t("accountDashboard.accountSettings.description")}
               link="/account-settings"
-              icon="applications"
+              icon="settings"
             />
           </div>
         </div>
