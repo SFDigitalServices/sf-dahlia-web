8d963bbf0c9e9d4d1ade530f1da0cd0310fdf7e1
cliu02 <32694266+cliu02@users.noreply.github.com>
2024-06-12 15:47:07 -0700

    feat: card styling

--- app/javascript/components/ApplicationItem.tsx
+++ app/javascript/components/ApplicationItem.tsx
@@ -1,81 +1,64 @@
 import React from "react"
-import { t } from "@bloom-housing/ui-components"
-import { Button, Card, Tag } from "@bloom-housing/ui-seeds"
-import styles from "./ApplicationItem.module.scss"
+import { t, Button } from "@bloom-housing/ui-components"
+import { Card, Link } from "@bloom-housing/ui-seeds"
+import "./ApplicationItem.scss"
+import { getCurrentLanguage } from "../util/languageUtil"
+import { getLocalizedPath } from "../util/routeUtil"
 
 interface ApplicationItemProps {
   applicationDueDate?: string
   applicationURL: string
   applicationUpdatedAt: string
   confirmationNumber?: string
+  editedDate?: string
+  listingAddress: string
   listingName: string
   listingURL: string
-  strings?: {
-    applicationsDeadline?: string
-    edited?: string
-    seeListing?: string
-    status?: string
-    submittedStatus?: string
-    viewApplication?: string
-    yourNumber?: string
-  }
 }
 
 const ApplicationItem = (props: ApplicationItemProps) => {
   return (
-    <Card.Section>
-      <article className={styles["application-item"]}>
-        <header className={styles["application-item__header"]}>
-          <h3 className={styles["application-item__title"]}>{props.listingName}</h3>
-          <p className={styles["application-item__status"]}>
-            {props.strings?.status ?? t("t.status")}:{" "}
-            <Tag variant="primary-inverse">
-              {props.strings?.submittedStatus ?? t("application.statuses.submitted")}
-            </Tag>
+    <Card.Section className="border-t">
+      <header className={"application-item__header"}>
+        <h3 className={"application-item__title"}>{props.listingName}</h3>
+        {props.applicationDueDate && (
+          <p className={"application-item__text"}>
+            {t("listings.applicationDeadline")}
+            {": "}
+            {props.applicationDueDate}
           </p>
-        </header>
-
-        <section className={styles["application-item__content"]}>
-          <div>
-            {props.confirmationNumber && (
-              <>
-                <span className={styles["application-item__confirm-text"]}>
-                  {t("myApplications.yourLotteryNumberIs", {
-                    lotteryNumber: props.confirmationNumber,
-                  })}
-                </span>
-                <br />
-                <span className={styles["application-item__confirm-number"]}>
-                  {props.confirmationNumber}
-                </span>
-              </>
-            )}
-          </div>
-
-          <div className={styles["application-item__action"]}>
-            {props.applicationDueDate && (
-              <p className={styles["application-item__due"]}>
-                {props.strings?.applicationsDeadline ?? t("listings.applicationDeadline")}:{" "}
-                <span className={styles["application-item__due-date"]}>
-                  {props.applicationDueDate}
-                </span>
-              </p>
-            )}
-          </div>
-        </section>
-        <footer className={styles["application-item__footer"]}>
-          <div>
-            <Button href={props.applicationURL} variant="primary-outlined" size="sm">
-              {props.strings?.viewApplication ?? t("label.viewApplication")}
-            </Button>
-          </div>
-          <div>
-            <Button href={props.listingURL} variant="primary-outlined" size="sm">
-              {props.strings?.seeListing ?? t("label.seeListing")}
-            </Button>
-          </div>
-        </footer>
-      </article>
+        )}
+      </header>
+      <section className={"application-item__content"}>
+        <div className="space-y-2 flex flex-col">
+          <p className={"application-item__text"}>{props.listingAddress}</p>
+          {props.confirmationNumber && (
+            <div className={"application-item__confirm-text"}>
+              {t("myApplications.yourLotteryNumberIs", {
+                lotteryNumber: props.confirmationNumber,
+              })}
+            </div>
+          )}
+          <Link className="text-sm" href={getLocalizedPath(props.listingURL, getCurrentLanguage())}>
+            {t("label.seeListing")}
+          </Link>
+        </div>
+        <div className={"application-item__action"}>
+          <p className={"application-item__status"}>
+            {t("t.status")}
+            {": "}
+            {t("t.submitted")}
+          </p>
+          <Button
+            onClick={() =>
+              (window.location.href = getLocalizedPath(props.applicationURL, getCurrentLanguage()))
+            }
+          >
+            {t("label.viewApplication")}
+          </Button>
+          <p className={"application-item_edited-text"}>Edited: {props.editedDate}</p>
+        </div>
+      </section>
     </Card.Section>
   )
 }
--- app/javascript/pages/account/my-applications.tsx
+++ app/javascript/pages/account/my-applications.tsx
@@ -1,23 +1,33 @@
 import React from "react"
 import Layout from "../../layouts/Layout"
 import withAppSetup from "../../layouts/withAppSetup"
-import { t, Icon } from "@bloom-housing/ui-components"
-import { Button, Card, Heading } from "@bloom-housing/ui-seeds"
+import { t, Icon, Button } from "@bloom-housing/ui-components"
+import { Card, Heading } from "@bloom-housing/ui-seeds"
 import { ApplicationItem } from "../../components/ApplicationItem"
+import { getLocalizedPath } from "../../util/routeUtil"
+import { getCurrentLanguage } from "../../util/languageUtil"
 
 const MyApplications = () => {
   const noApplications = () => {
     return (
-      <Card.Section>
-        <Heading priority={2} size="md">
-          {t("myApplications.noApplications")}
-        </Heading>
-        <Button size="sm" variant="primary-outlined" href="/listings">
-          {t("listings.browseRentals")}
-        </Button>
-        <Button size="sm" variant="primary-outlined" href="/listings">
-          {t("listings.browseSales")}
-        </Button>
+      <Card.Section className="flex flex-col bg-primary-lighter items-center pb-12 border-t">
+        <h2 className="text-xl">{t("myApplications.noApplications")}</h2>
+        <div className="flex flex-col gap-y-4 w-3/5 pt-4">
+          <Button
+            onClick={() =>
+              (window.location.href = getLocalizedPath("/listings/for-rent", getCurrentLanguage()))
+            }
+          >
+            {t("listings.browseRentals")}
+          </Button>
+          <Button
+            onClick={() =>
+              (window.location.href = getLocalizedPath("/listings/for-sale", getCurrentLanguage()))
+            }
+          >
+            {t("listings.browseSales")}
+          </Button>
+        </div>
       </Card.Section>
     )
   }
@@ -28,7 +38,7 @@ const MyApplications = () => {
         <section className="bg-gray-300 border-t border-gray-450">
           <div className="flex flex-wrap relative max-w-3xl mx-auto sm:p-8">
             <Card>
-              <Card.Header className="flex justify-center w-full flex-col items-center">
+              <Card.Header className="flex justify-center w-full flex-col items-center pb-8">
                 <div
                   className="pb-4 border-blue-500 w-min px-4 md:px-8 mb-6"
                   style={{ borderBottom: "3px solid" }}
@@ -40,15 +50,73 @@ const MyApplications = () => {
                   {t("myApplications.title")}
                 </Heading>
               </Card.Header>
+              {/* Component if application is submitted */}
               <ApplicationItem
                 applicationDueDate={"June 1, 2024"}
                 applicationURL={"application/1234abcd"}
                 applicationUpdatedAt={"March 8th, 2022"}
-                confirmationNumber={"1234abcd"}
-                listingName={"Listing Name"}
+                confirmationNumber={"#12345678"}
+                editedDate={"June 1, 2024"}
+                listingAddress={"1 Listing Address St, San Francisco CA, 94102"}
+                listingName={"App submitted, no lottery results"}
                 listingURL={"/listing/abcd1234/listing-name"}
               />
-              {/* Show only if no applications */}
+              {/* Component if application is submitted with lottery results and error */}
+              <ApplicationItem
+                applicationDueDate={"June 1, 2024"}
+                applicationURL={"application/1234abcd"}
+                applicationUpdatedAt={"March 8th, 2022"}
+                confirmationNumber={"#12345678"}
+                editedDate={"June 1, 2024"}
+                listingAddress={"1 Listing Address St, San Francisco CA, 94102"}
+                listingName={"App submitted, download lottery results"}
+                listingURL={"/listing/abcd1234/listing-name"}
+              />
+              {/* Component if application is submitted with lottery results and no error */}
+              <ApplicationItem
+                applicationDueDate={"June 1, 2024"}
+                applicationURL={"application/1234abcd"}
+                applicationUpdatedAt={"March 8th, 2022"}
+                confirmationNumber={"#12345678"}
+                editedDate={"June 1, 2024"}
+                listingAddress={"1 Listing Address St, San Francisco CA, 94102"}
+                listingName={"App submitted, view lottery results"}
+                listingURL={"/listing/abcd1234/listing-name"}
+              />
+              {/* Component if application is not submitted with lottery results */}
+              <ApplicationItem
+                applicationDueDate={"June 1, 2024"}
+                applicationURL={"application/1234abcd"}
+                applicationUpdatedAt={"March 8th, 2022"}
+                confirmationNumber={"#12345678"}
+                editedDate={"June 1, 2024"}
+                listingAddress={"1 Listing Address St, San Francisco CA, 94102"}
+                listingName={"App not submitted with lottery results"}
+                listingURL={"/listing/abcd1234/listing-name"}
+              />
+              {/* Component if application is not submitted and is past due */}
+              <ApplicationItem
+                applicationDueDate={"June 1, 2024"}
+                applicationURL={"application/1234abcd"}
+                applicationUpdatedAt={"March 8th, 2022"}
+                confirmationNumber={"#12345678"}
+                editedDate={"June 1, 2024"}
+                listingAddress={"1 Listing Address St, San Francisco CA, 94102"}
+                listingName={"App not submitted, past due"}
+                listingURL={"/listing/abcd1234/listing-name"}
+              />
+              {/* Component if application is not submitted and is not past due */}
+              <ApplicationItem
+                applicationDueDate={"June 1, 2024"}
+                applicationURL={"application/1234abcd"}
+                applicationUpdatedAt={"March 8th, 2022"}
+                confirmationNumber={"#12345678"}
+                editedDate={"June 1, 2024"}
+                listingAddress={"1 Listing Address St, San Francisco CA, 94102"}
+                listingName={"App not submitted, not past due"}
+                listingURL={"/listing/abcd1234/listing-name"}
+              />
+              {/* Component if no applications */}
               {noApplications()}
             </Card>
           </div>
