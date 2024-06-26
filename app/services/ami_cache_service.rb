e55950e0d1bb2db555d5227c3db7c85ecd2a0123
tallulah <tallulah.kay@gmail.com>
2024-06-25 17:03:42 -0700

    DAH-2371 simplify

--- app/services/ami_cache_service.rb
+++ app/services/ami_cache_service.rb
@@ -8,31 +8,36 @@ class AmiCacheService
     end
   end
 
-  def ami_match(unique_chart, unit, field)
-    unique_chart['year'] == unit['AMI_chart_year'] and
-      unique_chart['type'] == unit['AMI_chart_type'] and
-      unique_chart['percent'] == unit[field]
-  end
-
   private
+
+  def check_ami_match(unique_charts, unit)
+    match = {
+      'max' => false,
+      'min' => false
+    }
+    unique_charts.each do |unique_chart|
+      if unique_chart['year'] == unit['AMI_chart_year'] and
+        unique_chart['type'] == unit['AMI_chart_type']
+        if unique_chart['percent'] == unit['Max_AMI_for_Qualifying_Unit']
+          match["max"] = true
+        end
+        if unique_chart['percent'] == unit['Min_AMI_for_Qualifying_Unit']
+          match["min"] = true
+        end
+      end
+    end
+    match
+  end
 
   def get_ami_chart_data_from_units(units)
     unique_charts = []
 
     units.each do |unit|
-      unique_chart_match_for_max = nil
-      unique_chart_match_for_min = nil
-      unique_charts.each do |unique_chart|
-        if ami_match(unique_chart, unit, 'Max_AMI_for_Qualifying_Unit')
-          unique_chart_match_for_max = unique_chart
-        end
+      # check if the min and max ami of the unit is already in our list of ami charts
+      match = check_ami_match(unique_charts, unit)
 
-        if ami_match(unique_chart, unit, 'Min_AMI_for_Qualifying_Unit')
-          unique_chart_match_for_min = unique_chart
-        end
-      end
-
-      if !unique_chart_match_for_max && unit['Max_AMI_for_Qualifying_Unit']
+      # if max ami is not already in the list of ami charts
+      if !match["max"] && unit['Max_AMI_for_Qualifying_Unit']
         unique_charts << {
           'year' => unit['AMI_chart_year'],
           'type' => unit['AMI_chart_type'],
@@ -41,12 +46,13 @@ class AmiCacheService
         }
       end
 
-      if !unique_chart_match_for_min && unit['Min_AMI_for_Qualifying_Unit']
+      # if min ami is not already in the list of ami charts
+      if !match["min"] && unit['Min_AMI_for_Qualifying_Unit']
         unique_charts << {
           'year' => unit['AMI_chart_year'],
           'type' => unit['AMI_chart_type'],
-          'percent' => unit['Max_AMI_for_Qualifying_Unit'],
-          'derivedFrom' => 'MaxAmi',
+          'percent' => unit['Min_AMI_for_Qualifying_Unit'],
+          'derivedFrom' => 'MinAmi',
         }
       end
     end
