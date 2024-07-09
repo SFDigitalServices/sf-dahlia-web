84634db99b8d945a6a5f0d80e340b8c5ef01d522
tallulah <tallulah.kay@gmail.com>
2024-07-09 11:22:53 -0700

    DAH-2339 expand test

--- app/javascript/__tests__/pages/account/account-settings.test.tsx
+++ app/javascript/__tests__/pages/account/account-settings.test.tsx
@@ -3,42 +3,91 @@ import UserContext, { ContextProps } from "../../../authentication/context/UserC
 import { renderAndLoadAsync } from "../../__util__/renderUtils"
 import AccountSettingsPage from "../../../pages/account/account-settings"
 
-describe("when the user is signed in", () => {
-  let getByText
-  let originalUseContext
+describe("<AccountSettingsPage />", () => {
+  describe("when the user is signed in", () => {
+    let getByText
+    let originalUseContext
 
-  beforeEach(async () => {
-    originalUseContext = React.useContext
-    const mockContextValue: ContextProps = {
-      profile: {
-        uid: "abc123",
-        email: "email@email.com",
-        created_at: new Date(),
-        updated_at: new Date(),
-      },
-      signIn: jest.fn(),
-      signOut: jest.fn(),
-      loading: false,
-      initialStateLoaded: true,
-    }
-
-    jest.spyOn(React, "useContext").mockImplementation((context) => {
-      if (context === UserContext) {
-        return mockContextValue
+    beforeEach(async () => {
+      originalUseContext = React.useContext
+      const mockContextValue: ContextProps = {
+        profile: {
+          uid: "abc123",
+          email: "email@email.com",
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        signIn: jest.fn(),
+        signOut: jest.fn(),
+        loading: false,
+        initialStateLoaded: true,
       }
-      return originalUseContext(context)
+
+      jest.spyOn(React, "useContext").mockImplementation((context) => {
+        if (context === UserContext) {
+          return mockContextValue
+        }
+        return originalUseContext(context)
+      })
+
+      const renderResult = await renderAndLoadAsync(<AccountSettingsPage assetPaths={{}} />)
+      getByText = renderResult.getByText
     })
 
-    const renderResult = await renderAndLoadAsync(<AccountSettingsPage assetPaths={{}} />)
-    getByText = renderResult.getByText
+    afterEach(() => {
+      jest.restoreAllMocks()
+    })
+    it("shows the correct header text", () => {
+      const title = getByText("Account Settings")
+
+      expect(title).not.toBeNull()
+    })
   })
 
-  afterEach(() => {
-    jest.restoreAllMocks()
-  })
-  it("contains two links within the main content", () => {
-    const title = getByText("Account Settings")
+  describe("when the user is not signed in", () => {
+    let originalUseContext
+    let originalLocation: Location
 
-    expect(title).not.toBeNull()
+    beforeEach(() => {
+      originalUseContext = React.useContext
+      originalLocation = window.location
+      const mockContextValue: ContextProps = {
+        profile: undefined,
+        signIn: jest.fn(),
+        signOut: jest.fn(),
+        loading: false,
+        initialStateLoaded: true,
+      }
+
+      jest.spyOn(React, "useContext").mockImplementation((context) => {
+        if (context === UserContext) {
+          return mockContextValue
+        }
+        return originalUseContext(context)
+      })
+
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      delete (window as any)?.location
+      ;(window as Window).location = {
+        ...originalLocation,
+        href: "http://dahlia.com",
+        assign: jest.fn(),
+        replace: jest.fn(),
+        reload: jest.fn(),
+        toString: jest.fn(),
+      }
+    })
+
+    afterEach(() => {
+      jest.restoreAllMocks()
+      window.location = originalLocation
+    })
+
+    it("redirects to the sign in page", async () => {
+      const { queryByText } = await renderAndLoadAsync(<AccountSettingsPage assetPaths={{}} />)
+
+      expect(window.location.href).toBe("/sign-in")
+      expect(queryByText("Account Settings")).toBeNull()
+    })
   })
 })
