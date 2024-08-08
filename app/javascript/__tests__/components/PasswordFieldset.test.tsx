61b2a1973f41200a7dcae2e92b81ae74b3bdf6ab
Chad Brokaw <36685920+chadbrokaw@users.noreply.github.com>
2024-08-08 10:02:55 -0600

    test: fix tests

--- app/javascript/__tests__/components/PasswordFieldset.test.tsx
+++ app/javascript/__tests__/components/PasswordFieldset.test.tsx
@@ -1,10 +1,23 @@
+/* eslint-disable @typescript-eslint/unbound-method */
 import React from "react"
-import { fireEvent, render, screen } from "@testing-library/react"
+import { render, screen } from "@testing-library/react"
+import userEvent from "@testing-library/user-event"
 import PasswordFieldset from "../../pages/account/PasswordFieldset"
+import { useForm } from "react-hook-form"
+
+const WrappedPasswordFieldset = () => {
+  const {
+    register,
+    formState: { errors },
+    watch,
+  } = useForm({ mode: "all" })
+
+  return <PasswordFieldset register={register} errors={errors} watch={watch} />
+}
 
 describe("Password Fieldset", () => {
   it("displays password instructions without validation", () => {
-    render(<PasswordFieldset register={jest.fn()} errors={{}} />)
+    render(<WrappedPasswordFieldset />)
     expect(screen.getByText("Must include at least:")).not.toBeNull()
     expect(screen.getByText("8 characters")).not.toBeNull()
     expect(screen.getByText("1 letter")).not.toBeNull()
@@ -14,32 +27,36 @@ describe("Password Fieldset", () => {
     expect(screen.queryByTestId("validation-x")).toBeNull()
   })
 
-  it("displays validation for password confirmation", () => {
-    render(<PasswordFieldset register={jest.fn()} errors={{}} />)
+  it("displays validation for password confirmation", async () => {
+    render(<WrappedPasswordFieldset />)
+
+    const user = userEvent.setup()
     const input = screen.getByLabelText(/choose a new password/i)
     expect(screen.getAllByTestId("validation-none")).toHaveLength(3)
 
-    fireEvent.change(input, { target: { value: "p" } })
+    await user.type(input, "p")
     expect(screen.getAllByTestId("validation-x")).toHaveLength(2)
     expect(screen.getByTestId("validation-check")).not.toBeNull()
 
-    fireEvent.change(input, { target: { value: "p1" } })
+    await user.type(input, "p1")
     expect(screen.getAllByTestId("validation-x")).toHaveLength(1)
     expect(screen.getAllByTestId("validation-check")).toHaveLength(2)
 
-    fireEvent.change(input, { target: { value: "p1234567" } })
+    await user.type(input, "p1234567")
     expect(screen.queryByTestId("validation-x")).toBeNull()
     expect(screen.getAllByTestId("validation-check")).toHaveLength(3)
   })
 
-  it("displays password validation", () => {
-    render(<PasswordFieldset register={jest.fn()} errors={{}} />)
+  it("displays password validation", async () => {
+    render(<WrappedPasswordFieldset />)
+    const user = userEvent.setup()
+
     const input = screen.getByLabelText(/current password/i)
     const button = screen.getAllByRole("button")[0]
     expect(input.getAttribute("type")).toBe("password")
-    fireEvent.click(button)
+    await user.click(button)
     expect(input.getAttribute("type")).toBe("text")
-    fireEvent.click(button)
+    await user.click(button)
     expect(input.getAttribute("type")).toBe("password")
   })
 })
