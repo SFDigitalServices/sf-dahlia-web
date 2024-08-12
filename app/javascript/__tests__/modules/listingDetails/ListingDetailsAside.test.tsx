acc3dd55c24d1f731e00b4be3ccac748ae2ea9d1
tallulah <tallulah.kay@gmail.com>
2024-08-12 15:09:20 -0700

    DAH-2497 add test for sale listing

--- app/javascript/__tests__/modules/listingDetails/ListingDetailsAside.test.tsx
+++ app/javascript/__tests__/modules/listingDetails/ListingDetailsAside.test.tsx
@@ -2,9 +2,10 @@ import React from "react"
 import { render } from "@testing-library/react"
 import { ListingDetailsAside } from "../../../modules/listingDetailsAside/ListingDetailsAside"
 import { closedRentalListing } from "../../data/RailsRentalListing/listing-rental-closed"
+import { openSaleListing } from "../../data/RailsSaleListing/listing-sale-open"
 
 describe("ListingDetailsAside", () => {
-  it("renders ListingDetailsAside component", () => {
+  it("renders ListingDetailsAside component rental", () => {
     window.matchMedia = jest.fn().mockImplementation((query) => {
       return {
         matches: true,
@@ -19,6 +20,25 @@ describe("ListingDetailsAside", () => {
     })
 
     const { asFragment } = render(<ListingDetailsAside listing={closedRentalListing} imageSrc="" />)
+
+    expect(asFragment()).toMatchSnapshot()
+  })
+
+  it("renders ListingDetailsAside component sales", () => {
+    window.matchMedia = jest.fn().mockImplementation((query) => {
+      return {
+        matches: true,
+        media: query,
+        onchange: null,
+        addListener: jest.fn(),
+        removeListener: jest.fn(),
+        addEventListener: jest.fn(),
+        removeEventListener: jest.fn(),
+        dispatchEvent: jest.fn(),
+      }
+    })
+
+    const { asFragment } = render(<ListingDetailsAside listing={openSaleListing} imageSrc="" />)
 
     expect(asFragment()).toMatchSnapshot()
   })
