b61f3acf51cd941ed2201965726349aa632af625
Alula Beshue <135058064+alulabeshue-sfgov@users.noreply.github.com>
2024-09-05 09:58:29 -0700

    feat: DAH-2519 fcfs no lottery required (#2272)
    * feat: fcfs no lottery required
    
    * feat: moving content to en.json
    
    * feat: refactor and test
    
    * feat: adding link
    
    * feat: updating test
    
    * feat: adding bottom border
    
    * feat: updating snapshot
    
    * feat: clean up additional div
    
    * feat: translating learn more how it works link
    
    * fix: adjusting heading size

--- app/javascript/__tests__/modules/listingDetails/ListingDetailsAside.test.tsx
+++ app/javascript/__tests__/modules/listingDetails/ListingDetailsAside.test.tsx
@@ -3,6 +3,7 @@ import { render } from "@testing-library/react"
 import { ListingDetailsAside } from "../../../modules/listingDetailsAside/ListingDetailsAside"
 import { closedRentalListing } from "../../data/RailsRentalListing/listing-rental-closed"
 import { openSaleListing } from "../../data/RailsSaleListing/listing-sale-open"
+import { fcfsSaleListing } from "../../data/RailsSaleListing/listing-sale-fcfs"
 
 describe("ListingDetailsAside", () => {
   it("renders ListingDetailsAside component rental", () => {
@@ -39,6 +40,25 @@ describe("ListingDetailsAside", () => {
     })
 
     const { asFragment } = render(<ListingDetailsAside listing={openSaleListing} imageSrc="" />)
+
+    expect(asFragment()).toMatchSnapshot()
+  })
+
+  it("renders ListingDetailsAside component fcfs", () => {
+    window.matchMedia = jest.fn().mockImplementation((query) => {
+      return {
+        matches: true,
+        media: query,
+        onchange: null,
+        addListener: jest.fn(),
+        removeListener: jest.fn(),
+        addEventListener: jest.fn(),
+        removeEventListener: jest.fn(),
+        dispatchEvent: jest.fn(),
+      }
+    })
+
+    const { asFragment } = render(<ListingDetailsAside listing={fcfsSaleListing} imageSrc="" />)
 
     expect(asFragment()).toMatchSnapshot()
   })
