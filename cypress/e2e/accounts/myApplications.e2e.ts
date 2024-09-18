ce9c7ec52564776caac47c179e33703a89896c3e
Chad Brokaw <36685920+chadbrokaw@users.noreply.github.com>
2024-09-18 08:49:14 -0600

    fix: intake circleCI changes

--- cypress/e2e/accounts/myApplications.e2e.ts
+++ cypress/e2e/accounts/myApplications.e2e.ts
@@ -1,16 +1,8 @@
 describe("My Applications", () => {
   it("signs in", () => {
-    cy.task("log", "Sign in and navigate to My Applications page")
-    cy.url().then((url) => {
-      cy.task("log", `Current URL: ${url}`)
-    })
     cy.signIn()
     cy.addReactQueryParam()
 
-    cy.task("log", "Navigate to My Applications page")
-    cy.url().then((url) => {
-      cy.task("log", `Current URL: ${url}`)
-    })
     cy.intercept("/api/v1/account/my-applications", { applications: [] })
     cy.get('a[href="/my-applications"]').click()
     cy.addReactQueryParam()
@@ -18,29 +10,17 @@ describe("My Applications", () => {
     // There should be no applications right now
     cy.contains("It looks like you haven't applied to any listings yet.")
 
-    cy.task("log", "Navigate to the listings page")
-    cy.url().then((url) => {
-      cy.task("log", `Current URL: ${url}`)
-    })
     // Go to the rental listings page
     cy.get('a[href="/listings/for-rent"]').eq(1).click()
     cy.url().should("include", "/listings/for-rent")
     cy.contains("Rent affordable housing")
     cy.visit("/my-applications?react=true")
 
-    cy.task("log", "Navigate to the sales listings page")
-    cy.url().then((url) => {
-      cy.task("log", `Current URL: ${url}`)
-    })
     // GO to the sales listings page
     cy.get('a[href="/listings/for-sale"]').eq(1).click()
     cy.url().should("include", "/listings/for-sale")
     cy.contains("Buy affordable housing")
 
-    cy.task("log", "Navigate back to the my applications page")
-    cy.url().then((url) => {
-      cy.task("log", `Current URL: ${url}`)
-    })
     // Try to continue an application
     cy.intercept("/api/v1/account/my-applications", { fixture: "applications.json" })
     cy.visit("/my-applications?react=true")
@@ -51,10 +31,6 @@ describe("My Applications", () => {
       fixtures: "preferences.json",
     })
 
-    cy.task("log", "Continue an application")
-    cy.url().then((url) => {
-      cy.task("log", `Current URL: ${url}`)
-    })
     cy.get('a[href="/listings/a0W0P00000Hc7RcUAJ/apply/name"]').click()
     cy.contains("TEST Dahlia Commons Application")
 
@@ -65,10 +41,6 @@ describe("My Applications", () => {
     cy.contains("Rental units")
     cy.contains("Sale units")
 
-    cy.task("log", "Try to delete an application")
-    cy.url().then((url) => {
-      cy.task("log", `Current URL: ${url}`)
-    })
     // Try to delete an application
     cy.contains("TEST Dahlia Commons")
     cy.intercept("DELETE", "/api/v1/short-form/application/a0o6s000001dN5TAAU", {})
@@ -95,10 +67,6 @@ describe("My Applications", () => {
         cy.get("button").contains("#01517927").click()
       })
 
-    cy.task("log", "View the lottery results")
-    cy.url().then((url) => {
-      cy.task("log", `Current URL: ${url}`)
-    })
     cy.contains("Lottery Results")
     cy.contains("Your lottery ranking")
     cy.get("input#lotterySearchNumber").should("have.value", "01517927")
