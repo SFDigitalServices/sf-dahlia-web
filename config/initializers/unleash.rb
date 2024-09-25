0a66f4d689652b11553b232efdf3533e72d4667b
Jim Lin <jim.lin@sfgov.org>
2024-09-25 16:00:01 -0700

    fix: set refresh interval

--- config/initializers/unleash.rb
+++ config/initializers/unleash.rb
@@ -3,6 +3,9 @@ Unleash.configure do |config|
   config.url      = ENV.fetch('UNLEASH_URL', nil)
   config.logger   = Rails.logger
   config.custom_http_headers = { Authorization: ENV.fetch('UNLEASH_TOKEN_RAILS', nil) }
+  # this needs to be explicitly set, otherwise the toggle fetcher never re-fetches,
+  #   might be an Unleash SDK bug
+  config.refresh_interval = 15
 end
 
 ::UNLEASH = Unleash::Client.new # rubocop:disable Style/RedundantConstantBase
