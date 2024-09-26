80e06fd91f2a9dcfb8e9494bce8c7a95a6212c24
Jim Lin <jim.lin@sfgov.org>
2024-09-26 14:51:47 -0700

    chore: update Unleash log level

--- config/initializers/unleash.rb
+++ config/initializers/unleash.rb
@@ -1,8 +1,7 @@
 Unleash.configure do |config|
-  config.app_name  = 'webapp'
-  config.url       = ENV.fetch('UNLEASH_URL', nil)
-  config.logger    = Rails.logger
-  config.log_level = Logger::DEBUG
+  config.app_name = 'webapp'
+  config.url      = ENV.fetch('UNLEASH_URL', nil)
+  config.logger   = Rails.logger
   config.custom_http_headers = { Authorization: ENV.fetch('UNLEASH_TOKEN_RAILS', nil) }
 end
 
