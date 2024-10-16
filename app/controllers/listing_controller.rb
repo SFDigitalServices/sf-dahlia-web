7ce54ecbc2b82d217c80bf61e80c9d74cc00c4c6
tallulah <tallulah.kay@gmail.com>
2024-10-16 15:52:46 -0700

    DAH-2825 add redirects

--- app/controllers/listing_controller.rb
+++ app/controllers/listing_controller.rb
@@ -1,6 +1,6 @@
 # Controller for the listing detail page
 class ListingController < ApplicationController
-  before_action :check_fcfs_flag, only: [:how_to_apply]
+  before_action :should_allow_render, only: [:how_to_apply]
 
   def index
     @listing_detail_props = { assetPaths: static_asset_paths }
@@ -8,21 +8,7 @@ class ListingController < ApplicationController
 
   def how_to_apply
     @how_to_apply_props = { assetPaths: static_asset_paths }
-    listing_id = params['id']
-    listing = Force::ListingService.listing(listing_id)
-    # if listing is not ownership fcfs
-    if listing['RecordType']['Name'] != 'Ownership' ||
-       listing['Listing_Type'] != 'First Come, First Served' ||
-       # If the listing is closed, redirect
-       # TODO: DAH-2846 Status will be added to the listing object
-       # Until then, that field will be nil
-       (!listing['Status'].nil? && listing['Status'] != 'Active') ||
-       listing['Accepting_Online_Applications'] == false
-      redirect_back_or_to({ action: 'index' })
-      nil
-    else
-      render 'how_to_apply'
-    end
+    render 'how_to_apply'
   end
 
   protected
@@ -31,11 +17,39 @@ class ListingController < ApplicationController
     true
   end
 
-  def check_fcfs_flag
-    return if Rails.configuration.unleash.is_enabled? 'FCFS'
+  def should_allow_render
+    # if the FCFS feature flag is enabled
+    listing_id = params['id']
+    listing = Force::ListingService.listing(listing_id)
+    return if fcfs_flag_enabled &&
+              listing_type_fcfs_sales_bmr(listing) &&
+              listing_active(listing)
 
     # use the listing_path helper to redirect to the listing page
     # https://edgeguides.rubyonrails.org/routing.html#generating-paths-and-urls-from-code
     redirect_to listing_path(lang: params[:lang]) and return
+  end
+
+  private
+
+  def fcfs_flag_enabled
+    Rails.configuration.unleash.is_enabled? 'FCFS'
+  end
+
+  def listing_type_fcfs_sales_bmr(listing)
+    puts 'hello world'
+    # Record Type has to be Ownership
+    # Listing Type has to be First Come, First Served
+    listing['RecordType']['Name'] == 'Ownership' &&
+      listing['Listing_Type'] == 'First Come, First Served'
+  end
+
+  def listing_active(listing)
+    # Status has to be Active
+    # TODO: DAH-2846 Status will be added to the listing object
+    # Until then, that field will be nil
+    !listing['Status'].nil? && listing['Status'] == 'Active' &&
+      # Accepting Online Applications has to be true
+      listing['Accepting_Online_Applications'] == true
   end
 end
