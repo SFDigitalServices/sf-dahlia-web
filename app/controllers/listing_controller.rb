31caf9c58ceae9a18076d89232b9ac68c5f026f6
tallulah <tallulah.kay@gmail.com>
2024-10-16 12:11:44 -0700

    DAH-2810 basic redirect

--- app/controllers/listing_controller.rb
+++ app/controllers/listing_controller.rb
@@ -6,7 +6,15 @@ class ListingController < ApplicationController
 
   def how_to_apply
     @how_to_apply_props = { assetPaths: static_asset_paths }
-    render 'how_to_apply' if ENV['HOW_TO_APPLY_PAGE_REACT'].to_s.casecmp('true').zero?
+    listing_id = params['id']
+    listing = Force::ListingService.listing(listing_id)
+    if (!listing['Status'].nil? && listing['Status'] != 'Active') ||
+       listing['Accepting_Online_Applications'] == false
+      redirect_back_or_to({ action: 'index' })
+      nil
+    else
+      render 'how_to_apply'
+    end
   end
 
   protected
