a0d59d2575c5dce2e64f3316abccda70cff4ecc3
tallulah <tallulah.kay@gmail.com>
2024-10-17 09:42:50 -0700

    DAH-2741 add snapshot test

--- app/javascript/__tests__/pages/how-to-apply/how-to-apply.test.tsx
+++ app/javascript/__tests__/pages/how-to-apply/how-to-apply.test.tsx
@@ -1,4 +1,5 @@
 import React from "react"
+import { render } from "@testing-library/react"
 
 import { renderAndLoadAsync } from "../../__util__/renderUtils"
 import HowToApply from "../../../pages/how-to-apply/how-to-apply"
@@ -6,6 +7,12 @@ import HowToApply from "../../../pages/how-to-apply/how-to-apply"
 describe("<HowToApply />", () => {
   afterEach(() => {
     jest.restoreAllMocks()
+  })
+
+  it("renders HowToApply component", () => {
+    const { asFragment } = render(<HowToApply assetPaths={{}} />)
+
+    expect(asFragment()).toMatchSnapshot()
   })
 
   it("shows the correct header text", async () => {
