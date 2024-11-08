00dca0be4440e03f4f6e33598293191f0048d92d
Chad Brokaw <36685920+chadbrokaw@users.noreply.github.com>
2024-11-08 07:37:16 -0700

    fix: more tests

--- app/javascript/__tests__/hooks/useTranslate.test.tsx
+++ app/javascript/__tests__/hooks/useTranslate.test.tsx
@@ -1,4 +1,4 @@
-import { renderHook } from "@testing-library/react-hooks"
+import { renderHook } from "@testing-library/react"
 import useTranslate from "../../hooks/useTranslate"
 import * as useScript from "../../hooks/useScript"
 import * as usePollElementRender from "../../hooks/usePollElementRender"
