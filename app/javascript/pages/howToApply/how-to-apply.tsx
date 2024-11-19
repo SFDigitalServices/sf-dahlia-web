a931cd4ad15a5512853376b571815051203e3d83
cliu02 <32694266+cliu02@users.noreply.github.com>
2024-11-18 19:03:51 -0800

    fix: update strings

--- app/javascript/pages/howToApply/how-to-apply.tsx
+++ app/javascript/pages/howToApply/how-to-apply.tsx
@@ -362,7 +362,7 @@ const SubmitApplicationStep = ({ listing }: { listing: RailsSaleListing }) => {
 const HowToApplySection = ({ listing }: { listing: RailsSaleListing }) => {
   return (
     <>
-      <Header headerText={t("listings.apply.howToApply")} />
+      <Header headerText={t("pageTitle.howToApply.lowercase")} />
       <div className="pt-4" id="HowToApplySection">
         <ol className="process-list">
           <FillOutPdfAppStep />
