76d15c79a62872c03f7845668e4fe210e145b22e
Chad Brokaw <36685920+chadbrokaw@users.noreply.github.com>
2024-11-20 10:23:02 -0700

    feat: new account modal

--- app/javascript/authentication/SignInForm.tsx
+++ app/javascript/authentication/SignInForm.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useContext } from "react"
+import React, { useState } from "react"
 
 import {
   AppearanceStyleType,
@@ -10,18 +10,57 @@ import {
   AlertBox,
   SiteAlert,
   LinkButton,
+  debounce,
 } from "@bloom-housing/ui-components"
+import { Dialog, Link, Heading, Alert } from "@bloom-housing/ui-seeds"
 import { useForm } from "react-hook-form"
 
 import { getMyAccountPath } from "../util/routeUtil"
-import UserContext from "./context/UserContext"
 import EmailFieldset from "../pages/account/components/EmailFieldset"
 import PasswordFieldset from "../pages/account/components/PasswordFieldset"
-import { Link, Heading } from "@bloom-housing/ui-seeds"
 import "../pages/account/styles/account.scss"
+import { AxiosError } from "axios"
+import { confirmEmail, signIn } from "../api/authApiService"
+
+const NotConfirmedModal: React.FC<{
+  email: string
+  onClose: () => void
+}> = ({ email, onClose }) => {
+  const [emailSent, setEmailSent] = useState(false)
+  const [, setEmailSentError] = useState(false)
+  const requestEmail = debounce(() => {
+    confirmEmail(email)
+      .then(() => {
+        setEmailSent(true)
+      })
+      .catch(() => {
+        setEmailSentError(true)
+      })
+  }, 1000)
+
+  return (
+    <Dialog isOpen={!!email} onClose={onClose}>
+      <Dialog.Header>{t("signIn.newAccount.title")}</Dialog.Header>
+      {emailSent && (
+        <Alert className="sign-in-banner" onClose={onClose}>
+          {t("signIn.resentConfirmationMessage")}
+        </Alert>
+      )}
+      <Dialog.Content>{t("signIn.newAccount.p1", { email })}</Dialog.Content>
+      <Dialog.Content>{t("signIn.newAccount.p2")}</Dialog.Content>
+      <Dialog.Footer>
+        <Button type="submit" styleType={AppearanceStyleType.primary} onClick={onClose}>
+          {t("t.ok")}
+        </Button>
+        <Button styleType={AppearanceStyleType.secondary} onClick={requestEmail}>
+          {t("signIn.newAccount.sendEmailAgainButton")}
+        </Button>
+      </Dialog.Footer>
+    </Dialog>
+  )
+}
 
 const SignInForm = () => {
-  const { signIn } = useContext(UserContext)
   /* Form Handler */
   // TODO(DAH-1575): Upgrade React-Hook-Form. Note: When you update to Version 7 of react-hook-form, "errors" becomes: "formState: { errors }""
   // This is causing a linting issue with unbound-method, see open issue as of 10/21/2020:
@@ -29,6 +68,7 @@ const SignInForm = () => {
   // eslint-disable-next-line @typescript-eslint/unbound-method
   const { register, handleSubmit, errors, watch } = useForm()
   const [requestError, setRequestError] = useState<string>()
+  const [notConfirmed, setNotConfirmed] = useState<string | null>(null)
 
   const onSubmit = (data: { email: string; password: string }) => {
     const { email, password } = data
@@ -38,63 +78,64 @@ const SignInForm = () => {
         window.location.href = getMyAccountPath()
         window.scrollTo(0, 0)
       })
-      .catch(() => {
-        // TODO: handle sign-in error states
-        setRequestError(`${t("signIn.badCredentials")}`)
+      .catch((error: AxiosError<{ error: string; email: string }>) => {
+        if (error.response.data.error === "not_confirmed") {
+          setNotConfirmed(error.response.data.email)
+        } else {
+          // TODO: handle sign-in error states
+          setRequestError(`${t("signIn.badCredentials")}`)
+        }
       })
   }
 
   return (
-    <FormCard>
-      <div className="form-card__lead text-center border-b mx-0">
-        <Icon size="2xl" symbol="profile" />
-        <h2 className="form-card__title">{t("pageTitle.signIn")}</h2>
-      </div>
-      {requestError && (
-        <AlertBox onClose={() => setRequestError(undefined)} type="alert">
-          {requestError}
-        </AlertBox>
-      )}
-      <SiteAlert type="notice" dismissable />
-      <div className="form-card__group pt-0 border-b">
-        <Form id="sign-in" className="mt-10 relative" onSubmit={handleSubmit(onSubmit)}>
-          <EmailFieldset register={register} errors={errors} />
-          <span className="right-0 absolute">
-            <Link href="/forgot-password" className="forgot-password-link">
-              {t("forgotPassword.title")}
-            </Link>
-          </span>
-          <PasswordFieldset
-            register={register}
-            errors={errors}
-            watch={watch}
-            labelText={t("label.password")}
-            passwordType="signIn"
-          />
-          <div className="text-center mt-4">
-            <Button
-              styleType={AppearanceStyleType.primary}
-              type="submit"
-              onClick={() => {
-                //
-              }}
-            >
-              {t("pageTitle.signIn")}
-            </Button>
-          </div>
-        </Form>
-      </div>
-      <div className="form-card__group text-center">
-        <Heading size="2xl" priority={3}>
-          {t("createAccount.title.sentenceCase")}
-        </Heading>
-        <div className="py-4">
-          <p>{t("signIn.fillInFaster")}</p>
-          <p>{t("signIn.easilyCheckLottery")}</p>
+    <>
+      <NotConfirmedModal email={notConfirmed} onClose={() => setNotConfirmed(null)} />
+      <FormCard>
+        <div className="form-card__lead text-center border-b mx-0">
+          <Icon size="2xl" symbol="profile" />
+          <h2 className="form-card__title">{t("pageTitle.signIn")}</h2>
         </div>
-        <LinkButton href="/create-account">{t("label.createAccount")}</LinkButton>
-      </div>
-    </FormCard>
+        {requestError && (
+          <AlertBox onClose={() => setRequestError(undefined)} type="alert">
+            {requestError}
+          </AlertBox>
+        )}
+        <SiteAlert type="notice" dismissable />
+        <div className="form-card__group pt-0 border-b">
+          <Form id="sign-in" className="mt-10 relative" onSubmit={handleSubmit(onSubmit)}>
+            <EmailFieldset register={register} errors={errors} />
+            <span className="right-0 absolute">
+              <Link href="/forgot-password" className="forgot-password-link">
+                {t("forgotPassword.title")}
+              </Link>
+            </span>
+            <PasswordFieldset
+              register={register}
+              errors={errors}
+              watch={watch}
+              labelText={t("label.password")}
+              passwordType="signIn"
+            />
+            <div className="text-center mt-4">
+              <Button styleType={AppearanceStyleType.primary} type="submit">
+                {t("pageTitle.signIn")}
+              </Button>
+            </div>
+          </Form>
+        </div>
+        <div className="form-card__group text-center">
+          <Heading size="2xl" priority={3}>
+            {t("createAccount.title.sentenceCase")}
+          </Heading>
+          <div className="py-4">
+            <p>{t("signIn.fillInFaster")}</p>
+            <p>{t("signIn.easilyCheckLottery")}</p>
+          </div>
+          <LinkButton href="/create-account">{t("label.createAccount")}</LinkButton>
+        </div>
+      </FormCard>
+    </>
   )
 }
 
