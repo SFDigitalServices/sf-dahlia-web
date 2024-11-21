09f215281fee934c0a48bb5281cfbb55194aac85
Chad Brokaw <36685920+chadbrokaw@users.noreply.github.com>
2024-11-21 12:49:54 -0700

    fix: pull apart the sign in page

--- app/javascript/authentication/SignInForm.tsx
+++ app/javascript/authentication/SignInForm.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react"
+import React, { Dispatch, SetStateAction, useEffect, useState } from "react"
 
 import {
   AppearanceStyleType,
@@ -13,7 +13,7 @@ import {
   debounce,
 } from "@bloom-housing/ui-components"
 import { Dialog, Link, Heading, Alert } from "@bloom-housing/ui-seeds"
-import { useForm } from "react-hook-form"
+import { FieldValues, SubmitHandler, useForm } from "react-hook-form"
 
 import { getMyAccountPath } from "../util/routeUtil"
 import EmailFieldset from "../pages/account/components/EmailFieldset"
@@ -60,17 +60,72 @@ const NotConfirmedModal: React.FC<{
   )
 }
 
-const SignInForm = () => {
+const SignInFormCard = ({
+  onSubmit,
+  requestError,
+  setRequestError,
+}: {
+  onSubmit: SubmitHandler<FieldValues>
+  requestError: string
+  setRequestError: Dispatch<SetStateAction<string>>
+}) => {
   /* Form Handler */
   // TODO(DAH-1575): Upgrade React-Hook-Form. Note: When you update to Version 7 of react-hook-form, "errors" becomes: "formState: { errors }""
   // This is causing a linting issue with unbound-method, see open issue as of 10/21/2020:
   // https://github.com/react-hook-form/react-hook-form/issues/2887
   // eslint-disable-next-line @typescript-eslint/unbound-method
   const { register, handleSubmit, errors, watch } = useForm()
-  const [requestError, setRequestError] = useState<string>()
-  const [notConfirmed, setNotConfirmed] = useState<string | null>(null)
+  return (
+    <FormCard>
+      <div className="form-card__lead text-center border-b mx-0">
+        <Icon size="2xl" symbol="profile" />
+        <h2 className="form-card__title">{t("pageTitle.signIn")}</h2>
+      </div>
+      {requestError && (
+        <AlertBox onClose={() => setRequestError(undefined)} type="alert">
+          {requestError}
+        </AlertBox>
+      )}
+      <SiteAlert type="notice" dismissable />
+      <div className="form-card__group pt-0 border-b">
+        <Form id="sign-in" className="mt-10 relative" onSubmit={handleSubmit(onSubmit)}>
+          <EmailFieldset register={register} errors={errors} />
+          <span className="right-0 absolute">
+            <Link href="/forgot-password" className="forgot-password-link">
+              {t("forgotPassword.title")}
+            </Link>
+          </span>
+          <PasswordFieldset
+            register={register}
+            errors={errors}
+            watch={watch}
+            labelText={t("label.password")}
+            passwordType="signIn"
+          />
+          <div className="text-center mt-4">
+            <Button styleType={AppearanceStyleType.primary} type="submit">
+              {t("pageTitle.signIn")}
+            </Button>
+          </div>
+        </Form>
+      </div>
+      <div className="form-card__group text-center">
+        <Heading size="2xl" priority={3}>
+          {t("createAccount.title.sentenceCase")}
+        </Heading>
+        <div className="py-4">
+          <p>{t("signIn.fillInFaster")}</p>
+          <p>{t("signIn.easilyCheckLottery")}</p>
+        </div>
+        <LinkButton href="/create-account">{t("label.createAccount")}</LinkButton>
+      </div>
+    </FormCard>
+  )
+}
 
-  const onSubmit = (data: { email: string; password: string }) => {
+const onSubmit =
+  (setNotConfirmed: (email: string) => void, setRequestError: (error: string) => void) =>
+  (data: { email: string; password: string }) => {
     const { email, password } = data
 
     signIn(email, password)
@@ -88,6 +143,10 @@ const SignInForm = () => {
       })
   }
 
+const SignInForm = () => {
+  const [requestError, setRequestError] = useState<string>()
+  const [notConfirmed, setNotConfirmed] = useState<string | null>(null)
+
   useEffect(() => {
     const newAccountEmail: string | null = window.sessionStorage.getItem("newAccount")
     if (newAccountEmail) {
@@ -99,50 +158,11 @@ const SignInForm = () => {
   return (
     <>
       <NotConfirmedModal email={notConfirmed} onClose={() => setNotConfirmed(null)} />
-      <FormCard>
-        <div className="form-card__lead text-center border-b mx-0">
-          <Icon size="2xl" symbol="profile" />
-          <h2 className="form-card__title">{t("pageTitle.signIn")}</h2>
-        </div>
-        {requestError && (
-          <AlertBox onClose={() => setRequestError(undefined)} type="alert">
-            {requestError}
-          </AlertBox>
-        )}
-        <SiteAlert type="notice" dismissable />
-        <div className="form-card__group pt-0 border-b">
-          <Form id="sign-in" className="mt-10 relative" onSubmit={handleSubmit(onSubmit)}>
-            <EmailFieldset register={register} errors={errors} />
-            <span className="right-0 absolute">
-              <Link href="/forgot-password" className="forgot-password-link">
-                {t("forgotPassword.title")}
-              </Link>
-            </span>
-            <PasswordFieldset
-              register={register}
-              errors={errors}
-              watch={watch}
-              labelText={t("label.password")}
-              passwordType="signIn"
-            />
-            <div className="text-center mt-4">
-              <Button styleType={AppearanceStyleType.primary} type="submit">
-                {t("pageTitle.signIn")}
-              </Button>
-            </div>
-          </Form>
-        </div>
-        <div className="form-card__group text-center">
-          <Heading size="2xl" priority={3}>
-            {t("createAccount.title.sentenceCase")}
-          </Heading>
-          <div className="py-4">
-            <p>{t("signIn.fillInFaster")}</p>
-            <p>{t("signIn.easilyCheckLottery")}</p>
-          </div>
-          <LinkButton href="/create-account">{t("label.createAccount")}</LinkButton>
-        </div>
-      </FormCard>
+      <SignInFormCard
+        onSubmit={onSubmit(setNotConfirmed, setRequestError)}
+        requestError={requestError}
+        setRequestError={setRequestError}
+      />
     </>
   )
 }
