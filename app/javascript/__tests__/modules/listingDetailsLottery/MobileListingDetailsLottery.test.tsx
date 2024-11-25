e7481fd49ed1c88dc737a40b71ed909535ed9646
cliu02 <32694266+cliu02@users.noreply.github.com>
2024-11-25 14:32:50 -0800

    fix: DAH-3002 Restore lottery section for open sales lottery listings (#2427)
    * fix: check for fcfs instead of isOpen
    
    * test: remove incorrect test

--- app/javascript/__tests__/modules/listingDetailsLottery/MobileListingDetailsLottery.test.tsx
+++ app/javascript/__tests__/modules/listingDetailsLottery/MobileListingDetailsLottery.test.tsx
@@ -5,7 +5,6 @@ import { fcfsSaleListing } from "../../data/RailsSaleListing/listing-sale-fcfs"
 import { closedRentalListing } from "../../data/RailsRentalListing/listing-rental-closed"
 import { useFeatureFlag } from "../../../hooks/useFeatureFlag"
 import { notYetOpenSaleFcfsListing } from "../../data/RailsSaleListing/listing-sale-fcfs-not-yet-open"
-import { openSaleListing } from "../../data/RailsSaleListing/listing-sale-open"
 
 jest.mock("../../../hooks/useFeatureFlag", () => ({
   useFeatureFlag: jest.fn(),
@@ -52,16 +51,6 @@ describe("MobileListingDetailsLottery", () => {
 
     const { asFragment } = render(
       <MobileListingDetailsLottery listing={closedRentalListing} imageSrc={""} />
-    )
-
-    expect(asFragment()).toMatchSnapshot()
-  })
-
-  it("does not display if listing is open", () => {
-    ;(useFeatureFlag as jest.Mock).mockReturnValue({ flagsReady: true, unleashFlag: true })
-
-    const { asFragment } = render(
-      <MobileListingDetailsLottery listing={openSaleListing} imageSrc={""} />
     )
 
     expect(asFragment()).toMatchSnapshot()
