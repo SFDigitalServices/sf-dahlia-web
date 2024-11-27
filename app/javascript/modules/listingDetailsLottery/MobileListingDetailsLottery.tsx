95e6da59b8736f5e820032bee5d3c43488c32d34
cliu02 <32694266+cliu02@users.noreply.github.com>
2024-11-27 15:31:15 -0800

    fix: show for rental

--- app/javascript/modules/listingDetailsLottery/MobileListingDetailsLottery.tsx
+++ app/javascript/modules/listingDetailsLottery/MobileListingDetailsLottery.tsx
@@ -3,10 +3,11 @@ import { ListingDetailItem, Mobile, t } from "@bloom-housing/ui-components"
 import { RailsListing } from "../listings/SharedHelpers"
 import { ListingDetailsLotteryResults } from "./ListingDetailsLotteryResults"
 import { ListingDetailsProcess } from "../listingDetailsAside/ListingDetailsProcess"
-import { isFcfsSalesListing, isOpen } from "../../util/listingUtil"
+import { isFcfsSalesListing, isOpen, isRental } from "../../util/listingUtil"
 import { ListingDetailsLotteryInfo } from "./LotteryDetailsLotteryInfo"
 import { ListingDetailsWaitlist } from "../listingDetailsAside/ListingDetailsWaitlist"
 import { useFeatureFlag } from "../../hooks/useFeatureFlag"
+import { ListingDetailsOpenHouses } from "../listingDetailsAside/ListingDetailsOpenHouses"
 
 export interface ListingDetailsLotteryProps {
   imageSrc: string
@@ -31,6 +32,7 @@ export const MobileListingDetailsLottery = ({ imageSrc, listing }: ListingDetail
           <ListingDetailsLotteryInfo listing={listing} />
           <ListingDetailsLotteryResults listing={listing} />
           <ListingDetailsWaitlist listing={listing} />
+          {isRental(listing) && <ListingDetailsOpenHouses listing={listing} />}
           <ListingDetailsProcess listing={listing} isApplicationOpen={shouldRenderComponent} />
         </ListingDetailItem>
       </Mobile>
