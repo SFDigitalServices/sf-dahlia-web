24242a512f49e606db78967e7ca7110007789400
Chad <36685920+chadbrokaw@users.noreply.github.com>
2024-11-27 13:27:12 -0700

    feat: DAH-2881 New Account modal (#2425)
    * feat: new account modal
    
    * test: clean up tests
    
    * tests: stabilize tests more
    
    * test: further refine test
    
    * Remove debounce
    
    * Integrate create account redirect
    
    * fix: create account test
    
    * fix: pull apart the sign in page
    
    * fix: add error message
    
    * fix: pr review
    
    * fix: translations ordering
    
    * feat: remove debounce
    
    * fix request function

--- app/javascript/__tests__/pages/SignIn.test.tsx
+++ app/javascript/__tests__/pages/SignIn.test.tsx
@@ -2,6 +2,9 @@ import React from "react"
 
 import SignIn from "../../pages/sign-in"
 import { renderAndLoadAsync } from "../__util__/renderUtils"
+import { screen, waitFor } from "@testing-library/react"
+import { userEvent } from "@testing-library/user-event"
+import { post } from "../../api/apiService"
 
 jest.mock("react-helmet-async", () => {
   return {
@@ -10,15 +13,41 @@ jest.mock("react-helmet-async", () => {
   }
 })
 
-jest.spyOn(window, "alert").mockImplementation(() => {})
+jest.mock("../../api/apiService", () => ({
+  post: jest.fn(),
+}))
+
+jest.mock("@bloom-housing/ui-seeds", () => {
+  const originalModule = jest.requireActual("@bloom-housing/ui-seeds")
+
+  const MockDialog = ({ children, isOpen }: { children: React.ReactNode; isOpen: boolean }) =>
+    isOpen ? <div data-testid="modalMock">{children}</div> : null
+  MockDialog.Header = ({ children }: { children: React.ReactNode }) => <div>{children}</div>
+  MockDialog.Content = ({ children }: { children: React.ReactNode }) => <div>{children}</div>
+  MockDialog.Footer = ({ children }: { children: React.ReactNode }) => <div>{children}</div>
+
+  return {
+    __esModule: true,
+    ...originalModule,
+    Dialog: MockDialog,
+  }
+})
 
 describe("<SignIn />", () => {
+  beforeEach(() => {
+    jest.resetAllMocks()
+  })
+
   it("alerts if redirect is true", async () => {
-    window.sessionStorage.setItem("redirect", "true")
+    window.sessionStorage.setItem("newAccount", "test@test.com")
 
     await renderAndLoadAsync(<SignIn assetPaths={{}} />)
 
-    expect(window.alert).toHaveBeenCalled()
+    await waitFor(() => {
+      expect(screen.getByText("Check your email to finish creating your account")).not.toBeNull()
+      expect(screen.getByText(/we sent a link to test@test\.com\./i)).not.toBeNull()
+      expect(screen.queryByText("Email sent. Check your email.")).toBeNull()
+    })
   })
 
   it("shows the correct form text", async () => {
@@ -33,5 +62,118 @@ describe("<SignIn />", () => {
     ).not.toBeNull()
     expect(getByText("Password")).not.toBeNull()
     expect(getByText("Create an account")).not.toBeNull()
+  })
+
+  it("shows the correct error message when bad credentials are entered", async () => {
+    ;(post as jest.Mock).mockRejectedValueOnce({
+      response: {
+        status: 401,
+        data: { error: "bad_credentials" },
+      },
+    })
+
+    await renderAndLoadAsync(<SignIn assetPaths={{}} />)
+
+    await userEvent.type(screen.getByRole("textbox", { name: /email/i }), "test@test.com")
+    await userEvent.type(screen.getByLabelText(/^password$/i), "Password1")
+    await userEvent.click(screen.getByRole("button", { name: /sign in/i }))
+
+    await waitFor(() => {
+      expect(post).toHaveBeenCalledWith("/api/v1/auth/sign_in", {
+        email: "test@test.com",
+        password: "Password1",
+      })
+    })
+
+    await waitFor(() => {
+      expect(screen.getByText(/invalid login credentials\. please try again\./i)).not.toBeNull()
+    })
+  })
+
+  it("shows the correct expired unconfirmed modal", async () => {
+    ;(post as jest.Mock).mockRejectedValueOnce({
+      response: {
+        status: 422,
+        data: { error: "not_confirmed", email: "test@test.com" },
+      },
+    })
+
+    await renderAndLoadAsync(<SignIn assetPaths={{}} />)
+
+    await userEvent.type(screen.getByRole("textbox", { name: /email/i }), "test@test.com")
+    await userEvent.type(screen.getByLabelText(/^password$/i), "Password1")
+    await userEvent.click(screen.getByRole("button", { name: /sign in/i }))
+
+    await waitFor(() => {
+      expect(post).toHaveBeenCalledWith("/api/v1/auth/sign_in", {
+        email: "test@test.com",
+        password: "Password1",
+      })
+    })
+
+    await waitFor(() => {
+      expect(screen.getByText("Check your email to finish creating your account")).not.toBeNull()
+      expect(screen.getByText(/we sent a link to test@test\.com\./i)).not.toBeNull()
+      expect(screen.queryByText("Email sent. Check your email.")).toBeNull()
+    })
+
+    jest.resetAllMocks()
+    ;(post as jest.Mock).mockResolvedValueOnce({
+      response: {
+        status: 200,
+        data: { success: true },
+      },
+    })
+
+    await userEvent.click(
+      screen.getByRole("button", {
+        name: /send email again/i,
+      })
+    )
+
+    await waitFor(() => {
+      expect(post).toHaveBeenCalledWith("/api/v1/auth/confirmation", { email: "test@test.com" })
+    })
+
+    await waitFor(() => {
+      expect(screen.getByText("Email sent. Check your email.")).not.toBeNull()
+    })
+
+    jest.resetAllMocks()
+    ;(post as jest.Mock).mockRejectedValueOnce({
+      response: {
+        status: 400,
+        data: { error: "Bad Request", message: "Invalid input" },
+      },
+    })
+
+    await userEvent.click(
+      screen.getByRole("button", {
+        name: /send email again/i,
+      })
+    )
+
+    await waitFor(() => {
+      expect(post).toHaveBeenCalledWith("/api/v1/auth/confirmation", { email: "test@test.com" })
+    })
+
+    await waitFor(() => {
+      expect(
+        screen.getByText(
+          "Something went wrong. We could not send an email. Try sending it again or refreshing the page."
+        )
+      ).not.toBeNull()
+    })
+
+    await userEvent.click(
+      screen.getByRole("button", {
+        name: /ok/i,
+      })
+    )
+
+    await waitFor(() => {
+      expect(screen.queryByText("Check your email to finish creating your account")).toBeNull()
+      expect(screen.queryByText("Email sent. Check your email.")).toBeNull()
+    })
   })
 })
