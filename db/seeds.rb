9f1de7017763450abafbcb73e9454092797dc157
Chad Brokaw <36685920+chadbrokaw@users.noreply.github.com>
2024-12-23 16:36:20 -0700

    gate around environment

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,9 +2,11 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-User.create!(
-  email: "foo@foo.com",
-  password: "barbarbar1",
-  password_confirmation: "barbarbar1",
-  confirmed_at: Time.now.utc # Marks the user as confirmed
-)
+if Rails.env.development? || Rails.env.test?
+    User.create!(
+      email: "foo@foo.com",
+      password: "barbarbar1",
+      password_confirmation: "barbarbar1",
+      confirmed_at: Time.now.utc # Marks the user as confirmed
+    )
+  end
