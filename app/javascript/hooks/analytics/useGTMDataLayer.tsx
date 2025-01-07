2f4af60e912d83575e194bbf391bf48938b6b679
Chad Brokaw <36685920+chadbrokaw@users.noreply.github.com>
2025-01-07 08:20:34 -0700

    Improve testing

--- app/javascript/hooks/analytics/useGTMDataLayer.tsx
+++ app/javascript/hooks/analytics/useGTMDataLayer.tsx
@@ -7,6 +7,10 @@ export const useGTMDataLayer = () => {
       console.error("Data must be an object when pushing to the data layer.")
       return
     }
+    if (!event) {
+      console.error("An event must be provided when pushing to the data layer.")
+      return
+    }
     TagManager.dataLayer({ dataLayer: { event, ...data } })
   }, [])
 
