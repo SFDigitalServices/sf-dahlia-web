4c4bafcbcebcafe2559e2301f5474d9e2827a908
tallulah <tallulah.kay@gmail.com>
2025-02-03 14:02:04 -0800

    fix: DAH-3169 flip logic

--- app/constraints/dalp_constraint.rb
+++ app/constraints/dalp_constraint.rb
@@ -3,6 +3,10 @@ class DalpConstraint
   def matches?(request)
     listing_id = request.params['id']
     listing = Force::ListingService.listing(listing_id)
-    return true if listing['Custom_Listing_Type'] != 'Downpayment Assistance Loan Program'
+    unless listing['Custom_Listing_Type'] == 'Downpayment Assistance Loan Program'
+      return true
+    end
+
+    false
   end
 end
