aad044c7133f4bdf40866e8372ff009a8c174068
tallulah <tallulah.kay@gmail.com>
2025-02-03 15:36:31 -0800

    test: DAH-3169 add existence check

--- app/constraints/dalp_constraint.rb
+++ app/constraints/dalp_constraint.rb
@@ -3,7 +3,7 @@ class DalpConstraint
   def matches?(request)
     listing_id = request.params['id']
     listing = Force::ListingService.listing(listing_id)
-    unless listing['Custom_Listing_Type'] == 'Downpayment Assistance Loan Program'
+    unless listing && listing['Custom_Listing_Type'] == 'Downpayment Assistance Loan Program'
       return true
     end
 
