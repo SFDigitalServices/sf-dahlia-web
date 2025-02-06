ea5ed3772c1b7762c913a59dcfe7f53bc44bdf51
Jim Lin <jim.lin@sfgov.org>
2025-02-06 09:49:58 -0800

    chore: add comment

--- spec/e2e/conf.js
+++ spec/e2e/conf.js
@@ -1,6 +1,7 @@
 var config = require('./conf-shared')
 
 config.baseUrl = 'http://localhost:3000/'
+// chromedriver version in package.json should match chrome version installed by .circleci/config.yml
 config.chromeDriver = '../../node_modules/chromedriver/bin/chromedriver',
 config.capabilities = {
   browserName: 'chrome',
