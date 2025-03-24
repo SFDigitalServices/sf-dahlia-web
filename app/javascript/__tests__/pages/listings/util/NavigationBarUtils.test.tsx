cbff1dd935368066d3510bff5187950df770d423
Josh Chou <626661+josh-chou@users.noreply.github.com>
2025-03-24 10:36:21 -0700

    test: add new handleSectionHeaderEntries test

--- app/javascript/__tests__/pages/listings/util/NavigationBarUtils.test.tsx
+++ app/javascript/__tests__/pages/listings/util/NavigationBarUtils.test.tsx
@@ -34,6 +34,23 @@ describe("handleSectionHeaderEvents", () => {
 
     expect(handleSectionHeaderEntries(events)).toEqual("enter-a-lottery")
   })
+
+  it("handleSectionHeaderEvents set the correct active item based on viewport visibility", () => {
+    const events: IntersectionObserverEntry[] = [
+      mockIntersectionObserverEntry("enter-a-lottery", false),
+      mockIntersectionObserverEntry("buy-now", false),
+      mockIntersectionObserverEntry("lottery-results", false),
+    ]
+
+    const mockQuerySelector = jest.fn()
+    mockQuerySelector.mockReturnValueOnce(null).mockReturnValueOnce({
+      getBoundingClientRect: jest.fn().mockReturnValue({ top: 0 }),
+      clientHeight: 50,
+    } as unknown as HTMLElement)
+    document.querySelector = mockQuerySelector
+
+    expect(handleSectionHeaderEntries(events)).toEqual("buy-now")
+  })
 })
 
 describe("isElementInViewport", () => {
