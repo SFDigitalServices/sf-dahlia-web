af56989d258264da66557b89f87c32610527db69
Cade Wolcott <cade.wolcott@exygy.com>
2025-04-02 13:54:22 -0500

    feat: DAH-3233 add message service api url env var

--- app/controllers/api/v1/short_form_controller.rb
+++ app/controllers/api/v1/short_form_controller.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require 'http'
+
 # RESTful JSON API to query for short form actions
 class Api::V1::ShortFormController < ApiController
   before_action :authenticate_user!,
@@ -228,17 +230,19 @@ class Api::V1::ShortFormController < ApiController
       listingName: listingName.to_s,
       lotteryDate: lotteryDate.to_s,
     }
-    Rails.logger.info("Sending application submission message: #{submission_fields}")
+    application_submission_endpoint = "#{ENV.fetch('MESSAGE_SERVICE_URL',
+                                                   nil)}/messages/application-submission"
+    Rails.logger.info("Sending application submission message: #{submission_fields} to #{application_submission_endpoint}")
     message_response = HTTP.headers('x-api-key' => ENV.fetch('MESSAGE_SERVICE_API_KEY', nil)).post(
-      'http://localhost:3001/messages/application-submission', params: submission_fields
+      application_submission_endpoint, params: submission_fields
     )
     if message_response.code >= 400
       Rails.logger.error("Error sending application submission message: #{message_response}")
     else
       Rails.logger.info("Application Submission message sent: #{message_response}")
     end
-  rescue HTTP::Error
-    Rails.logger.error("Error sending application submission message: #{message_response}")
+  rescue StandardError => e
+    Rails.logger.error("Standard Error sending application submission message: #{e.message}")
   end
 
   def send_submit_app_confirmation(response)
@@ -246,6 +250,7 @@ class Api::V1::ShortFormController < ApiController
     lottery_date = listing.Lottery_Date
     Time.zone.parse(lottery_date).strftime('%B %e, %Y') if lottery_date
 
+    # TODO: Add feature toggle
     send_application_submission_message(
       lotteryNumber: response&.[]('lotteryNumber').to_s,
       listingName: listing.Name,
