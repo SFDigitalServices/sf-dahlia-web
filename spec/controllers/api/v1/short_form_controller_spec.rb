bfe3e5d0a49273d8dd114e66ea5c30f636bb1d76
Cade Wolcott <cade.wolcott@exygy.com>
2025-04-03 16:52:46 -0500

    test: add test for app confirmation

--- spec/controllers/api/v1/short_form_controller_spec.rb
+++ spec/controllers/api/v1/short_form_controller_spec.rb
@@ -3,16 +3,75 @@
 require 'rails_helper'
 require 'support/vcr_setup'
 
-describe Api::V1::ShortFormController do
+describe Api::V1::ShortFormController, type: :controller do
+  describe '#submit_application' do
+    let(:application_params) do
+      {
+        status: 'submitted',
+        primaryApplicant: { email: 'test@example.com' },
+        listingID: '12345',
+        id: '12345',
+        applicationLanguage: 'en',
+        locale: 'en',
+        action: 'submit',
+      }
+    end
+
+    let(:params) do
+      ActionController::Parameters.new(
+        application: application_params,
+        uploaded_file: { file: 'file.pdf' },
+      )
+    end
+
+    let(:applicant_attrs) do
+      {
+        contactId: 'user_contact_id',
+        webAppID: 'current_user_id',
+      }
+    end
+
+    let(:response_data) do
+      {
+        'lotteryNumber' => '67890',
+        'primaryApplicant' => {
+          'firstName' => 'John',
+          'lastName' => 'Doe',
+        },
+      }
+    end
+
+    before do
+      allow(controller).to receive(:params).and_return(params)
+      allow(controller).to receive(:application_params).and_return(application_params)
+      allow(controller).to receive(:applicant_attrs).and_return(applicant_attrs)
+      allow(Force::ShortFormService).to receive(:create_or_update).and_return(response_data)
+      allow(DahliaBackend::MessageService).to receive(:send_application_confirmation)
+    end
+
+    it 'submits the application and sends confirmation' do
+      # Precise expectations with arguments
+      expect(Force::ShortFormService).to receive(:create_or_update)
+        .with(application_params, applicant_attrs)
+        .and_return(response_data)
+
+      expect(DahliaBackend::MessageService).to receive(:send_application_confirmation)
+        .with(application_params, response_data)
+
+      post :submit_application
+      expect(response).to have_http_status(:ok)
+    end
+  end
+
   describe '#lending_institutions' do
-    it 'returns calls Force::ShortFormService' do
+    it 'retrieves lending institutions' do
       expect(Force::ShortFormService).to receive(:lending_institutions)
       get :lending_institutions
     end
   end
 
   describe '#lending_institutions_dalp' do
-    it 'returns calls Force::ShortFormService' do
+    it 'retrieves DALP lending institutions' do
       expect(Force::ShortFormService).to receive(:lending_institutions_dalp)
       get :lending_institutions_dalp
     end
