993be00ca64bafb2c5fdc7a5a7140539592fea76
Josh Chou <626661+josh-chou@users.noreply.github.com>
2025-04-10 16:21:09 -0700

    test: add test for MenuIntersectionObserver

--- app/javascript/__tests__/pages/listings/util/NavigationBarUtils.test.tsx
+++ app/javascript/__tests__/pages/listings/util/NavigationBarUtils.test.tsx
@@ -1,4 +1,6 @@
-import { handleSectionHeaderEntries } from "../../../../modules/listings/util/NavigationBarUtils"
+import React from "react"
+import { cleanup, render } from "@testing-library/react"
+import * as navBarUtils from "../../../../modules/listings/util/NavigationBarUtils"
 
 const mockIntersectionObserverEntry = (id, isIntersecting, intersectionRatio) => {
   return {
@@ -8,13 +10,37 @@ const mockIntersectionObserverEntry = (id, isIntersecting, intersectionRatio) =>
   } as IntersectionObserverEntry
 }
 
+const mockIntersectionObserver = jest.fn()
+const mockResizeObserver = jest.fn()
+const mockResizeObserveFunction = jest.fn()
+const mockResizeDisconnectFunction = jest.fn()
+
 describe("handleSectionHeaderEvents", () => {
+  beforeEach(() => {
+    mockIntersectionObserver.mockReturnValue({
+      observe: () => null,
+    })
+    window.IntersectionObserver = mockIntersectionObserver
+
+    mockResizeObserver.mockReturnValue({
+      observe: mockResizeObserveFunction,
+      disconnect: mockResizeDisconnectFunction,
+    })
+    window.ResizeObserver = mockResizeObserver
+  })
+
+  afterEach(() => {
+    cleanup()
+    jest.clearAllMocks()
+    jest.resetAllMocks()
+  })
+
   it("handleSectionHeaderEvents sets the correct active item", () => {
     const events: IntersectionObserverEntry[] = [
       mockIntersectionObserverEntry("buy-now", true, 0.2),
     ]
 
-    const result = handleSectionHeaderEntries(events)
+    const result = navBarUtils.handleSectionHeaderEntries(events)
 
     expect(result).toEqual("buy-now")
   })
@@ -22,8 +48,26 @@ describe("handleSectionHeaderEvents", () => {
   it("handleSectionHeaderEvents returns null for empty arrays", () => {
     const events: IntersectionObserverEntry[] = []
 
-    const result = handleSectionHeaderEntries(events)
+    const result = navBarUtils.handleSectionHeaderEntries(events)
 
     expect(result).toBeUndefined()
+  })
+
+  it("component MenuIntersectionObserver can create and add an intersection observer", () => {
+    const mockElement = jest.fn()
+    mockElement.mockReturnValue({
+      id: "elem_id",
+      clientHeight: 100,
+    })
+
+    const setActiveItem = jest.fn()
+    window.innerHeight = 200
+    const ref = React.createRef<navBarUtils.MenuIntersectionObserverHandle>()
+
+    render(<navBarUtils.MenuIntersectionObserver ref={ref} setActiveItem={setActiveItem} />)
+
+    ref.current?.addObservedElement(mockElement)
+    expect(mockResizeObserver).toHaveBeenCalled()
+    expect(mockIntersectionObserver).toHaveBeenCalled()
   })
 })
