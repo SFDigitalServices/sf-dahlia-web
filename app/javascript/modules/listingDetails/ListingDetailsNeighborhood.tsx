d1c07011f0b51c076e3ef730f776bc8f0aaeed5a
Justin Caovan <justincaovan@gmail.com>
2025-07-01 14:10:11 -0700

    fix: DAH-2607 add mobile view to neighborhood map title (#2658)
    * fix: add mobile view to neighborhood map title
    
    * fix: update listing snapshot
    
    * refactor: hide changes behind feature flag

--- app/javascript/modules/listingDetails/ListingDetailsNeighborhood.tsx
+++ app/javascript/modules/listingDetails/ListingDetailsNeighborhood.tsx
@@ -1,8 +1,9 @@
 import React from "react"
-import { ListingDetailItem, t } from "@bloom-housing/ui-components"
+import { Desktop, ListingDetailItem, Mobile, t } from "@bloom-housing/ui-components"
 import { getListingAddressString } from "../../util/listingUtil"
 import { RailsListing } from "../listings/SharedHelpers"
 import { getCurrentLanguage } from "../../util/languageUtil"
+import { useFeatureFlag } from "../../hooks/useFeatureFlag"
 
 export interface ListingDetailsEligibilityProps {
   imageSrc: string
@@ -14,6 +15,10 @@ export const ListingDetailsNeighborhood = ({
   listing,
 }: ListingDetailsEligibilityProps) => {
   const listingAddress = getListingAddressString(listing)
+  const { unleashFlag: translationsReady } = useFeatureFlag(
+    "temp.webapp.listings.neighborhoodHeader",
+    false
+  )
 
   if (!listingAddress) {
     return null
@@ -22,6 +27,53 @@ export const ListingDetailsNeighborhood = ({
   const lang = getCurrentLanguage(window.location.pathname)
   const iframeUrl = `https://www.google.com/maps/embed/v1/place?key=${process.env.GOOGLE_PLACES_KEY}&q=${listingAddress}&language=${lang}`
 
+  if (translationsReady) {
+    return (
+      <>
+        <Desktop>
+          <ListingDetailItem
+            desktopClass="bg-primary-lighter"
+            imageAlt={""}
+            imageSrc={imageSrc}
+            title={t("listings.neighborhood.header")}
+            subtitle={t("listings.neighborhood.subheader")}
+          >
+            <iframe
+              className="md:mb-6 md:pl-16 pl-0"
+              src={iframeUrl}
+              title={t("listings.neighborhood.mapTitle", {
+                listingAddress: listingAddress,
+              })}
+              width="100%"
+              height="450"
+            />
+          </ListingDetailItem>
+        </Desktop>
+
+        <Mobile>
+          <ListingDetailItem
+            desktopClass="bg-primary-lighter"
+            imageAlt={""}
+            imageSrc={imageSrc}
+            title={t("listings.neighborhood.header.mobile")}
+            subtitle={t("listings.neighborhood.subheader")}
+          >
+            <iframe
+              className="md:mb-6 md:pl-16 pl-0"
+              src={iframeUrl}
+              title={t("listings.neighborhood.mapTitle", {
+                listingAddress: listingAddress,
+              })}
+              width="100%"
+              height="450"
+            />
+          </ListingDetailItem>
+        </Mobile>
+      </>
+    )
+  }
+
+  // TODO:Remove once translations are ready
   return (
     <ListingDetailItem
       desktopClass="bg-primary-lighter"
