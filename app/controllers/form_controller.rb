a14223b592345d54c932d68df17efc8b5f4e7341
Jim Lin <jim.lin@sfgov.org>
2025-08-06 14:40:50 -0700

    test: rails controller

--- app/controllers/form_controller.rb
+++ app/controllers/form_controller.rb
@@ -1,10 +1,11 @@
-# Controller for Listing Application Forms
+# Controller for pages with forms
 class FormController < ApplicationController
   def listing_apply_form
     @listing_apply_form_props = {
       assetPaths: static_asset_paths,
       listingId: params[:id],
     }
+    render 'listing_apply_form'
   end
 
   protected
