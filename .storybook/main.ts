03269b0bbf7833824cac867a8263dcd81b586002
cliu02 <32694266+cliu02@users.noreply.github.com>
2025-08-14 13:48:08 -0700

    fix: update story glob pattern

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,7 +1,7 @@
 import type { StorybookConfig } from "@storybook/react-webpack5"
 
 const config: StorybookConfig = {
-  stories: ["../stories/**/*.mdx", "../stories/**/*.stories.@(ts|tsx)"],
+  stories: ["../stories/**/*.mdx", "../stories/**/*.stories.ts"],
   addons: [
     "@storybook/addon-webpack5-compiler-swc",
     "@storybook/addon-docs",
