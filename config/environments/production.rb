3031f00f0a5742d1bce6319932c923af764a9ce0
cliu02 <32694266+cliu02@users.noreply.github.com>
2025-08-25 15:09:25 -0700

    fix: test config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,10 +27,6 @@ Rails.application.configure do
     config.logger = ActiveSupport::TaggedLogging.new(logger)
   end
 
-  # Disable serving static files from the `/public` folder by default since
-  # Apache or NGINX already handles this.
-  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
-
   # cache for 1 year
   config.public_file_server.headers = {
     'Cache-Control' => 'public, max-age=31536000'
