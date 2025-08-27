af06a0097c0ffdc223f4867be5a0537040b9bb0f
Jim Lin <jim.lin@sfgov.org>
2025-08-27 16:20:16 -0700

    test: use uuid package again

--- app/javascript/formEngine/recursiveRenderer.tsx
+++ app/javascript/formEngine/recursiveRenderer.tsx
@@ -1,6 +1,7 @@
 import React from "react"
 import type { ComponentSchema } from "./formSchemas"
 import getFormComponentRegistry from "./formComponentRegistry"
+import { v4 as uuidv4 } from "uuid"
 
 interface FormEngineProps {
   schema: ComponentSchema & Record<string, unknown>
@@ -16,12 +17,8 @@ const RecursiveRenderer = ({ schema }: FormEngineProps) => {
     children = schema.children.map((childSchema) => <RecursiveRenderer schema={childSchema} />)
   }
 
-  if (!self || !self.crypto || typeof self.crypto.randomUUID !== "function") {
-    console.error("browser does not support crypto.randomUUID()")
-  }
-
   // set key property to avoid weird behavior when navigating between form steps
-  const props = { ...schema.props, key: crypto.randomUUID() }
+  const props = { ...schema.props, key: uuidv4() }
 
   return React.createElement(ComponentToRender, props, ...children)
 }
