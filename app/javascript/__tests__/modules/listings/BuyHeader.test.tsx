3c1dc7d709fbac0d71dd2b881d476114e62a1720
Justin Caovan <justincaovan@gmail.com>
2025-09-02 10:41:42 -0700

    chore: DAH-3493 remove feature toggle for seeHomesForSale (#2689)
    * chore: remove feature toggle for seeHomesForSale
    
    * refactor: update test and snapshot for updated text

--- app/javascript/__tests__/modules/listings/BuyHeader.test.tsx
+++ app/javascript/__tests__/modules/listings/BuyHeader.test.tsx
@@ -1,36 +1,9 @@
 import React from "react"
 import { render } from "@testing-library/react"
 import BuyHeader from "../../../modules/listings/BuyHeader"
-import { useFeatureFlag } from "../../../hooks/useFeatureFlag"
-
-jest.mock("../../../hooks/useFeatureFlag", () => ({
-  useFeatureFlag: jest.fn(),
-}))
 
 describe("BuyHeader", () => {
-  it("renders BuyHeader component with See the listings button", async () => {
-    ;(useFeatureFlag as jest.Mock).mockReturnValue({ flagsReady: true, unleashFlag: false })
-    window.matchMedia = jest.fn().mockImplementation((query) => {
-      return {
-        matches: true,
-        media: query,
-        onchange: null,
-        addListener: jest.fn(),
-        removeListener: jest.fn(),
-        addEventListener: jest.fn(),
-        removeEventListener: jest.fn(),
-        dispatchEvent: jest.fn(),
-      }
-    })
-
-    const { findByText, asFragment } = render(<BuyHeader />)
-
-    expect(asFragment()).toMatchSnapshot()
-    expect(await findByText("See the listings")).toBeDefined()
-  })
-
   it("renders BuyHeader component with See homes for sale button", async () => {
-    ;(useFeatureFlag as jest.Mock).mockReturnValue({ flagsReady: true, unleashFlag: true })
     window.matchMedia = jest.fn().mockImplementation((query) => {
       return {
         matches: true,
