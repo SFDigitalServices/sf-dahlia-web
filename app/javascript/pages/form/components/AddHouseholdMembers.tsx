bac781313d67fd33b077f29f762ee502a8cad0d5
Jim Lin <jim.lin@sfgov.org>
2025-09-12 12:50:58 -0700

    fix: onClick handler

--- app/javascript/pages/form/components/AddHouseholdMembers.tsx
+++ app/javascript/pages/form/components/AddHouseholdMembers.tsx
@@ -19,8 +19,8 @@ const AddHouseholdMembers = () => {
         <Heading>AddHouseholdMembers Component</Heading>
       </CardSection>
       <CardSection>
-        <Button onClick={() => handleNextStep}>{t("label.addHouseholdMember")}</Button>
-        <Button onClick={() => handleNextStep()}>{t("label.doneAddingPeople")}</Button>
+        <Button onClick={handleNextStep}>{t("label.addHouseholdMember")}</Button>
+        <Button onClick={handleNextStep}>{t("label.doneAddingPeople")}</Button>
       </CardSection>
     </>
   )
