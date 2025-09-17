1e0059805f27f4f69bf66c2e37f596ad7de1a0dc
cliu02 <32694266+cliu02@users.noreply.github.com>
2025-09-17 15:17:59 -0700

    feat: forms overview page

--- app/javascript/pages/form/components/ListingApplyOverview.tsx
+++ app/javascript/pages/form/components/ListingApplyOverview.tsx
@@ -3,27 +3,40 @@ import { t } from "@bloom-housing/ui-components"
 import { Button, Heading } from "@bloom-housing/ui-seeds"
 import { CardSection } from "@bloom-housing/ui-seeds/src/blocks/Card"
 import { useFormEngineContext } from "../../../formEngine/formEngineContext"
+import "./ListingApplyOverview.scss"
+import { getDocumentChecklistPath } from "../../../util/routeUtil"
+import { renderInlineMarkup } from "../../../util/languageUtil"
 
 const ListingApplyOverview = () => {
   const formEngineContext = useFormEngineContext()
   const { handleNextStep, handlePrevStep } = formEngineContext
 
   return (
-    <>
+    <div className="listing-apply-overview">
       <CardSection>
         <Button variant="text" onClick={handlePrevStep}>
           {t("t.back")}
         </Button>
       </CardSection>
       <CardSection>
-        <Heading>ListingApplyOverview Component</Heading>
+        <Heading priority={2} size="2xl" className="listing-apply-overview-title">
+          {t("a4Overview.title")}
+        </Heading>
+        <hr />
+        <div className="listing-apply-overview-text">
+          <p>{t("a4Overview.p1")}</p>
+          <p>{renderInlineMarkup(t("a4Overview.p2", { href: getDocumentChecklistPath() }))}</p>
+          <p>{t("a4Overview.p3")}</p>
+          <p>{t("a4Overview.p4")}</p>
+          <p>{t("a4Overview.warning")}</p>
+        </div>
       </CardSection>
-      <CardSection>
+      <CardSection className="listing-apply-overview-footer">
         <Button variant="primary" onClick={handleNextStep}>
           {t("t.next")}
         </Button>
       </CardSection>
-    </>
+    </div>
   )
 }
 
