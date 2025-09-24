12f15f4add3cdc373181afad194b4a436c96ef28
Jim Lin <133722877+jimlin-sfgov@users.noreply.github.com>
2025-09-24 13:49:50 -0700

    feat: DAH-2610 sales listing realtor section (#2702)

--- app/javascript/__tests__/modules/listingDetails/ListingDetailsAdditionalInformation.test.tsx
+++ app/javascript/__tests__/modules/listingDetails/ListingDetailsAdditionalInformation.test.tsx
@@ -46,4 +46,58 @@ describe("ListingDetailsAdditionalInformation", () => {
 
     expect(asFragment()).toMatchSnapshot()
   })
+
+  it("displays realtor commission section if Allows_Realtor_Commission is true for a sales listing", () => {
+    window.matchMedia = jest.fn().mockImplementation((query) => {
+      return {
+        matches: true,
+        media: query,
+        onchange: null,
+        addListener: jest.fn(),
+        removeListener: jest.fn(),
+        addEventListener: jest.fn(),
+        removeEventListener: jest.fn(),
+        dispatchEvent: jest.fn(),
+      }
+    })
+
+    const listing = {
+      ...openSaleListing,
+      Allows_Realtor_Commission: true,
+    }
+
+    const { asFragment } = render(
+      <ListingDetailsAdditionalInformation listing={listing} imageSrc={"/image-url"} />
+    )
+
+    expect(asFragment()).toMatchSnapshot()
+  })
+
+  it("displays realtor commission text if Realtor_Commission_Amount and Realtor_Commission_Unit are present for a sales listing", () => {
+    window.matchMedia = jest.fn().mockImplementation((query) => {
+      return {
+        matches: true,
+        media: query,
+        onchange: null,
+        addListener: jest.fn(),
+        removeListener: jest.fn(),
+        addEventListener: jest.fn(),
+        removeEventListener: jest.fn(),
+        dispatchEvent: jest.fn(),
+      }
+    })
+
+    const listing = {
+      ...openSaleListing,
+      Allows_Realtor_Commission: true,
+      Realtor_Commission_Amount: 3,
+      Realtor_Commission_Unit: "percent",
+    }
+
+    const { asFragment } = render(
+      <ListingDetailsAdditionalInformation listing={listing} imageSrc={"/image-url"} />
+    )
+
+    expect(asFragment()).toMatchSnapshot()
+  })
 })
