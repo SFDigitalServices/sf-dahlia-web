98fc04a8d7739be777cc671ff6474b394dbd68cc
Justin Caovan <justincaovan@gmail.com>
2025-09-25 11:44:12 -0700

    feat: DAH-3531 Rewrite Listings App Nav Bar in React (#2696)
    * feat: rewrite listings app nav bar in react
    
    * feat: matches navigation to section name instead of index
    
    * refactor: simplify navigation section util search
    
    * refactor: removes angular-style uppercase, adds media query
    
    * refactor: change border to seeds styling, media query font size
    
    * chore: remove skeleton debug header
    
    * refactor: pull in updated schema
    
    * fix: formatting for linter

--- app/javascript/pages/form/components/ListingApplyFormWrapper.tsx
+++ app/javascript/pages/form/components/ListingApplyFormWrapper.tsx
@@ -1,7 +1,6 @@
 import React from "react"
-import { t } from "@bloom-housing/ui-components"
+import { ProgressNav, t } from "@bloom-housing/ui-components"
 import { Card, Heading } from "@bloom-housing/ui-seeds"
-// import { CardSection } from "@bloom-housing/ui-seeds/src/blocks/Card"
 import { useFormEngineContext } from "../../../formEngine/formEngineContext"
 import "./ListingApplyFormWrapper.scss"
 
@@ -15,31 +14,36 @@ const ListingApplyFormWrapper = ({ children }: ListingApplyFormWrapperProps) => 
   const { listing, currentStepIndex, stepInfoMap, sectionNames } = formEngineContext
   const currentStepChild = children[currentStepIndex]
   const currentStepInfo = stepInfoMap[currentStepIndex]
+  const currentSectionIndex = sectionNames.indexOf(currentStepInfo.sectionName)
   const showLayout = !currentStepInfo.hideLayout
 
   return (
     <section className="bg-gray-300">
-      {showLayout ? (
-        <div className="md:mb-20 md:mt-12 mx-auto max-w-xl print:my-0 print:max-w-full">
-          <Card className="application-form-header">
-            <Card.Section className="application-form-header-title">
-              <Heading priority={1} className="application-form-header-heading">
-                {t("pageTitle.listingApplication", { listing: listing.Name })}
-              </Heading>
-            </Card.Section>
-            <Card.Section>
-              <p>currentSection: {currentStepInfo.sectionName}</p>
-              <p>sections: {JSON.stringify(sectionNames)}</p>
-            </Card.Section>
-          </Card>
-          <Card>
-            <Heading>debug current step slug: {JSON.stringify(currentStepInfo.slug)}</Heading>
-          </Card>
-          <Card>{currentStepChild}</Card>
-        </div>
-      ) : (
-        <>{currentStepChild}</>
-      )}
+      <div className="md:mb-20 md:mt-12 mx-auto max-w-lg print:my-0 print:max-w-full">
+        {showLayout ? (
+          <>
+            <Card className="application-form-header">
+              <Card.Section className="application-form-header-title">
+                <Heading priority={1} className="application-form-header-heading">
+                  {t("pageTitle.listingApplication", { listing: listing.Name })}
+                </Heading>
+              </Card.Section>
+              <Card.Section className="application-form-nav">
+                <ProgressNav
+                  labels={sectionNames}
+                  currentPageSection={currentSectionIndex + 1}
+                  completedSections={currentSectionIndex}
+                  mounted={true}
+                  removeSrHeader
+                />
+              </Card.Section>
+            </Card>
+            <Card>{currentStepChild}</Card>
+          </>
+        ) : (
+          <>{currentStepChild}</>
+        )}
+      </div>
     </section>
   )
 }
