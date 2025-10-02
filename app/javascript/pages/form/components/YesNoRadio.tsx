8e368e5798c1c68431bb6ee09650dc8ce2f1b9ac
cliu02 <32694266+cliu02@users.noreply.github.com>
2025-10-02 16:59:40 -0700

    feat: init radio component

--- app/javascript/pages/form/components/YesNoRadio.tsx
+++ app/javascript/pages/form/components/YesNoRadio.tsx
@@ -1,16 +1,44 @@
 import React from "react"
-import { t } from "@bloom-housing/ui-components"
+import { t, FieldGroup } from "@bloom-housing/ui-components"
 import { Card } from "@bloom-housing/ui-seeds"
+import { useFormStepContext } from "../../../formEngine/formStepContext"
 
 interface YesNoRadioProps {
-  label: string
+  label?: string
+  note?: string
+  description?: string
+  yesText?: string
+  fieldNames?: {
+    question?: string
+  }
 }
 
-const YesNoRadio = ({ label }: YesNoRadioProps) => {
+const YesNoRadio = ({ label, note, description, yesText, fieldNames }: YesNoRadioProps) => {
+  const { register, errors } = useFormStepContext()
   return (
     <Card>
-      <Card.Header>YesNoRadio Component</Card.Header>
       <Card.Section>{t(label)}</Card.Section>
+      <Card.Section>
+        <fieldset>
+          <FieldGroup
+            fieldGroupClassName="grid grid-cols-1"
+            fieldClassName="ml-0"
+            type="radio"
+            name={fieldNames?.question}
+            groupNote={t(note)}
+            error={errors?.[fieldNames?.question]}
+            errorMessage={t("errorMessage")}
+            register={register}
+            fields={[
+              { id: "yes", value: "true", label: t("t.yes") },
+              { id: "no", value: "false", label: t("t.no") },
+            ]}
+            validation={{
+              required: true,
+            }}
+          />
+        </fieldset>
+      </Card.Section>
     </Card>
   )
 }
