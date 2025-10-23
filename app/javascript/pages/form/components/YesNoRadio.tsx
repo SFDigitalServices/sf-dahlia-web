b274fe1d798ae9aebc96562e47ed9272c31d59e3
cliu02 <32694266+cliu02@users.noreply.github.com>
2025-10-23 13:26:36 -0700

    fix: update label

--- app/javascript/pages/form/components/YesNoRadio.tsx
+++ app/javascript/pages/form/components/YesNoRadio.tsx
@@ -6,41 +6,35 @@ import "./YesNoRadio.scss"
 interface YesNoRadioProps {
   label?: string
   note?: string
-  description?: string
   yesText?: string
   fieldNames?: {
     question?: string
   }
 }
 
-const YesNoRadio = ({ label, note, description, yesText, fieldNames }: YesNoRadioProps) => {
+const YesNoRadio = ({ label, note, yesText, fieldNames }: YesNoRadioProps) => {
   const { register, errors, watch } = useFormStepContext()
   const selected = watch(fieldNames?.question)
   return (
-    <>
-      {t(label)}
-      <fieldset>
-        <FieldGroup
-          fieldGroupClassName="radio-field-group"
-          fieldClassName="radio-field"
-          type="radio"
-          name={fieldNames?.question}
-          groupLabel={t(description)}
-          groupNote={t(note)}
-          {...(selected === "true" && { groupSubNote: t(yesText) })}
-          error={errors?.[fieldNames?.question]}
-          errorMessage={t("error.pleaseSelectAnOption")}
-          register={register}
-          fields={[
-            { id: "yes", value: "true", label: t("t.yes") },
-            { id: "no", value: "false", label: t("t.no") },
-          ]}
-          validation={{
-            required: true,
-          }}
-        />
-      </fieldset>
-    </>
+    <FieldGroup
+      fieldGroupClassName="radio-field-group"
+      fieldClassName="radio-field"
+      type="radio"
+      name={fieldNames?.question}
+      groupLabel={t(label)}
+      groupNote={t(note)}
+      {...(selected === "true" && { groupSubNote: t(yesText) })}
+      error={errors?.[fieldNames?.question]}
+      errorMessage={t("error.pleaseSelectAnOption")}
+      register={register}
+      fields={[
+        { id: "yes", value: "true", label: t("t.yes") },
+        { id: "no", value: "false", label: t("t.no") },
+      ]}
+      validation={{
+        required: true,
+      }}
+    />
   )
 }
 
