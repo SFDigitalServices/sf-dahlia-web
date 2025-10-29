0b0c9e119f263d207646613388ca97e882824f2c
cliu02 <32694266+cliu02@users.noreply.github.com>
2025-10-29 09:51:43 -0700

    test: refactor util

--- app/javascript/__tests__/components/Phone.test.tsx
+++ app/javascript/__tests__/components/Phone.test.tsx
@@ -1,74 +1,31 @@
 import React from "react"
-import { useForm } from "react-hook-form"
-import { render, screen } from "@testing-library/react"
-import { userEvent } from "@testing-library/user-event"
+import { screen } from "@testing-library/react"
 import { t } from "@bloom-housing/ui-components"
 import Phone from "../../pages/form/components/Phone"
-import { FormStepProvider } from "../../formEngine/formStepContext"
-import { FormEngineProvider } from "../../formEngine/formEngineContext"
-import { openRentalListing } from "../data/RailsRentalListing/listing-rental-open"
+import { formContextWrapper } from "../__util__/renderUtils"
+import userEvent from "@testing-library/user-event"
 
-const FieldSetWrapper = () => {
-  // eslint-disable-next-line @typescript-eslint/unbound-method
-  const { register, watch, trigger, errors, setValue, clearErrors, control } = useForm({
-    mode: "onBlur",
-    reValidateMode: "onBlur",
-    shouldFocusError: false,
-  })
-  const formStepContextValue = { register, errors, watch, trigger, setValue, clearErrors, control }
-
-  const listing = openRentalListing
-  const formData = {}
-  const formEngineContextValue = {
-    listing,
-    formData,
-    saveFormData: jest.fn(),
-    dataSources: {
-      listing,
-      form: formData,
-      preferences: {},
-    },
-    stepInfoMap: [
-      {
-        slug: "test",
-        fieldNames: Object.values({
-          phone: "primaryApplicantPhone",
-          phoneType: "primaryApplicantPhoneType",
-          additionalPhone: "primaryApplicantAdditionalPhone",
-          additionalPhoneType: "primaryApplicantAdditionalPhoneType",
-        }),
-      },
-    ],
-    sectionNames: [],
-    currentStepIndex: 0,
-    handleNextStep: jest.fn(),
-    handlePrevStep: jest.fn(),
-  }
-
-  return (
-    <FormEngineProvider value={formEngineContextValue}>
-      <FormStepProvider value={formStepContextValue}>
-        <Phone
-          label="label.applicantPhone"
-          showTypeOfNumber
-          showDontHavePhoneNumber
-          showAdditionalPhoneNumber
-          labelForAdditionalPhoneNumber="label.applicantSecondPhone"
-          fieldNames={{
-            phone: "primaryApplicantPhone",
-            phoneType: "primaryApplicantPhoneType",
-            additionalPhone: "primaryApplicantAdditionalPhone",
-            additionalPhoneType: "primaryApplicantAdditionalPhoneType",
-          }}
-        />
-      </FormStepProvider>
-    </FormEngineProvider>
+const renderPhone = () => {
+  return formContextWrapper(
+    <Phone
+      label="label.applicantPhone"
+      showTypeOfNumber
+      showDontHavePhoneNumber
+      showAdditionalPhoneNumber
+      labelForAdditionalPhoneNumber="label.applicantSecondPhone"
+      fieldNames={{
+        phone: "primaryApplicantPhone",
+        phoneType: "primaryApplicantPhoneType",
+        additionalPhone: "primaryApplicantAdditionalPhone",
+        additionalPhoneType: "primaryApplicantAdditionalPhoneType",
+      }}
+    />
   )
 }
 
 describe("Phone", () => {
   it("displays the two field inputs and two checkboxes", () => {
-    render(<FieldSetWrapper />)
+    renderPhone()
     expect(screen.getByText(t("label.applicantPhone"))).toBeInTheDocument()
     expect(screen.getByText(t("label.whatTypeOfNumber"))).toBeInTheDocument()
     expect(screen.getByText(t("label.applicantNoPhone"))).toBeInTheDocument()
@@ -76,7 +33,7 @@ describe("Phone", () => {
   })
 
   it("disables the inputs if the no phone checkbox is checked", async () => {
-    render(<FieldSetWrapper />)
+    renderPhone()
     const noPhoneCheckbox = screen.getByRole("checkbox", { name: t("label.applicantNoPhone") })
     const user = userEvent.setup()
     await user.click(noPhoneCheckbox)
@@ -85,7 +42,7 @@ describe("Phone", () => {
   })
 
   it("displays the additional input if the additional phone checkbox is checked", async () => {
-    render(<FieldSetWrapper />)
+    renderPhone()
     const additionalPhoneCheckbox = screen.getByRole("checkbox", {
       name: t("label.applicantAdditionalPhone"),
     })
@@ -95,7 +52,7 @@ describe("Phone", () => {
   })
 
   it("disables the no phone checkbox if the additional phone checkbox is checked", async () => {
-    render(<FieldSetWrapper />)
+    renderPhone()
     const additionalPhoneCheckbox = screen.getByRole("checkbox", {
       name: t("label.applicantAdditionalPhone"),
     })
--- app/javascript/__tests__/pages/form/components/DateOfBirth.test.tsx
+++ app/javascript/__tests__/pages/form/components/DateOfBirth.test.tsx
@@ -1,64 +1,26 @@
 import React from "react"
-import { useForm } from "react-hook-form"
-import { render, screen } from "@testing-library/react"
-import { userEvent } from "@testing-library/user-event"
+import { screen } from "@testing-library/react"
 import { t } from "@bloom-housing/ui-components"
 import MockDate from "mockdate"
 import DateOfBirth from "../../../../pages/form/components/DateOfBirth"
-import { FormStepProvider } from "../../../../formEngine/formStepContext"
-import { FormEngineProvider } from "../../../../formEngine/formEngineContext"
-import { openRentalListing } from "../../../data/RailsRentalListing/listing-rental-open"
+import { formContextWrapper } from "../../../__util__/renderUtils"
+import userEvent from "@testing-library/user-event"
 
 const INVALID_DATE_ERROR_MSG = "error.dob"
 const INVALID_AGE_ERROR_MSG = "error.dobPrimaryApplicantAge"
 
-const FieldSetWrapper = () => {
-  const label = "label.yourDob"
-  const minimumAge = 18
-  const fieldNames = {
-    birthMonth: "primaryApplicantBirthMonth",
-    birthDay: "primaryApplicantBirthDate",
-    birthYear: "primaryApplicantBirthYear",
-  }
-  const fieldNameValues = Object.values(fieldNames)
-
-  // eslint-disable-next-line @typescript-eslint/unbound-method
-  const { register, watch, trigger, errors, setValue, clearErrors, control } = useForm({
-    mode: "onBlur",
-    reValidateMode: "onBlur",
-    shouldFocusError: false,
-  })
-  const formStepContextValue = { register, errors, watch, trigger, setValue, clearErrors, control }
-
-  const listing = openRentalListing
-  const formData = Object.assign({}, ...fieldNameValues.map((name) => ({ [name]: null })))
-  const formEngineContextValue = {
-    listing,
-    formData,
-    saveFormData: jest.fn(),
-    dataSources: {
-      listing,
-      form: formData,
-      preferences: {},
-    },
-    stepInfoMap: [{ slug: "test", fieldNames: fieldNameValues }],
-    sectionNames: [],
-    currentStepIndex: 0,
-    handleNextStep: jest.fn(),
-    handlePrevStep: jest.fn(),
-  }
-
-  return (
-    <FormEngineProvider value={formEngineContextValue}>
-      <FormStepProvider value={formStepContextValue}>
-        <DateOfBirth
-          label={label}
-          ageErrorMessage="error.dobPrimaryApplicantAge"
-          fieldNames={fieldNames}
-          minimumAge={minimumAge}
-        />
-      </FormStepProvider>
-    </FormEngineProvider>
+const renderDateOfBirth = () => {
+  return formContextWrapper(
+    <DateOfBirth
+      label="label.yourDob"
+      ageErrorMessage="error.dobPrimaryApplicantAge"
+      fieldNames={{
+        birthMonth: "primaryApplicantBirthMonth",
+        birthDay: "primaryApplicantBirthDate",
+        birthYear: "primaryApplicantBirthYear",
+      }}
+      minimumAge={18}
+    />
   )
 }
 
@@ -74,23 +36,20 @@ describe("DateOfBirth", () => {
   beforeEach(() => {
     MockDate.set("2020-01-01")
   })
-
   it("renders without errors", () => {
-    render(<FieldSetWrapper />)
+    renderDateOfBirth()
   })
 
   it("renders invalid date error for invalid dates", async () => {
-    render(<FieldSetWrapper />)
+    renderDateOfBirth()
     await inputDate("13", "1", "2000")
-
     expect(screen.queryByText(t(INVALID_DATE_ERROR_MSG))).not.toBeNull()
     expect(screen.queryByText(t(INVALID_AGE_ERROR_MSG))).toBeNull()
   })
 
   it("renders invalid age error for invalid ages", async () => {
-    render(<FieldSetWrapper />)
+    renderDateOfBirth()
     await inputDate("1", "1", "2010")
-
     expect(screen.queryByText(t(INVALID_DATE_ERROR_MSG))).toBeNull()
     expect(screen.queryByText(t(INVALID_AGE_ERROR_MSG))).not.toBeNull()
   })
