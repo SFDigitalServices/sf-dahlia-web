04d091d9f4125310715fda16edbe90a7d8de7f05
cliu02 <32694266+cliu02@users.noreply.github.com>
2025-11-04 11:40:00 -0800

    fix: lint

--- app/javascript/pages/inviteToApply/invite-to-apply.tsx
+++ app/javascript/pages/inviteToApply/invite-to-apply.tsx
@@ -3,7 +3,6 @@ import withAppSetup from "../../layouts/withAppSetup"
 import { t, Icon } from "@bloom-housing/ui-components"
 import { Card, Button, Heading, Message } from "@bloom-housing/ui-seeds"
 import FormLayout from "../../layouts/FormLayout"
-import Link from "../../navigation/Link"
 import { AppPages } from "../../util/routeUtil"
 import { getListing } from "../../api/listingApiService"
 import { RailsListing } from "../../modules/listings/SharedHelpers"
@@ -64,31 +63,6 @@ const InviteToApplyHeader = ({ listing }: { listing: RailsListing }) => (
   </Card>
 )
 
-const InviteToApplyInterested = ({ listing }: { listing: RailsListing }) => (
-  <div className="mt-4 bg-white rounded-lg border border-solid">
-    <div className="pt-8 pb-8 text-center border-b border-solid">
-      <div className="text-2xl">Thank you for your response</div>
-      <div className="mt-4 text-sm">
-        You answered: <span className="font-bold">Yes, I'm still interested</span>
-      </div>
-    </div>
-    <div className="p-8 bg-blue-100">
-      <span className="font-bold">What to expect</span>
-      <ul className="p-4 space-y-4 list-disc">
-        <li>
-          The leasing agent will contact you when it's your turn to move forward with your
-          application.
-        </li>
-        <li>We will send you an email to let you know once all units get leased.</li>
-        <li>We will contact you again if more units become available in the next 12 months.</li>
-      </ul>
-      <Link external href="https://www.sf.gov/after-rental-housing-lottery" target="_blank">
-        Learn more about what happens after the housing lottery.
-      </Link>
-    </div>
-  </div>
-)
-
 const InviteToApplyPage = (_props: HomePageProps) => {
   const [listing, setListing] = useState<RailsListing>(null)
 
@@ -113,7 +87,6 @@ const InviteToApplyPage = (_props: HomePageProps) => {
       {_props.urlParams.response && isDeadlinePassed(_props.urlParams.deadline) && (
         <DeadlinePassedBanner deadline={_props.urlParams.deadline} />
       )}
-      {_props.urlParams.response === "yes" && <InviteToApplyInterested listing={listing} />}
       {_props.urlParams.response === "no" && (
         <InviteToApplyWithdrawn
           listingName={listing?.Name}
