3ed6d00da526491fe1834712cc516233ee783411
cliu02 <32694266+cliu02@users.noreply.github.com>
2025-11-03 16:33:09 -0800

    feat: DAH-3544 Forms email address component (#2716)
    * fix: schema nesting bug
    
    * feat: email address
    
    * fix: update label and subNote
    
    * fix: add no email to schema
    
    * fix: error handling
    
    * fix: override note positioning
    
    * test: add unit tests
    
    * test: update existing test
    
    * test: add error tests
    
    * test: linter
    
    * test: linter
    
    * test: add eslint line

--- app/javascript/pages/form/components/EmailAddress.tsx
+++ app/javascript/pages/form/components/EmailAddress.tsx
@@ -1,18 +1,65 @@
 import React from "react"
-import { t } from "@bloom-housing/ui-components"
-import { Card } from "@bloom-housing/ui-seeds"
+import { t, Field } from "@bloom-housing/ui-components"
+import { useFormStepContext } from "../../../formEngine/formStepContext"
+import { EMAIL_REGEX, LISTING_APPLY_FORMS_INPUT_MAX_LENGTH } from "../../../modules/constants"
+import "./EmailAddress.scss"
 
 interface EmailAddressProps {
   label: string
+  fieldNames: {
+    email: string
+    noEmail?: string
+  }
+  showDontHaveEmailAddress?: boolean
+  note?: string
 }
 
-const Name = ({ label }: EmailAddressProps) => {
+const EmailAddress = ({
+  label,
+  fieldNames: { email, noEmail },
+  showDontHaveEmailAddress,
+  note,
+}: EmailAddressProps) => {
+  const { register, errors, setValue, clearErrors } = useFormStepContext()
+  const [noEmailCheckbox, setNoEmailCheckbox] = React.useState(false)
   return (
-    <Card>
-      <Card.Header>EamilAddress Component</Card.Header>
-      <Card.Section>{t(label)}</Card.Section>
-    </Card>
+    <fieldset>
+      <Field
+        name={email}
+        label={t(label)}
+        register={register}
+        disabled={noEmailCheckbox}
+        subNote={t(note)}
+        className="email-address-field"
+        validation={{
+          required: !noEmailCheckbox,
+          maxLength: LISTING_APPLY_FORMS_INPUT_MAX_LENGTH.email,
+          pattern: {
+            value: EMAIL_REGEX,
+            message: t("error.email"),
+          },
+        }}
+        error={!!errors?.[email]}
+        errorMessage={t("error.email")}
+      />
+      {showDontHaveEmailAddress && (
+        <Field
+          type="checkbox"
+          name={noEmail}
+          label={t("label.applicantNoEmail")}
+          register={register}
+          onChange={(e) => {
+            const isChecked = e.target.checked
+            setNoEmailCheckbox(isChecked)
+            if (isChecked) {
+              setValue(email, "")
+              clearErrors(email)
+            }
+          }}
+        />
+      )}
+    </fieldset>
   )
 }
 
-export default Name
+export default EmailAddress
