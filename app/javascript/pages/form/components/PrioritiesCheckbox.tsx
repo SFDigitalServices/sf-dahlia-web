342ef3493874e7e1197313ccf26b09adb3d5babb
Justin Caovan <justincaovan@gmail.com>
2025-11-07 17:23:39 -0700

    feat: priority checkbox group and mutual exclusive logic

--- app/javascript/pages/form/components/PrioritiesCheckbox.tsx
+++ app/javascript/pages/form/components/PrioritiesCheckbox.tsx
@@ -1,17 +1,80 @@
-import React from "react"
-import { t } from "@bloom-housing/ui-components"
-import { Card } from "@bloom-housing/ui-seeds"
+import { FieldGroup, t } from "@bloom-housing/ui-components"
+import React, { useEffect, useRef } from "react"
+import { useFormStepContext } from "../../../formEngine/formStepContext"
 
 interface PrioritiesCheckboxProps {
-  label: string
+  description: string
+  members: {
+    dataSource: string
+    dataKey: string
+  }
+  fieldNames: {
+    priorityMembers: string
+  }
 }
 
-const PrioritiesCheckbox = ({ label }: PrioritiesCheckboxProps) => {
+const PrioritiesCheckbox = ({
+  description,
+  fieldNames: { priorityMembers },
+}: PrioritiesCheckboxProps) => {
+  const { register, errors, watch, setValue } = useFormStepContext()
+  const selectedCheckboxValues = watch(priorityMembers)
+  const previousValuesRef = useRef(selectedCheckboxValues || [])
+
+  useEffect(() => {
+    if (!selectedCheckboxValues) return
+    const newCheckboxValue = selectedCheckboxValues.filter(
+      (value) => !previousValuesRef.current.includes(value)
+    )
+
+    if (newCheckboxValue.includes("noImpairments")) {
+      setValue(priorityMembers, ["noImpairments"])
+    } else if (selectedCheckboxValues.includes("noImpairments") && newCheckboxValue.length > 0) {
+      setValue(
+        priorityMembers,
+        selectedCheckboxValues.filter((value) => value !== "noImpairments")
+      )
+    }
+
+    previousValuesRef.current = selectedCheckboxValues
+  }, [selectedCheckboxValues, setValue, priorityMembers])
+
   return (
-    <Card>
-      <Card.Header>MonthlyRent Component</Card.Header>
-      <Card.Section>{t(label)}</Card.Section>
-    </Card>
+    <FieldGroup
+      name={priorityMembers}
+      type="checkbox"
+      groupLabel={t(description)}
+      fields={[
+        {
+          label: t("label.mobilityImpairments"),
+          value: "mobilityImpairment",
+          id: "mobilityImpairment",
+        },
+        {
+          label: t("label.visionImpairments"),
+          value: "visionImpairment",
+          id: "visionImpairment",
+        },
+        {
+          label: t("label.hearingImpairments"),
+          value: "hearingImpairment",
+          id: "hearingImpairment",
+        },
+        {
+          label: t("t.no"),
+          value: "noImpairments",
+          id: "noImpairments",
+        },
+      ]}
+      fieldGroupClassName="radio-field-group"
+      fieldClassName="radio-field"
+      register={register}
+      error={!!errors?.[priorityMembers]}
+      errorMessage={t("error.pleaseSelectAnOption")}
+      validation={{
+        required: true,
+      }}
+    />
   )
 }
 
