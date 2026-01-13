b4f5f2d8a1d7da875ca4fc9d167424b03abe7ffd
cliu02 <32694266+cliu02@users.noreply.github.com>
2026-01-13 11:42:01 -0800

    fix: list styling

--- app/javascript/pages/inviteToApply/InviteToApplySubmitYourInfo.tsx
+++ app/javascript/pages/inviteToApply/InviteToApplySubmitYourInfo.tsx
@@ -211,13 +211,9 @@ const WhatHappensNext = () => {
       </Heading>
       <p>{t("inviteToApplyPage.submitYourInfo.whatHappensNext.p2")}</p>
       <p>{t("inviteToApplyPage.submitYourInfo.whatHappensNext.p3")}</p>
-      <ul>
-        <li className={styles.submitYourInfoList}>
-          {t("inviteToApplyPage.submitYourInfo.whatHappensNext.p4")}
-        </li>
-        <li className={styles.submitYourInfoList}>
-          {t("inviteToApplyPage.submitYourInfo.whatHappensNext.p5")}
-        </li>
+      <ul className={styles.submitYourInfoList}>
+        <li>{t("inviteToApplyPage.submitYourInfo.whatHappensNext.p4")}</li>
+        <li>{t("inviteToApplyPage.submitYourInfo.whatHappensNext.p5")}</li>
       </ul>
       <p>{t("inviteToApplyPage.submitYourInfo.whatHappensNext.p6")}</p>
       <Heading priority={3} size="lg">
