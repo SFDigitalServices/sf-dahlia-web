e981e8ed09aca8e78780cfc06c98235f8cbbb8f3
cliu02 <32694266+cliu02@users.noreply.github.com>
2026-02-04 14:14:36 -0800

    test: add tests

--- app/javascript/__tests__/util/formEngineUtil.test.ts
+++ app/javascript/__tests__/util/formEngineUtil.test.ts
@@ -37,6 +37,7 @@ describe("formEngineUtil", () => {
       preferences: {
         testKey1: "test key 1",
         testKey2: "test key 2",
+        testKey3: "test key 3",
       },
     }
     const conditions = [
@@ -48,6 +49,14 @@ describe("formEngineUtil", () => {
         dataSource: "preferences",
         dataKey: "testKey2",
         negate: true,
+      },
+    ]
+
+    const valueCondition = [
+      {
+        dataSource: "preferences",
+        dataKey: "testKey3",
+        dataValueToMatch: "dataValue3",
       },
     ]
 
@@ -69,6 +78,30 @@ describe("formEngineUtil", () => {
     it("returns false for operation 'hideStepIfAnyPresent'", () => {
       const operation = "hideStepIfAnyPresent"
       expect(showStep(operation, conditions, dataSources)).toBe(false)
+    })
+
+    it("returns false for operation 'hideStepIfAnyPresent' when dataValueToMatch matches", () => {
+      expect(
+        showStep("hideStepIfAnyPresent", valueCondition, {
+          ...dataSources,
+          preferences: {
+            ...dataSources.preferences,
+            testKey3: "dataValue3",
+          },
+        })
+      ).toBe(false)
+    })
+
+    it("returns true for operation 'hideStepIfAnyPresent' when dataValueToMatch doesn't match", () => {
+      expect(
+        showStep("hideStepIfAnyPresent", valueCondition, {
+          ...dataSources,
+          preferences: {
+            ...dataSources.preferences,
+            testKey3: "notDataValue3",
+          },
+        })
+      ).toBe(true)
     })
   })
 
