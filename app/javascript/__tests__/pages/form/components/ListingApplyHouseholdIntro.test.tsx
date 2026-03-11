9bc80c3e2e11bc0b82c05ed1639b7207cf32b05b
cliu02 <32694266+cliu02@users.noreply.github.com>
2026-03-10 17:11:43 -0700

    test: unit tests

--- app/javascript/__tests__/pages/form/components/ListingApplyHouseholdIntro.test.tsx
+++ app/javascript/__tests__/pages/form/components/ListingApplyHouseholdIntro.test.tsx
@@ -40,7 +40,10 @@ describe("ListingApplyHouseholdIntro", () => {
   })
   it("skips to the next section if alone", async () => {
     await user.click(screen.getByText(t("label.liveAlone")))
-    expect(formEngineContextValue.handleNextStep).toHaveBeenCalledWith({ liveAlone: "true" })
+    expect(formEngineContextValue.handleNextStep).toHaveBeenCalledWith({
+      "household-member-form": [],
+      liveAlone: "true",
+    })
   })
   it("goes to the next page if there are household members", async () => {
     await user.click(screen.getByText(t("label.otherPeople")))
