ff67e3d96d027266d948866994a20bba3cfa6f09
cliu02 <32694266+cliu02@users.noreply.github.com>
2026-03-19 08:59:46 -0700

    test: add test

--- app/javascript/__tests__/util/formEngineUtil.test.ts
+++ app/javascript/__tests__/util/formEngineUtil.test.ts
@@ -31,6 +31,24 @@ describe("formEngineUtil", () => {
         "for Jane"
       )
     })
+
+    it("supports translations with an alternate household title", () => {
+      const translationKey = "label.forUser"
+      const translationVars = { user: { dataSource: "form", dataKey: "userName" } }
+      const dataSources = {
+        form: { userName: "Jane", liveAlone: "false" },
+        listing: openRentalListing,
+        preferenceNames: {},
+      }
+      expect(
+        translationFromDataSchema(
+          translationKey,
+          translationVars,
+          dataSources,
+          "label.householdDetails"
+        )
+      ).toBe("Household Details")
+    })
   })
 
   describe("showStep", () => {
