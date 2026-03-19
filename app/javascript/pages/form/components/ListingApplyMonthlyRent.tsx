cad29bae4b6a4b65b2191746217c74852b525faf
cliu02 <32694266+cliu02@users.noreply.github.com>
2026-03-19 08:42:24 -0700

    feat: init fields

--- app/javascript/pages/form/components/ListingApplyMonthlyRent.tsx
+++ app/javascript/pages/form/components/ListingApplyMonthlyRent.tsx
@@ -1,20 +1,16 @@
-import { t } from "@bloom-housing/ui-components"
+import React from "react"
+import { t, Field } from "@bloom-housing/ui-components"
 import { Button, Heading, Card } from "@bloom-housing/ui-seeds"
 import { useFormEngineContext } from "../../../formEngine/formEngineContext"
+import { useForm, FormProvider } from "react-hook-form"
 import stepStyles from "./ListingApplyStepWrapper.module.scss"
-import {
-  getAddress,
-  getFullName,
-  getPrimaryApplicantData,
-  translationFromDataSchema,
-} from "../../../util/formEngineUtil"
-import React from "react"
+import { getAddress, translationFromDataSchema } from "../../../util/formEngineUtil"
 
 type GroupedAddress = {
   address: string
   monthlyRent: string
-  doesNotPayRent: boolean
-  members: Array<{ name: string }>
+  doesNotPayRent: string
+  members: string[]
 }
 
 type ListingApplyMonthlyRentProps = {
@@ -26,13 +22,28 @@ type ListingApplyMonthlyRentProps = {
   }
 }
 
-const RentFieldGroup = ({ groupedAddress }: { groupedAddress: GroupedAddress }) => {
+const RentField = ({ groupedAddress }: { groupedAddress: GroupedAddress }) => {
+  const getLabel = () => {
+    if (groupedAddress.members.length === 0) {
+      return t("c5HouseholdMonthlyRent.titleYou")
+    }
+    return groupedAddress.members.length === 1
+      ? t("c5HouseholdMonthlyRent.howMuchDoYouPay", { address: groupedAddress.address })
+      : t("c5HouseholdMonthlyRent.howMuchDoYouAndMembersPay", {
+          address: groupedAddress.address,
+          members: groupedAddress.members.join(", "),
+        })
+  }
+
   return (
     <>
-      <p>{groupedAddress.address}</p>
-      <p>{groupedAddress.monthlyRent}</p>
-      <p>{groupedAddress.doesNotPayRent}</p>
-      <p>{groupedAddress.members.map((member) => member.name).join(", ")}</p>
+      <legend className="legend-header">{getLabel()}</legend>
+      <Field name={groupedAddress.monthlyRent} errorMessage={t("error.pleasePutInARent")} />
+      <Field
+        type="checkbox"
+        name={groupedAddress.address + "_checkbox"}
+        label={t("label.noRentPaid")}
+      />
     </>
   )
 }
@@ -44,6 +55,10 @@ const ListingApplyMonthlyRent = ({
   householdTitle,
 }: ListingApplyMonthlyRentProps) => {
   const { formData, saveFormData, handleNextStep, handlePrevStep } = useFormEngineContext()
+  const methods = useForm({
+    mode: "onChange",
+    shouldFocusError: false,
+  })
   const groupedAddressesArray: GroupedAddress[] = []
   const primaryAddress = getAddress(
     formData.primaryApplicantAddressStreet as string,
@@ -56,48 +71,52 @@ const ListingApplyMonthlyRent = ({
   const addresses: GroupedAddress[] = [
     {
       address: primaryAddress,
-      monthlyRent: null,
-      doesNotPayRent: null,
-      members: [
-        {
-          name: getFullName(getPrimaryApplicantData(formData)),
-        },
-      ],
+      monthlyRent: "0",
+      doesNotPayRent: "false",
+      members: [],
     },
   ]
+
   return (
-    <>
-      <Card.Section>
-        <Button variant="text" onClick={handlePrevStep}>
-          {t("t.back")}
-        </Button>
-      </Card.Section>
-      <Card.Header divider="inset">
-        <Heading className={stepStyles["step-title"]} priority={1} size="2xl">
-          {translationFromDataSchema(title, {}, useFormEngineContext().dataSources, householdTitle)}
-        </Heading>
-        {description && <p className="field-note text-base">{t(description)}</p>}
-      </Card.Header>
-      <Card.Section>
-        {addresses.map((groupedAddress: GroupedAddress, index: number) => (
-          <RentFieldGroup key={index} groupedAddress={groupedAddress} />
-        ))}
-      </Card.Section>
-      <Card.Footer className={stepStyles["step-footer"]}>
-        <Button
-          variant="primary"
-          onClick={() => {
-            saveFormData({ [fieldNames.groupedAddresses]: groupedAddressesArray })
-            handleNextStep({
-              ...formData,
-              [fieldNames.groupedAddresses]: groupedAddressesArray,
-            })
-          }}
-        >
-          {t("t.next")}
-        </Button>
-      </Card.Footer>
-    </>
+    <FormProvider {...methods}>
+      <Card>
+        <Card.Section>
+          <Button variant="text" onClick={handlePrevStep}>
+            {t("t.back")}
+          </Button>
+        </Card.Section>
+        <Card.Header divider="inset">
+          <Heading className={stepStyles["step-title"]} priority={1} size="2xl">
+            {translationFromDataSchema(
+              title,
+              {},
+              useFormEngineContext().dataSources,
+              householdTitle
+            )}
+          </Heading>
+          {description && <p className="field-note text-base">{t(description)}</p>}
+        </Card.Header>
+        <Card.Section>
+          {addresses.map((groupedAddress: GroupedAddress, index: number) => (
+            <RentField key={index} groupedAddress={groupedAddress} />
+          ))}
+        </Card.Section>
+        <Card.Footer className={stepStyles["step-footer"]}>
+          <Button
+            variant="primary"
+            onClick={() => {
+              saveFormData({ [fieldNames.groupedAddresses]: groupedAddressesArray })
+              handleNextStep({
+                ...formData,
+                [fieldNames.groupedAddresses]: groupedAddressesArray,
+              })
+            }}
+          >
+            {t("t.next")}
+          </Button>
+        </Card.Footer>
+      </Card>
+    </FormProvider>
   )
 }
 
