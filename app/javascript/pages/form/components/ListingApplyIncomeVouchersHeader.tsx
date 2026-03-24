8d66d49e6abf586f235ce1150a48a83710a0e48e
Jim Lin <jim.lin@sfgov.org>
2026-03-24 11:26:07 -0700

    feat: income vouchers

--- app/javascript/pages/form/components/ListingApplyIncomeVouchersHeader.tsx
+++ app/javascript/pages/form/components/ListingApplyIncomeVouchersHeader.tsx
@@ -1,15 +1,26 @@
 import React from "react"
 import { t } from "@bloom-housing/ui-components"
 import { CardHeader } from "@bloom-housing/ui-seeds/src/blocks/Card"
+import { useFormEngineContext } from "../../../formEngine/formEngineContext"
 import listingApplyStepWrapperStyles from "./ListingApplyStepWrapper.module.scss"
+import { renderInlineMarkup } from "../../../util/languageUtil"
+import styles from "./ListingApplyIncomeVouchersHeader.module.scss"
 
 const ListingApplyIncomeVouchersHeader = () => {
-  const titleString = t("d1IncomeVouchers.titleHousehold")
+  const formEngineContext = useFormEngineContext()
+  const { formData } = formEngineContext
+
+  const titleString =
+    formData.liveAlone === "false"
+      ? t("d1IncomeVouchers.titleHousehold")
+      : t("d1IncomeVouchers.titleYou")
 
   return (
     <CardHeader divider="inset">
       <h1 className={listingApplyStepWrapperStyles["step-title"]}>{titleString}</h1>
-      <p className="field-note text-base">Income Vouchers Description</p>
+      <p className={styles["step-description"]}>{renderInlineMarkup(t("d1IncomeVouchers.p1"))}</p>
+      <p className={styles["step-description"]}>{renderInlineMarkup(t("d1IncomeVouchers.p2"))}</p>
+      <p className={styles["step-description"]}>{renderInlineMarkup(t("d1IncomeVouchers.p3"))}</p>
     </CardHeader>
   )
 }
