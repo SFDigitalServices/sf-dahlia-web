8a207b0781507fbeb8b770a2c9173b90fd7921a9
John Dunning <fw@johndunning.com>
2026-03-28 17:45:52 -0700

    feat: add I2I withdrawn, waitlist, and deadline passed pages
    - Create InviteToInterviewWithdrawn with same card layout as I2A,
      body text references appointment instead of document submission
    - Create InviteToInterviewWaitlist (new page type) with waitlist
      explanation, listing link, and change-answer section
    - Create InviteToInterviewDeadlinePassed with leasing agent contact
    - Wire all response pages into invite-to-interview.tsx entry point
      with JWT handling mirroring the I2A pattern
    - Update controller with JWT decode/encode and response routing
    - Add 13 new translation strings for response pages

--- app/controllers/invite_to_interview_page_controller.rb
+++ app/controllers/invite_to_interview_page_controller.rb
@@ -1,14 +1,14 @@
 # Controller for the page shown when applicants are invited to interview (100% affordable)
 class InviteToInterviewPageController < ApplicationController
   def index
-    @invite_to_interview_props = {
-      assetPaths: static_asset_paths,
-      urlParams: {
-        deadline: params['deadline'],
-        response: params['response'],
-        applicationNumber: params['applicationNumber'],
-      },
-    }
+    decoded_params = use_jwt? && decode_token(params[:t])
+    if decoded_params.is_a?(String)
+      redirect_to decoded_params
+      return
+    end
+
+    decoded_params ||= params
+    @invite_to_interview_props = props(decoded_params)
     render 'invite_to_interview'
   end
 
@@ -21,6 +21,54 @@ class InviteToInterviewPageController < ApplicationController
   end
 
   private
+
+  def props(decoded_params = params)
+    url_params = {
+      deadline: decoded_params['deadline'],
+      response: decoded_params['response'],
+      applicationNumber: decoded_params['applicationNumber'],
+    }
+
+    {
+      assetPaths: static_asset_paths,
+      urlParams: url_params,
+      submitPreviewLinkTokenParam: encode_token(url_params.except(:response)),
+    }.compact
+  end
+
+  def decode_token(token)
+    if token.blank?
+      return url_for(
+        controller: 'listing', id: params[:id], lang: params[:lang],
+      )
+    end
+
+    decoded_token = JWT.decode(
+      token,
+      ENV.fetch('JWT_TOKEN_SECRET', nil),
+      true,
+      { algorithm: ENV.fetch('JWT_ALGORITHM', nil), verify_expiration: false },
+    )
+    decoded_token.first['data']
+  rescue JWT::VerificationError
+    root_url
+  end
+
+  def encode_token(params)
+    return nil unless use_jwt?
+
+    JWT.encode(
+      { data: params },
+      ENV.fetch('JWT_TOKEN_SECRET', nil),
+      ENV.fetch('JWT_ALGORITHM', nil),
+    )
+  end
+
+  def use_jwt?
+    Rails.configuration.unleash.is_enabled?('temp.webapp.inviteToApply.JwtLinkParams') &&
+      ENV.fetch('JWT_TOKEN_SECRET', nil) &&
+      ENV.fetch('JWT_ALGORITHM', nil)
+  end
 
   def use_react_app
     true
