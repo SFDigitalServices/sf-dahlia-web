63fb713ce7fc1978d89fdd118f84cbff945b80f1
John Dunning <fw@johndunning.com>
2026-03-31 12:23:25 -0700

    fix: make I2I print button natural width instead of full width

--- app/javascript/pages/inviteToApply/InviteToInterviewSubmitYourInfo.tsx
+++ app/javascript/pages/inviteToApply/InviteToInterviewSubmitYourInfo.tsx
@@ -247,6 +247,7 @@ const InviteToInterviewSubmitYourInfo = ({
               variant="primary-outlined"
               onClick={() => window.print()}
               className={styles.actionButton}
+              style={{ width: "fit-content" }}
             >
               {t("inviteToInterviewPage.submitYourInfo.printThisPage")}
             </Button>
