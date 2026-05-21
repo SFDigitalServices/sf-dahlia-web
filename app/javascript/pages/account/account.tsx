92c70f49c61d00f7528f1419599eb8f770f7e977
cliu02 <32694266+cliu02@users.noreply.github.com>
2026-05-21 15:48:33 -0700

    feat: overview sign out

--- app/javascript/pages/account/account.tsx
+++ app/javascript/pages/account/account.tsx
@@ -1,16 +1,23 @@
 // New accounts layout for my-account.tsx
 import React from "react"
+import { t } from "@bloom-housing/ui-components"
+import { Button } from "@bloom-housing/ui-seeds"
 import Layout from "../../layouts/Layout"
 import AccountLayout from "../../layouts/AccountLayout"
 import withAppSetup from "../../layouts/withAppSetup"
+import UserContext from "../../authentication/context/UserContext"
 import { AppPages, RedirectType } from "../../util/routeUtil"
 import { withAuthentication } from "../../authentication/withAuthentication"
+import styles from "./account.module.scss"
 
 const Account = () => {
+  const { signOut } = React.useContext(UserContext)
   return (
     <Layout>
       <AccountLayout>
-        <p>Account</p>
+        <Button variant="text" onClick={signOut} className={styles.signOut}>
+          {t("accountLayout.account.signOut")}
+        </Button>
       </AccountLayout>
     </Layout>
   )
