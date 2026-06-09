12a6197a8b1dfe707455783c1b9a28f798e91c35
Justin Caovan <justincaovan@gmail.com>
2026-06-08 17:17:51 -0700

    Revert "refactor: clean test"
    This reverts commit a9eceeb5d898c682f9cd8db01c705690d374662e.

--- app/javascript/__tests__/pages/form/components/HouseholdMemberMultiStepWrapper.test.tsx
+++ app/javascript/__tests__/pages/form/components/HouseholdMemberMultiStepWrapper.test.tsx
@@ -168,12 +168,17 @@ describe("HouseholdMemberMultiStepWrapper", () => {
   })
 
   it("does not check address verification when household member shares applicant address", async () => {
-    renderHouseholdMemberMultiStepWrapper()
     const user = userEvent.setup()
 
+    renderHouseholdMemberMultiStepWrapper()
+
     await user.click(screen.getByText("+ " + t("label.addHouseholdMember")))
+
+    // fill required fields
     await user.type(screen.getByLabelText(/first name/i), "John")
     await user.type(screen.getByLabelText(/last name/i), "Doe")
+
+    // select same address = yes
     const yesButtons = screen.getAllByLabelText(t("t.yes"))
     await user.click(yesButtons[0])
 
@@ -182,6 +187,7 @@ describe("HouseholdMemberMultiStepWrapper", () => {
         name: t("label.householdMemberSave"),
       })
     )
+
     expect(locateVerifiedAddress).not.toHaveBeenCalled()
   })
 
@@ -189,7 +195,11 @@ describe("HouseholdMemberMultiStepWrapper", () => {
     renderHouseholdMemberMultiStepWrapper()
 
     const user = userEvent.setup()
+
+    // open form
     await user.click(screen.getByText("+ " + t("label.addHouseholdMember")))
+
+    // fill required fields
     await user.type(screen.getByLabelText(/first name/i), "John")
     await user.type(screen.getByLabelText(/last name/i), "Smith")
     await user.type(screen.getByLabelText("Month"), "12")
@@ -197,10 +207,10 @@ describe("HouseholdMemberMultiStepWrapper", () => {
     await user.type(screen.getByLabelText("Year"), "1990")
 
     const noButtons = screen.getAllByLabelText(t("t.no"))
-    await user.click(noButtons[0])
+    await user.click(noButtons[0]) // different address
 
     const yesButtons = screen.getAllByLabelText(t("t.yes"))
-    await user.click(yesButtons[1])
+    await user.click(yesButtons[1]) // works in sf
 
     await user.type(screen.getByLabelText(/street/i), "123 Main St")
     await user.type(screen.getByLabelText(/city/i), "San Francisco")
@@ -210,11 +220,13 @@ describe("HouseholdMemberMultiStepWrapper", () => {
       screen.getByLabelText(t("label.householdMemberRelationship")),
       "Spouse"
     )
+
     await user.click(
       screen.getByRole("button", {
         name: t("label.householdMemberSave"),
       })
     )
+
     await waitFor(() => {
       expect(mockLocateVerifiedAddress).toHaveBeenCalled()
     })
@@ -262,6 +274,7 @@ describe("HouseholdMemberMultiStepWrapper", () => {
         name: t("t.next"),
       })
     )
+
     expect(mockSaveFormData).toHaveBeenCalled()
     expect(screen.getByText("John Smith")).toBeInTheDocument()
   })
