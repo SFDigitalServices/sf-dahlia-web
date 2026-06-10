f219c1f83de476da5205612f7ae8c8bb813d6fec
cliu02 <32694266+cliu02@users.noreply.github.com>
2026-06-10 16:43:45 -0700

    feat: card styling

--- app/javascript/pages/account/components/ContactCard.tsx
+++ app/javascript/pages/account/components/ContactCard.tsx
@@ -1,9 +1,10 @@
-import { Icon, IconFillColors, t } from "@bloom-housing/ui-components"
-import { faEnvelope } from "@fortawesome/free-solid-svg-icons"
-import { Card, Button, Heading, Link } from "@bloom-housing/ui-seeds"
+import { t } from "@bloom-housing/ui-components"
+import { faEnvelope, faPhone } from "@fortawesome/free-solid-svg-icons"
+import { Card, Button, Heading, Link, Icon } from "@bloom-housing/ui-seeds"
 import { getMyAccountContactPath } from "../../../util/routeUtil"
 import React from "react"
 import { User } from "../../../authentication/user"
+import styles from "./ContactCard.module.scss"
 
 interface ContactCardProps {
   user?: User
@@ -12,33 +13,39 @@ interface ContactCardProps {
 const ContactCard = ({ user }: ContactCardProps) => {
   const hasContactInfo = user?.phone || user?.email
   return (
-    <Card>
-      <Heading priority={1} size="xl">
+    <Card className={styles.contactCard}>
+      <Heading priority={1} size="2xl">
         {t("accountLayout.accountCard.title", { name: user.firstName + " " + user.lastName })}
       </Heading>
-      {hasContactInfo ? (
-        <p>{t("accountLayout.accountCard.subtitle")}</p>
-      ) : (
-        <p>{t("accountLayout.accountCard.noInfo")}</p>
-      )}
-      <Icon symbol="phone" size="medium" fill={IconFillColors.primary} />
-      {user?.phone ? (
-        <span>{user.phone}</span>
-      ) : (
-        <Link href={getMyAccountContactPath()}>{t("accountLayout.accountCard.addPhone")}</Link>
-      )}
-      <Icon symbol={faEnvelope} size="medium" fill={IconFillColors.primary} />
-      {user?.email ? (
-        <span>{user.email}</span>
-      ) : (
-        <Link href={getMyAccountContactPath()}>{t("accountLayout.accountCard.addEmail")}</Link>
-      )}
-      <Button variant="primary" href={getMyAccountContactPath()}>
-        {" "}
-        {hasContactInfo
-          ? t("accountLayout.accountCard.changeInfo")
-          : t("accountLayout.accountCard.addInfo")}
-      </Button>
+      <p className={styles.subtitle}>
+        {t(
+          hasContactInfo ? "accountLayout.accountCard.subtitle" : "accountLayout.accountCard.noInfo"
+        )}
+      </p>
+      <div className={styles.contactRow}>
+        <Icon icon={faEnvelope} size="sm" className={styles.contactIcon} />
+        {user?.email ? (
+          <span>{user.email}</span>
+        ) : (
+          <Link href={getMyAccountContactPath()}>{t("accountLayout.accountCard.addEmail")}</Link>
+        )}
+      </div>
+      <div className={styles.contactRow}>
+        <Icon icon={faPhone} size="sm" className={styles.contactIcon} />
+        {user?.phone ? (
+          <span>{user.phone}</span>
+        ) : (
+          <Link href={getMyAccountContactPath()}>{t("accountLayout.accountCard.addPhone")}</Link>
+        )}
+      </div>
+
+      <span>
+        <Button variant="primary" className={styles.changeButton} href={getMyAccountContactPath()}>
+          {hasContactInfo
+            ? t("accountLayout.accountCard.changeInfo")
+            : t("accountLayout.accountCard.addInfo")}
+        </Button>
+      </span>
     </Card>
   )
 }
