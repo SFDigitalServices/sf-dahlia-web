582785158196372a5f302b2cec8315378c65c5c1
cliu02 <32694266+cliu02@users.noreply.github.com>
2026-06-10 11:55:22 -0700

    test: memory router

--- app/javascript/__tests__/components/MailingListSignup.test.tsx
+++ app/javascript/__tests__/components/MailingListSignup.test.tsx
@@ -1,10 +1,15 @@
 import React from "react"
 import { render } from "@testing-library/react"
 import { MailingListSignup } from "../../components/MailingListSignup"
+import { ConfigProvider } from "../../lib/ConfigContext"
 
 describe("MailingListSignup", () => {
   it("displays mailing list sign up", () => {
-    const { asFragment } = render(<MailingListSignup />)
+    const { asFragment } = render(
+      <ConfigProvider assetPaths={{}}>
+        <MailingListSignup />
+      </ConfigProvider>
+    )
 
     expect(asFragment()).toMatchSnapshot()
   })
