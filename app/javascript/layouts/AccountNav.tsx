660957ae6fff3c940027782b1c951a79f322fc52
cliu02 <32694266+cliu02@users.noreply.github.com>
2026-06-17 14:41:25 -0700

    fix: separate angular route from account settings page

--- app/javascript/layouts/AccountNav.tsx
+++ app/javascript/layouts/AccountNav.tsx
@@ -7,7 +7,7 @@ import { ConfigContext } from "../lib/ConfigContext"
 import { getPathWithoutLanguagePrefix } from "../util/languageUtil"
 import {
   getMyAccountPath,
-  getApplicationPath,
+  getApplicationsPath,
   getMyAccountSettingsPath,
   getSignInPath,
   getMyAccountContactPath,
@@ -50,7 +50,7 @@ const AccountNav = () => {
             />
             {t("accountLayout.nav.contactInfo")}
           </Tabs.Tab>
-          <Tabs.Tab href={getApplicationPath()} active={isNavActive(getApplicationPath())}>
+          <Tabs.Tab href={getApplicationsPath()} active={isNavActive(getApplicationsPath())}>
             <Icon size="md-large" symbol="application" className={styles.accountNavLinkIcon} />
             {t("accountLayout.nav.applications")}
           </Tabs.Tab>
