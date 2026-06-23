119632d56b0050e266d00f25049a27927a4f0a3b
Justin Caovan <justincaovan@gmail.com>
2026-06-23 12:32:16 -0700

    fix: cypress chaning accountsettings

--- cypress/e2e/accounts/accountSettings.e2e.ts
+++ cypress/e2e/accounts/accountSettings.e2e.ts
@@ -23,10 +23,11 @@ describe("Account Settings", () => {
 
     cy.visit("/account/settings")
     cy.contains("We use this information to help you fill in your application.")
-
     // Submit a name change
-    cy.get('input[name="firstName"]').clear().type("Jane")
-    cy.get('input[name="lastName"]').clear().type("Smith")
+    cy.get('input[name="firstName"]').clear()
+    cy.get('input[name="firstName"]').type("Jane")
+    cy.get('input[name="lastName"]').clear()
+    cy.get('input[name="lastName"]').type("Smith")
     cy.contains("We will update any applications you have not submitted yet.")
     cy.intercept(
       "/api/v1/account/update",
@@ -41,16 +42,20 @@ describe("Account Settings", () => {
     cy.get('input[name="firstName"]').clear()
     cy.get('input[name="lastName"]').click()
 
-    cy.get("#firstName-error").should("contain.text", "Enter first name").and("be.visible")
+    cy.get("#firstName-error").should("contain.text", "Enter first name")
+    cy.get("#firstName-error").should("be.visible")
     cy.get('input[name="firstName"]').should("not.be.focused")
     cy.get("button").contains("Enter first name").first().click()
     cy.get('input[name="firstName"]').should("be.focused")
     cy.get('input[name="firstName"]').type("Jane")
 
     // Submit DOB change
-    cy.get("input#dobObject\\.birthMonth").clear().type("10")
-    cy.get("input#dobObject\\.birthDay").clear().type("12")
-    cy.get("input#dobObject\\.birthYear").clear().type("2000")
+    cy.get("input#dobObject\\.birthMonth").clear()
+    cy.get("input#dobObject\\.birthMonth").type("10")
+    cy.get("input#dobObject\\.birthDay").clear()
+    cy.get("input#dobObject\\.birthDay").type("12")
+    cy.get("input#dobObject\\.birthYear").clear()
+    cy.get("input#dobObject\\.birthYear").type("2000")
     cy.intercept(
       "/api/v1/account/update",
       userObjectGenerator({ firstName: "Jane", lastName: "Smith", DOB: "2000-12-10" })
@@ -59,26 +64,32 @@ describe("Account Settings", () => {
     cy.wait("@updateDOB").its("response.statusCode").should("eq", 200)
 
     // Create DOB error
-    cy.get("input#dobObject\\.birthYear").clear().type("1899")
+    cy.get("input#dobObject\\.birthYear").clear()
+    cy.get("input#dobObject\\.birthYear").type("1899")
     cy.get("input#dobObject\\.birthMonth").click()
     cy.get("input#dobObject\\.birthYear").should("not.be.focused")
     cy.contains("Enter a valid date of birth. Enter date like: MM DD YYYY")
     cy.get("button").contains("Enter a valid date of birth").click()
     cy.get("input#dobObject\\.birthYear").should("be.focused")
-    cy.get("input#dobObject\\.birthYear").clear().type("2000")
+    cy.get("input#dobObject\\.birthYear").clear()
+    cy.get("input#dobObject\\.birthYear").type("2000")
 
     // Submit email change while validating
     cy.get('input[name="email"]').clear()
     cy.get("input#dobObject\\.birthMonth").click()
     cy.get('input[name="email"]').click()
     cy.contains("Enter email address like: example@web.com")
-    cy.get("button").contains("Enter email address").click().type("t")
+    cy.get("button").contains("Enter email address").click()
+    cy.get("button").contains("Enter email address").type("t")
     cy.contains("Email missing @ symbol. Enter email like: example@web.com")
-    cy.get("button").contains("Email missing @ symbol").click().type("est@")
+    cy.get("button").contains("Email missing @ symbol").click()
+    cy.get("button").contains("Email missing @ symbol").type("est@")
     cy.contains("Email entered incorrectly. Enter email like: example@web.com")
-    cy.get("button").contains("Email entered incorrectly").click().type("test")
+    cy.get("button").contains("Email entered incorrectly").click()
+    cy.get("button").contains("Email entered incorrectly").type("test")
     cy.contains("Email missing a dot ‘.’ in the domain. Enter email like: example@web.com")
-    cy.get("button").contains("Email missing a dot ‘.’ in the domain").click().type(".com")
+    cy.get("button").contains("Email missing a dot ‘.’ in the domain").click()
+    cy.get("button").contains("Email missing a dot ‘.’ in the domain").type(".com")
 
     cy.intercept("/api/v1/auth", AUTH).as("emailChange")
     cy.get('input[name="email"]').closest("form").find('button[type="submit"]').click()
@@ -90,12 +101,15 @@ describe("Account Settings", () => {
     // Submit password change
     cy.get('input[name="currentPassword"]').click()
     cy.get('input[name="password"]').click()
-    cy.get("button").contains("Enter current password").click().type("password")
+    cy.get("button").contains("Enter current password").click()
+    cy.get("button").contains("Enter current password").type("password")
     cy.get('input[name="password"]').click()
     cy.get('input[name="currentPassword"]').click()
-    cy.get("button").contains("Enter new password").click().type("password")
+    cy.get("button").contains("Enter new password").click()
+    cy.get("button").contains("Enter new password").type("password")
     cy.contains("Choose a strong password with at least 8 characters, 1 letter, and 1 number")
-    cy.get("button").contains("Choose a strong password").click().type("1")
+    cy.get("button").contains("Choose a strong password").click()
+    cy.get("button").contains("Choose a strong password").type("1")
     cy.intercept("/api/v1/auth/password", {
       message: "Your password has been successfully updated.",
       ...AUTH,
