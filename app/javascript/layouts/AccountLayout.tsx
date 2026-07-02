14c9cd05415438c58376f032876a41a42917f602
cliu02 <32694266+cliu02@users.noreply.github.com>
2026-07-02 12:02:50 -0700

    feat: DAH-4190 Account layout breadcrumb navigation (#2974)
    feat: mobile breadcrumbs

--- app/javascript/layouts/AccountLayout.tsx
+++ app/javascript/layouts/AccountLayout.tsx
@@ -1,14 +1,38 @@
 import React from "react"
 import AccountNav from "./AccountNav"
+import { Breadcrumbs, BreadcrumbLink, t } from "@bloom-housing/ui-components"
 import styles from "./AccountLayout.module.scss"
+import { getPathWithoutLanguagePrefix } from "../util/languageUtil"
+import { getMyAccountPath } from "../util/routeUtil"
 
 export interface AccountLayoutProps {
   children: React.ReactNode
 }
 
+const ACCOUNT_BREADCRUMB_LABELS: Record<string, string> = {
+  "/account/contact": "accountLayout.nav.contactInfo",
+  "/account/applications": "accountLayout.nav.applications",
+  "/account/settings": "accountSettings.title.sentenceCase",
+}
+
 const AccountLayout = ({ children }: AccountLayoutProps) => {
+  const breadcrumb =
+    ACCOUNT_BREADCRUMB_LABELS[getPathWithoutLanguagePrefix(window.location.pathname)]
+
   return (
     <div className={styles.accountLayoutBackground}>
+      {breadcrumb && (
+        <div className={styles.accountBreadcrumbs}>
+          <Breadcrumbs>
+            <BreadcrumbLink href={getMyAccountPath()}>
+              {t("accountLayout.nav.overview")}
+            </BreadcrumbLink>
+            <BreadcrumbLink href="" current>
+              {t(breadcrumb)}
+            </BreadcrumbLink>
+          </Breadcrumbs>
+        </div>
+      )}
       <div className={styles.accountLayout}>
         <AccountNav />
         <div className={styles.accountLayoutContent}>{children}</div>
