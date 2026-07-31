fa05bd49d418e5e225363ab2b90340785e98ab53
John Dunning <fw@johndunning.com>
2026-07-31 15:16:47 -0700

    Update Balance link/email and add rental agencies to housing counselor page
    DAH-4002: Update BALANCE website and email to bebalanced.org domain
    DAH-4076: Add Young Community Developers, Compass Family Services, and
    Bayview Senior Services as rental housing counselor agencies. Adds
    Samoan as a filterable language.

--- app/javascript/pages/getAssistance/counselor-filter.tsx
+++ app/javascript/pages/getAssistance/counselor-filter.tsx
@@ -42,6 +42,7 @@ const CounselorFilter = ({ handleFilterData, clearClick }: CounselorFilterProps)
     { label: t("assistance.housingCounselors.services.languages.french"), value: "french" },
     { label: t("assistance.housingCounselors.services.languages.mandarin"), value: "mandarin" },
     { label: t("assistance.housingCounselors.services.languages.russian"), value: "russian" },
+    { label: t("assistance.housingCounselors.services.languages.samoan"), value: "samoan" },
     { label: t("assistance.housingCounselors.services.languages.spanish"), value: "spanish" },
     { label: t("assistance.housingCounselors.services.languages.taiwanese"), value: "taiwanese" },
     { label: t("assistance.housingCounselors.services.languages.vietnamese"), value: "vietnamese" },
