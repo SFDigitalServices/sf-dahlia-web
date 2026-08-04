e51cd50dd042f7fafd73ed7c2aa17254b5375340
cliu02 <32694266+cliu02@users.noreply.github.com>
2026-08-04 12:16:51 -0700

    fix: redirect signed in user

--- app/javascript/pages/sign-in.tsx
+++ app/javascript/pages/sign-in.tsx
@@ -1,13 +1,16 @@
 import React from "react"
+import { Navigate } from "react-router"
 import { useFeatureFlag } from "../hooks/useFeatureFlag"
 
 import { t } from "@bloom-housing/ui-components"
 
 import { SignInForm } from "../authentication/SignInForm"
+import { isTokenValid } from "../authentication/token"
 import FormsLayout from "../layouts/FormLayout"
 import withAppSetup from "../layouts/withAppSetup"
-import { AppPages } from "../util/routeUtil"
+import { AppPages, getMyAccountPath } from "../util/routeUtil"
 import { UNLEASH_FLAG } from "../modules/constants"
+
 interface SignInProps {
   assetPaths: unknown
 }
@@ -16,7 +19,10 @@ const SignIn = (_props: SignInProps) => {
   const { unleashFlag: clerkEnabled } = useFeatureFlag(UNLEASH_FLAG.CLERK_AUTH, false)
   if (clerkEnabled) {
     console.log("Clerk authentication is enabled.")
+  } else if (isTokenValid()) {
+    return <Navigate to={getMyAccountPath()} replace />
   }
+
   return (
     <FormsLayout title={t("pageTitle.signIn")}>
       <SignInForm />
