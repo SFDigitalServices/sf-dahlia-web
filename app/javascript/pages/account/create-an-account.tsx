c8f03781eb70262b6ebb2a197112eedd476a3b72
cliu02 <32694266+cliu02@users.noreply.github.com>
2026-08-04 14:48:46 -0700

    feat: update get help section

--- app/javascript/pages/account/create-an-account.tsx
+++ app/javascript/pages/account/create-an-account.tsx
@@ -8,7 +8,7 @@ import { useForm } from "react-hook-form"
 import withAppSetup from "../../layouts/withAppSetup"
 import Layout from "../../layouts/Layout"
 import { AppPages, getEnterCodePath, getSignInPath } from "../../util/routeUtil"
-import { getCurrentLanguage, getSfGovUrl } from "../../util/languageUtil"
+import { getCurrentLanguage } from "../../util/languageUtil"
 import { useFeatureFlag } from "../../hooks/useFeatureFlag"
 import { UNLEASH_FLAG } from "../../modules/constants"
 import { CreateAccount } from "./create-account"
@@ -77,10 +77,7 @@ const CreateAnAccountPage = () => {
                 {t("nav.signIn")}
               </Button>
             </Card.Section>
-            <GetHelp
-              text={t("createAccount.getHelpLink")}
-              href={getSfGovUrl("https://www.sf.gov/learn-how-to-create-dahlia-account")}
-            />
+            <GetHelp flow="createAccount" />
           </Card>
         </div>
       </section>
