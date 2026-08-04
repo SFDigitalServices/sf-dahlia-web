b93ce184b2743b690a88aac295a959401b74bb18
cliu02 <32694266+cliu02@users.noreply.github.com>
2026-08-04 12:20:45 -0700

    fix: lint

--- app/javascript/pages/account/add-password.tsx
+++ app/javascript/pages/account/add-password.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/unbound-method */
 import React, { useEffect } from "react"
 import { useNavigate } from "react-router"
 import { useUser } from "@clerk/clerk-react"
--- app/javascript/pages/account/components/CodeField.tsx
+++ app/javascript/pages/account/components/CodeField.tsx
@@ -41,16 +41,16 @@ const CodeField = ({
               value: code[i] ?? "",
               inputMode: "numeric",
               maxLength: 6,
-// Remove code on backspace and move focus back
-onKeyDown: (event: React.KeyboardEvent<HTMLInputElement>) => {
-  if (event.key !== "Backspace") return
-  event.preventDefault()
-  if (!code) return
+              // Remove code on backspace and move focus back
+              onKeyDown: (event: React.KeyboardEvent<HTMLInputElement>) => {
+                if (event.key !== "Backspace") return
+                event.preventDefault()
+                if (!code) return
 
-  const index = Math.min(i, code.length - 1)
-  onChange(code.slice(0, index) + code.slice(index + 1))
-  document.querySelector<HTMLElement>(`#code-${Math.max(index, 1)}`)?.focus()
-},
+                const index = Math.min(i, code.length - 1)
+                onChange(code.slice(0, index) + code.slice(index + 1))
+                document.querySelector<HTMLElement>(`#code-${Math.max(index, 1)}`)?.focus()
+              },
               onFocus: (event: React.FocusEvent<HTMLInputElement>) => event.target.select(),
             }}
           />
