3d8e6496e18d138a5aee43b05636164b50666a67
cliu02 <32694266+cliu02@users.noreply.github.com>
2026-08-18 14:01:59 -0700

    test: mock axe

--- app/javascript/layouts/withAppSetup.tsx
+++ app/javascript/layouts/withAppSetup.tsx
@@ -35,7 +35,7 @@ const withAppSetup =
     }
   ) =>
   (props: P) => {
-    if (process.env.NODE_ENV !== "production") {
+    if (process.env.NODE_ENV !== "production" && process.env.NODE_ENV !== "test") {
       void axe(React, ReactDOM, 1000)
     }
 
