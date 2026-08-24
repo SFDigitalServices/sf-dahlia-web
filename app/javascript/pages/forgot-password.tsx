3ebb047cf996eae3476859dacf0eb4b16c8327d9
Justin Caovan <justincaovan@gmail.com>
2026-08-24 10:08:54 -0700

    fix: copilot

--- app/javascript/pages/forgot-password.tsx
+++ app/javascript/pages/forgot-password.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/unbound-method */
 import React from "react"
 import { ForgotPasswordFlow } from "../authentication/ForgotPasswordFlow"
 import { ForgotPasswordForm } from "../authentication/ForgotPasswordForm"
