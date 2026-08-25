ca316e91e1119ccf9bb3eda20ac9751e4798e4d4
Jim Lin <jim.lin@sfgov.org>
2026-08-25 16:19:18 -0700

    fix: update typing due to new sdk

--- app/javascript/authentication/context/UserProvider.tsx
+++ app/javascript/authentication/context/UserProvider.tsx
@@ -44,7 +44,7 @@ const ClerkProfile = ({
 
     void (async () => {
       try {
-        const sessionToken = await getToken()
+        const sessionToken: string | null = await getToken()
         if (!sessionToken) {
           throw new Error("Missing Clerk session token")
         }
