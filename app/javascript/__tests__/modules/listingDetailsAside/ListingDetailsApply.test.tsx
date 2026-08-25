71f3d4b7f36a9a3544236f56d750381b42f72ec7
cliu02 <32694266+cliu02@users.noreply.github.com>
2026-08-25 09:08:45 -0700

    test: update tests

--- app/javascript/__tests__/modules/listingDetailsAside/ListingDetailsApply.test.tsx
+++ app/javascript/__tests__/modules/listingDetailsAside/ListingDetailsApply.test.tsx
@@ -1,19 +1,41 @@
 import React from "react"
 import { render } from "@testing-library/react"
+import { MemoryRouter } from "react-router"
 import { ListingDetailsApply } from "../../../modules/listingDetailsAside/ListingDetailsApply"
 import { openSaleListing } from "../../data/RailsSaleListing/listing-sale-open"
 import { closedRentalListing } from "../../data/RailsRentalListing/listing-rental-closed"
 import { habitatListing } from "../../data/RailsSaleListing/listing-sale-habitat"
+import { useFeatureFlag } from "../../../hooks/useFeatureFlag"
+import { UNLEASH_FLAG } from "../../../modules/constants"
+
+jest.mock("../../../hooks/useFeatureFlag", () => ({
+  useFeatureFlag: jest.fn(),
+}))
 
 describe("ListingDetailsApply", () => {
+  beforeEach(() => {
+    ;(useFeatureFlag as jest.Mock).mockImplementation((flagName: string) => ({
+      flagsReady: true,
+      unleashFlag: flagName === UNLEASH_FLAG.FORM_ENGINE,
+    }))
+  })
+
   it("does not render if listing is closed", () => {
-    const { asFragment } = render(<ListingDetailsApply listing={closedRentalListing} />)
+    const { asFragment } = render(
+      <MemoryRouter>
+        <ListingDetailsApply listing={closedRentalListing} />
+      </MemoryRouter>
+    )
 
     expect(asFragment()).toMatchSnapshot()
   })
 
   it("renders if listing is open", () => {
-    const { asFragment } = render(<ListingDetailsApply listing={openSaleListing} />)
+    const { asFragment } = render(
+      <MemoryRouter>
+        <ListingDetailsApply listing={openSaleListing} />
+      </MemoryRouter>
+    )
 
     expect(asFragment()).toMatchSnapshot()
   })
@@ -26,7 +48,11 @@ describe("ListingDetailsApply", () => {
         Application_Due_Date: "2032-12-02T01:00:00.000+0000",
       }
 
-      const { asFragment } = render(<ListingDetailsApply listing={openHabitatListing} />)
+      const { asFragment } = render(
+        <MemoryRouter>
+          <ListingDetailsApply listing={openHabitatListing} />
+        </MemoryRouter>
+      )
 
       expect(asFragment()).toMatchSnapshot()
     }
